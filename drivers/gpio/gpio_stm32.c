kartben/zephyr-without-images

commit e04d70f5c971ead0dbf371695ce7edf24ec594ad

gpio: Remove obsolete API 1.0 callback mechanism
In 1.0 you could set only one callback on the whole gpio controller. It
was impossible for another sub-system to add another callback, without
overwritting an existing one.

Such API has been obsolete for a long time and no one is using it
anymore. Thus removing it entirely.

Change-Id: I6a17fd99373dc6cef1fa2ebb421e992412d5015e
Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/gpio/gpio_stm32.c
@@ -28,7 +28,6 @@
 #include <interrupt_controller/exti_stm32.h>
 
 #include "gpio_utils.h"
-#include "gpio_api_compat.h"
 
 /**
  * @brief Common GPIO driver for STM32 MCUs. Each SoC must implement a
@@ -214,8 +213,7 @@ DEVICE_AND_API_INIT(gpio_stm32_## __suffix,				\
 		    &gpio_stm32_cfg_## __suffix,			\
 		    SECONDARY,						\
 		    CONFIG_KERNEL_INIT_PRIORITY_DEVICE,			\
-		    &gpio_stm32_driver);				\
-GPIO_SETUP_COMPAT_DEV(gpio_stm32_## __suffix)
+		    &gpio_stm32_driver);
 
 #ifdef CONFIG_GPIO_STM32_PORTA
 GPIO_DEVICE_INIT("GPIOA", a, GPIOA_BASE, STM32_PORTA,

drivers/gpio/Makefile
@@ -1,7 +1,5 @@
 ccflags-y +=-I$(srctree)/drivers
 
-obj-y += gpio_api_compat.o
-
 obj-$(CONFIG_GPIO_DW) += gpio_dw.o
 obj-$(CONFIG_GPIO_PCAL9535A) += gpio_pcal9535a.o
 obj-$(CONFIG_GPIO_MMIO) += gpio_mmio.o

drivers/gpio/gpio_api_compat.c
@@ -1,102 +1,0 @@
-/*
- * Copyright (c) 2016 Intel Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @file Implementation of the API 1.0 GPIO compatibility layer
- */
-
-#include <errno.h>
-
-#include <gpio.h>
-#include <misc/util.h>
-
-#include "gpio_api_compat.h"
-
-/** These are maintained in a dedicated .gpio_compat section
- * See relevant arch's linker definitions in include/arch/
- */
-extern struct gpio_compat_cb __gpio_compat_start[];
-extern struct gpio_compat_cb __gpio_compat_end[];
-
-static struct gpio_compat_cb *_gpio_compat_dev_lookup(struct device *dev)
-{
-	struct gpio_compat_cb *cb;
-
-	for (cb = __gpio_compat_start; cb != __gpio_compat_end; cb++) {
-		if (cb->dev == dev) {
-			return cb;
-		}
-	}
-
-	return NULL;
-}
-
-static void _gpio_compat_handler(struct device *dev,
-				 struct gpio_callback *cb, uint32_t pins)
-{
-	struct _gpio_compat_data *data;
-	int bit;
-
-	data = CONTAINER_OF(cb, struct _gpio_compat_data, cb);
-
-	for (bit = 0; bit < 32; bit++) {
-		if (pins & BIT(bit)) {
-			data->handler(dev, bit);
-		}
-	}
-}
-
-int gpio_set_callback(struct device *dev, gpio_callback_t callback)
-{
-	struct gpio_compat_cb *compat = _gpio_compat_dev_lookup(dev);
-	int ret;
-
-	if (!compat) {
-		return -EIO;
-	}
-
-	ret = gpio_remove_callback(dev, &compat->d->cb);
-	if (ret != 0) {
-		return ret;
-	}
-
-	if (!callback) {
-		return 0;
-	}
-
-	compat->d->handler = callback;
-	compat->d->cb.handler = _gpio_compat_handler;
-
-	return gpio_add_callback(dev, &compat->d->cb);
-}
-
-void _gpio_enable_callback(struct device *dev, uint32_t pins)
-{
-	struct gpio_compat_cb *compat = _gpio_compat_dev_lookup(dev);
-
-	if (compat) {
-		compat->d->cb.pin_mask |= pins;
-	}
-}
-
-void _gpio_disable_callback(struct device *dev, uint32_t pins)
-{
-	struct gpio_compat_cb *compat = _gpio_compat_dev_lookup(dev);
-
-	if (compat) {
-		compat->d->cb.pin_mask &= ~(pins);
-	}
-}

drivers/gpio/gpio_api_compat.h
@@ -1,62 +1,0 @@
-/*
- * Copyright (c) 2016 Intel Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * @file Header for the API 1.0 GPIO compatibility code
- */
-
-#ifndef __GPIO_API_COMPAT_H__
-#define __GPIO_API_COMPAT_H__
-
-struct _gpio_compat_data {
-	struct gpio_callback cb;
-	gpio_callback_t handler;
-};
-
-struct gpio_compat_cb {
-	struct device *dev;
-	struct _gpio_compat_data *d;
-};
-
-/** This macro is mandatory to be used in order to enable the API 1.0
- * support on GPIO drivers.
- */
-#define GPIO_SETUP_COMPAT_DEV(dev_name)					\
-	static struct _gpio_compat_data (__gcd_##dev_name) = {};	\
-									\
-	static struct gpio_compat_cb (__gpio_compat_##dev_name) __used	\
-	__attribute__((__section__(".gpio_compat.init"))) = {		\
-		.dev = &(__device_##dev_name),				\
-		.d = &(__gcd_##dev_name)				\
-	}
-
-/**
- * @brief Enable the API v1.0 callback on given pins
- *
- * @param port device driver instance pointer to affect
- * @param pins mask of pins to enable
- */
-void _gpio_enable_callback(struct device *port, uint32_t pins);
-
-/**
- * @brief Disable the API v1.0 callback on given pins
- *
- * @param port device driver instance pointer to affect
- * @param pins mask of pins to disable
- */
-void _gpio_disable_callback(struct device *port, uint32_t pins);
-
-#endif /* __GPIO_API_COMPAT_H__ */

drivers/gpio/gpio_atmel_sam3.c
@@ -29,7 +29,6 @@
 
 #include <gpio.h>
 #include "gpio_utils.h"
-#include "gpio_api_compat.h"
 
 typedef void (*config_func_t)(struct device *port);
 
@@ -240,7 +239,6 @@ static int gpio_sam3_enable_callback(struct device *dev,
 		return -ENOTSUP;
 	}
 
-	_gpio_enable_callback(dev, mask);
 	cfg->port->ier |= mask;
 
 	return 0;
@@ -263,7 +261,6 @@ static int gpio_sam3_disable_callback(struct device *dev,
 		return -ENOTSUP;
 	}
 
-	_gpio_disable_callback(dev, mask);
 	cfg->port->idr |= mask;
 
 	return 0;
@@ -307,7 +304,6 @@ DEVICE_AND_API_INIT(gpio_sam3_a, CONFIG_GPIO_ATMEL_SAM3_PORTA_DEV_NAME,
 		    gpio_sam3_init, NULL, &gpio_sam3_a_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &gpio_sam3_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_sam3_a);
 
 void gpio_sam3_config_a(struct device *dev)
 {
@@ -334,7 +330,6 @@ DEVICE_AND_API_INIT(gpio_sam3_b, CONFIG_GPIO_ATMEL_SAM3_PORTB_DEV_NAME,
 		    gpio_sam3_init, NULL, &gpio_sam3_b_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &gpio_sam3_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_sam3_b);
 
 void gpio_sam3_config_b(struct device *dev)
 {
@@ -361,7 +356,6 @@ DEVICE_AND_API_INIT(gpio_sam3_c, CONFIG_GPIO_ATMEL_SAM3_PORTC_DEV_NAME,
 		    gpio_sam3_init, NULL, &gpio_sam3_c_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &gpio_sam3_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_sam3_c);
 
 void gpio_sam3_config_c(struct device *dev)
 {
@@ -388,7 +382,6 @@ DEVICE_AND_API_INIT(gpio_sam3_d, CONFIG_GPIO_ATMEL_SAM3_PORTD_DEV_NAME,
 		    gpio_sam3_init, NULL, &gpio_sam3_d_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &gpio_sam3_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_sam3_d);
 
 void gpio_sam3_config_d(struct device *dev)
 {

drivers/gpio/gpio_dw.c
@@ -20,7 +20,6 @@
 #include <gpio.h>
 #include "gpio_dw.h"
 #include "gpio_utils.h"
-#include "gpio_api_compat.h"
 
 #include <board.h>
 #include <sys_io.h>
@@ -313,11 +312,9 @@ static inline int gpio_dw_enable_callback(struct device *port, int access_op,
 	if (GPIO_ACCESS_BY_PIN == access_op) {
 		dw_write(base_addr, PORTA_EOI, BIT(pin));
 		dw_set_bit(base_addr, INTMASK, pin, 0);
-		_gpio_enable_callback(port, BIT(pin));
 	} else {
 		dw_write(base_addr, PORTA_EOI, BIT_MASK(config->bits));
 		dw_write(base_addr, INTMASK, 0);
-		_gpio_enable_callback(port, BIT_MASK(config->bits));
 	}
 
 	return 0;
@@ -331,10 +328,8 @@ static inline int gpio_dw_disable_callback(struct device *port, int access_op,
 
 	if (GPIO_ACCESS_BY_PIN == access_op) {
 		dw_set_bit(base_addr, INTMASK, pin, 1);
-		_gpio_disable_callback(port, BIT(pin));
 	} else {
 		dw_write(base_addr, INTMASK, BIT_MASK(config->bits));
-		_gpio_disable_callback(port, BIT_MASK(config->bits));
 	}
 
 	return 0;
@@ -511,7 +506,6 @@ DEVICE_AND_API_INIT(gpio_dw_0, CONFIG_GPIO_DW_0_NAME, gpio_dw_initialize,
 		    SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		    &api_funcs);
 #endif
-GPIO_SETUP_COMPAT_DEV(gpio_dw_0);
 
 void gpio_config_0_irq(struct device *port)
 {
@@ -579,7 +573,6 @@ DEVICE_AND_API_INIT(gpio_dw_1, CONFIG_GPIO_DW_1_NAME, gpio_dw_initialize,
 		    SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		    &api_funcs);
 #endif
-GPIO_SETUP_COMPAT_DEV(gpio_dw_1);
 
 void gpio_config_1_irq(struct device *port)
 {
@@ -646,7 +639,6 @@ DEVICE_AND_API_INIT(gpio_dw_2, CONFIG_GPIO_DW_2_NAME, gpio_dw_initialize,
 		    SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		    &api_funcs);
 #endif
-GPIO_SETUP_COMPAT_DEV(gpio_dw_2);
 
 void gpio_config_2_irq(struct device *port)
 {
@@ -713,7 +705,6 @@ DEVICE_AND_API_INIT(gpio_dw_3, CONFIG_GPIO_DW_3_NAME, gpio_dw_initialize,
 		    SECONDARY, CONFIG_GPIO_DW_INIT_PRIORITY,
 		    &api_funcs);
 #endif
-GPIO_SETUP_COMPAT_DEV(gpio_dw_3);
 
 void gpio_config_3_irq(struct device *port)
 {

drivers/gpio/gpio_k64.c
@@ -31,7 +31,6 @@
 
 #include "gpio_k64.h"
 #include "gpio_utils.h"
-#include "gpio_api_compat.h"
 
 static int gpio_k64_config(struct device *dev,
 			   int access_op, uint32_t pin, int flags)
@@ -202,10 +201,8 @@ static int gpio_k64_enable_callback(struct device *dev,
 	struct gpio_k64_data *data = dev->driver_data;
 
 	if (access_op == GPIO_ACCESS_BY_PIN) {
-		_gpio_enable_callback(dev, BIT(pin));
 		data->pin_callback_enables |= BIT(pin);
 	} else {
-		_gpio_enable_callback(dev, 0xFFFFFFFF);
 		data->pin_callback_enables = 0xFFFFFFFF;
 	}
 
@@ -219,10 +216,8 @@ static int gpio_k64_disable_callback(struct device *dev,
 	struct gpio_k64_data *data = dev->driver_data;
 
 	if (access_op == GPIO_ACCESS_BY_PIN) {
-		_gpio_disable_callback(dev, BIT(pin));
 		data->pin_callback_enables &= ~BIT(pin);
 	} else {
-		_gpio_disable_callback(dev, 0xFFFFFFFF);
 		data->pin_callback_enables = 0;
 	}
 
@@ -282,7 +277,6 @@ DEVICE_AND_API_INIT(gpio_k64_A, CONFIG_GPIO_K64_A_DEV_NAME, gpio_k64_A_init,
 		    &gpio_data_A, &gpio_k64_A_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
 		    &gpio_k64_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_k64_A);
 
 static int gpio_k64_A_init(struct device *dev)
 {
@@ -312,7 +306,6 @@ DEVICE_AND_API_INIT(gpio_k64_B, CONFIG_GPIO_K64_B_DEV_NAME, gpio_k64_B_init,
 		    &gpio_data_B, &gpio_k64_B_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
 		    &gpio_k64_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_k64_B);
 
 static int gpio_k64_B_init(struct device *dev)
 {
@@ -342,7 +335,6 @@ DEVICE_AND_API_INIT(gpio_k64_C, CONFIG_GPIO_K64_C_DEV_NAME, gpio_k64_C_init,
 		    &gpio_data_C, &gpio_k64_C_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
 		    &gpio_k64_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_k64_C);
 
 static int gpio_k64_C_init(struct device *dev)
 {
@@ -372,7 +364,6 @@ DEVICE_AND_API_INIT(gpio_k64_D, CONFIG_GPIO_K64_D_DEV_NAME, gpio_k64_D_init,
 		    &gpio_data_D, &gpio_k64_D_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
 		    &gpio_k64_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_k64_D);
 
 static int gpio_k64_D_init(struct device *dev)
 {
@@ -402,7 +393,6 @@ DEVICE_AND_API_INIT(gpio_k64_E, CONFIG_GPIO_K64_E_DEV_NAME, gpio_k64_E_init,
 		    &gpio_data_E, &gpio_k64_E_cfg,
 		    SECONDARY, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
 		    &gpio_k64_drv_api_funcs);
-GPIO_SETUP_COMPAT_DEV(gpio_k64_E);
 
 static int gpio_k64_E_init(struct device *dev)
 {

drivers/gpio/gpio_nrf5.c
@@ -75,8 +75,6 @@ struct gpio_nrf5_config {
 };
 
 struct gpio_nrf5_data {
-	/* port ISR callback routine address */
-	gpio_callback_t callback_func;
 	/* pin callback routine enable flags, by pin number */
 	uint32_t pin_callback_enables;
 	/* port callback routine enable flag */
@@ -229,4 +227,3 @@ static int gpio_nrf5_P0_init(struct device *dev)
 }
 
 #endif /* CONFIG_GPIO_NRF5_P0 */
-

commit 3c8c77db1ec400429f4ace3f99779e92694a7d68

gpio: stm32: Fix bug introduced by removing API 1.0 support
Bug was introduced by e04d70f5c971ead0dbf371695ce7edf24ec594ad

Change-Id: I91ceab2ca20c4d2f70a953b56d895152a471125a
Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/gpio/gpio_stm32.c
@@ -42,7 +42,9 @@ static void gpio_stm32_isr(int line, void *arg)
 	struct device *dev = arg;
 	struct gpio_stm32_data *data = dev->driver_data;
 
-	_gpio_fire_callbacks(&data->cb, dev, BIT(line));
+	if (BIT(line) & data->cb_pins) {
+		_gpio_fire_callbacks(&data->cb, dev, BIT(line));
+	}
 }
 
 /**
@@ -145,11 +147,13 @@ static int gpio_stm32_manage_callback(struct device *dev,
 static int gpio_stm32_enable_callback(struct device *dev,
 				      int access_op, uint32_t pin)
 {
+	struct gpio_stm32_data *data = dev->driver_data;
+
 	if (access_op != GPIO_ACCESS_BY_PIN) {
 		return -ENOTSUP;
 	}
 
-	_gpio_enable_callback(dev, BIT(pin));
+	data->cb_pins |= BIT(pin);
 
 	return 0;
 }
@@ -157,11 +161,13 @@ static int gpio_stm32_enable_callback(struct device *dev,
 static int gpio_stm32_disable_callback(struct device *dev,
 				       int access_op, uint32_t pin)
 {
+	struct gpio_stm32_data *data = dev->driver_data;
+
 	if (access_op != GPIO_ACCESS_BY_PIN) {
 		return -ENOTSUP;
 	}
 
-	_gpio_disable_callback(dev, BIT(pin));
+	data->cb_pins &= ~BIT(pin);
 
 	return 0;
 }

drivers/gpio/gpio_stm32.h
@@ -42,6 +42,8 @@ struct gpio_stm32_config {
  * @brief driver data
  */
 struct gpio_stm32_data {
+	/* Enabled INT pins generating a cb */
+	uint32_t cb_pins;
 	/* user ISR cb */
 	sys_slist_t cb;
 };