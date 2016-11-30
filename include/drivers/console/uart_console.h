kartben/zephyr-without-images

commit a6dc1f64f8cccc27e88d3c2c1aa6e3994974a9b2

console: Fix warnings related to the use of deprecated APIs
This fixes the warnings related to the use of deprecated APIs
converting them to use the unified version.

Change-Id: I76d076de27ecdae4af46abf4baac68e2cc1313c0
Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>

include/drivers/console/uart_console.h
@@ -38,14 +38,14 @@ struct uart_console_input {
  *  terminated. Application before calling register function need to
  *  initialize two fifo queues mentioned below.
  *
- *  @param avail nano_fifo queue keeping available input slots
- *  @param lines nano_fifo queue of entered lines which to be processed
+ *  @param avail k_fifo queue keeping available input slots
+ *  @param lines k_fifo queue of entered lines which to be processed
  *         in the application code.
  *  @param completion callback for tab completion of entered commands
  *
  *  @return N/A
  */
-void uart_register_input(struct nano_fifo *avail, struct nano_fifo *lines,
+void uart_register_input(struct k_fifo *avail, struct nano_fifo *lines,
 			 uint8_t (*completion)(char *str, uint8_t len));
 
 /*

drivers/console/console_handler_shell.c
@@ -52,8 +52,8 @@ static char __stack stack[STACKSIZE];
 #define MAX_CMD_QUEUED 3
 static struct uart_console_input buf[MAX_CMD_QUEUED];
 
-static struct nano_fifo avail_queue;
-static struct nano_fifo cmds_queue;
+static struct k_fifo avail_queue;
+static struct k_fifo cmds_queue;
 
 static shell_cmd_function_t app_cmd_handler;
 static shell_prompt_function_t app_prompt_handler;
@@ -81,7 +81,7 @@ static void line_queue_init(void)
 	int i;
 
 	for (i = 0; i < MAX_CMD_QUEUED; i++) {
-		nano_fifo_put(&avail_queue, &buf[i]);
+		k_fifo_put(&avail_queue, &buf[i]);
 	}
 }
 
@@ -323,7 +323,7 @@ static shell_cmd_function_t get_cb(int argc, char *argv[])
 	return NULL;
 }
 
-static void shell(int arg1, int arg2)
+static void shell(void *p1, void *p2, void *p3)
 {
 	char *argv[ARGC_MAX + 1];
 	size_t argc;
@@ -334,11 +334,11 @@ static void shell(int arg1, int arg2)
 
 		printk("%s", get_prompt());
 
-		cmd = nano_fiber_fifo_get(&cmds_queue, TICKS_UNLIMITED);
+		cmd = k_fifo_get(&cmds_queue, K_FOREVER);
 
 		argc = line2argv(cmd->line, argv, ARRAY_SIZE(argv));
 		if (!argc) {
-			nano_fiber_fifo_put(&avail_queue, cmd);
+			k_fifo_put(&avail_queue, cmd);
 			continue;
 		}
 
@@ -349,7 +349,7 @@ static void shell(int arg1, int arg2)
 			} else {
 				printk("Unrecognized command: %s\n", argv[0]);
 				printk("Type 'help' for list of available commands\n");
-				nano_fiber_fifo_put(&avail_queue, cmd);
+				k_fifo_put(&avail_queue, cmd);
 				continue;
 			}
 		}
@@ -359,7 +359,7 @@ static void shell(int arg1, int arg2)
 			show_cmd_help(argv);
 		}
 
-		nano_fiber_fifo_put(&avail_queue, cmd);
+		k_fifo_put(&avail_queue, cmd);
 	}
 }
 
@@ -434,7 +434,7 @@ static uint8_t completion(char *line, uint8_t len)
 
 	/*
 	 * line to completion is not ended by '\0' as the line that gets from
-	 * nano_fiber_fifo_get function
+	 * k_fifo_get function
 	 */
 	line[len] = '\0';
 	dest = get_command_to_complete(line, &command_prefix);
@@ -508,14 +508,15 @@ static uint8_t completion(char *line, uint8_t len)
 
 void shell_init(const char *str)
 {
-	nano_fifo_init(&cmds_queue);
-	nano_fifo_init(&avail_queue);
+	k_fifo_init(&cmds_queue);
+	k_fifo_init(&avail_queue);
 
 	line_queue_init();
 
 	prompt = str ? str : "";
 
-	task_fiber_start(stack, STACKSIZE, shell, 0, 0, 7, 0);
+	k_thread_spawn(stack, STACKSIZE, shell, NULL, NULL, NULL,
+		       K_PRIO_COOP(7), 0, K_NO_WAIT);
 
 	/* Register serial console handler */
 	uart_register_input(&avail_queue, &cmds_queue, completion);

drivers/console/uart_console.c
@@ -135,8 +135,8 @@ extern void __printk_hook_install(int (*fn)(int));
 #endif
 
 #if defined(CONFIG_CONSOLE_HANDLER)
-static struct nano_fifo *avail_queue;
-static struct nano_fifo *lines_queue;
+static struct k_fifo *avail_queue;
+static struct k_fifo *lines_queue;
 static uint8_t (*completion_cb)(char *line, uint8_t len);
 
 /* Control characters */
@@ -372,7 +372,7 @@ void uart_console_isr(struct device *unused)
 #endif
 
 		if (!cmd) {
-			cmd = nano_isr_fifo_get(avail_queue, TICKS_NONE);
+			cmd = k_fifo_get(avail_queue, K_NO_WAIT);
 			if (!cmd)
 				return;
 		}
@@ -414,7 +414,7 @@ void uart_console_isr(struct device *unused)
 				uart_poll_out(uart_console_dev, '\n');
 				cur = 0;
 				end = 0;
-				nano_isr_fifo_put(lines_queue, cmd);
+				k_fifo_put(lines_queue, cmd);
 				cmd = NULL;
 				break;
 			case '\t':
@@ -453,7 +453,7 @@ static void console_input_init(void)
 	uart_irq_rx_enable(uart_console_dev);
 }
 
-void uart_register_input(struct nano_fifo *avail, struct nano_fifo *lines,
+void uart_register_input(struct k_fifo *avail, struct k_fifo *lines,
 			 uint8_t (*completion)(char *str, uint8_t len))
 {
 	avail_queue = avail;

commit fe21ed82725ff68c1acf35dbe600e4114d5762c6

uart: fix typo in uart_console.h
Fix typo introduced by commit a6dc1f6("console: Fix warnings
related to the use of deprecated APIs")

Change-Id: I6b4db8ba781fecae4413f473001f40ff53c765f0
Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>

include/drivers/console/uart_console.h
@@ -23,7 +23,7 @@
 extern "C" {
 #endif
 
-#include <nanokernel.h>
+#include <kernel.h>
 
 #define MAX_LINE_LEN 256
 struct uart_console_input {
@@ -45,7 +45,7 @@ struct uart_console_input {
  *
  *  @return N/A
  */
-void uart_register_input(struct k_fifo *avail, struct nano_fifo *lines,
+void uart_register_input(struct k_fifo *avail, struct k_fifo *lines,
 			 uint8_t (*completion)(char *str, uint8_t len));
 
 /*