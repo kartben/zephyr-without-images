ttrahan/zephyr

net: Add net_analyze_stack() macro
This macro can be used to check stack usage. It should only
be used when debugging is activated.

Change-Id: I99fe6f9db1f3e955152a91f3a18d031126172cfd
Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

include/net/yaip/net_core.h
@@ -53,6 +53,61 @@ struct net_context;
 /* Called by lower network stack when a network packet has been received */
 int net_recv(struct net_if *iface, struct net_buf *buf);
 
+/** @cond ignore */
+#if defined(CONFIG_INIT_STACKS) && defined(CONFIG_PRINTK)
+#include <offsets.h>
+#include <misc/printk.h>
+
+static inline void net_analyze_stack(const char *name,
+				     unsigned char *stack,
+				     size_t size)
+{
+	unsigned i, stack_offset, pcnt, unused = 0;
+
+	/* The TCS is always placed on a 4-byte aligned boundary - if
+	 * the stack beginning doesn't match that there will be some
+	 * unused bytes in the beginning.
+	 */
+	stack_offset = __tTCS_SIZEOF + ((4 - ((unsigned)stack % 4)) % 4);
+
+/* TODO
+ * Currently all supported platforms have stack growth down and there is no
+ * Kconfig option to configure it so this always build "else" branch.
+ * When support for platform with stack direction up (or configurable direction)
+ * is added this check should be confirmed that correct Kconfig option is used.
+ */
+#if defined(CONFIG_STACK_GROWS_UP)
+	for (i = size - 1; i >= stack_offset; i--) {
+		if ((unsigned char)stack[i] == 0xaa) {
+			unused++;
+		} else {
+			break;
+		}
+	}
+#else
+	for (i = stack_offset; i < size; i++) {
+		if ((unsigned char)stack[i] == 0xaa) {
+			unused++;
+		} else {
+			break;
+		}
+	}
+#endif
+
+	/* Calculate the real size reserved for the stack */
+	size -= stack_offset;
+	pcnt = ((size - unused) * 100) / size;
+
+	printk("net (%p): %s stack real size %u "
+	       "unused %u usage %u/%u (%u %%)\n",
+	       sys_thread_self_get(), name,
+	       size + stack_offset, unused, size - unused, size, pcnt);
+}
+#else
+#define net_analyze_stack(...)
+#endif
+/* @endcond */
+
 #ifdef __cplusplus
 }
 #endif

kartben/zephyr-without-images

include/net/yaip/net_core.h
@@ -53,6 +53,61 @@ struct net_context;
 /* Called by lower network stack when a network packet has been received */
 int net_recv(struct net_if *iface, struct net_buf *buf);
 
+/** @cond ignore */
+#if defined(CONFIG_INIT_STACKS) && defined(CONFIG_PRINTK)
+#include <offsets.h>
+#include <misc/printk.h>
+
+static inline void net_analyze_stack(const char *name,
+				     unsigned char *stack,
+				     size_t size)
+{
+	unsigned i, stack_offset, pcnt, unused = 0;
+
+	/* The TCS is always placed on a 4-byte aligned boundary - if
+	 * the stack beginning doesn't match that there will be some
+	 * unused bytes in the beginning.
+	 */
+	stack_offset = K_THREAD_SIZEOF + ((4 - ((unsigned)stack % 4)) % 4);
+
+/* TODO
+ * Currently all supported platforms have stack growth down and there is no
+ * Kconfig option to configure it so this always build "else" branch.
+ * When support for platform with stack direction up (or configurable direction)
+ * is added this check should be confirmed that correct Kconfig option is used.
+ */
+#if defined(CONFIG_STACK_GROWS_UP)
+	for (i = size - 1; i >= stack_offset; i--) {
+		if ((unsigned char)stack[i] == 0xaa) {
+			unused++;
+		} else {
+			break;
+		}
+	}
+#else
+	for (i = stack_offset; i < size; i++) {
+		if ((unsigned char)stack[i] == 0xaa) {
+			unused++;
+		} else {
+			break;
+		}
+	}
+#endif
+
+	/* Calculate the real size reserved for the stack */
+	size -= stack_offset;
+	pcnt = ((size - unused) * 100) / size;
+
+	printk("net (%p): %s stack real size %u "
+	       "unused %u usage %u/%u (%u %%)\n",
+	       sys_thread_self_get(), name,
+	       size + stack_offset, unused, size - unused, size, pcnt);
+}
+#else
+#define net_analyze_stack(...)
+#endif
+/* @endcond */
+
 #ifdef __cplusplus
 }
 #endif