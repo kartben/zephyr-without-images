kartben/zephyr-without-images

commit e02376c57b290e6c86d7fb76e73040f7b7365542

kernel: Refactor common _new_thread init code
We do a bit of the same stuff on all the arch's to setup a new thread.
So lets put that code in a common place so we unify it for everyone and
reduce some duplicated code.

Change-Id: Ic04121bfd6846aece16aa7ffd4382bdcdb6136e3
Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

arch/xtensa/core/thread.c
@@ -86,18 +86,17 @@ void _new_thread(char *pStack, size_t stackSize,
 	/* k_thread is located at top of stack while frames are located at end
 	 * of it
 	 */
-	struct k_thread *thread = (struct k_thread *)(pStack);
+	struct k_thread *thread;
 #if XCHAL_CP_NUM > 0
 	u32_t *cpSA;
 	char *cpStack;
 #endif
 
+	thread = _new_thread_init(pStack, stackSize, priority, options);
+
 #ifdef CONFIG_DEBUG
 	printk("\nstackPtr = %p, stackSize = %d\n", pStack, stackSize);
 	printk("stackEnd = %p\n", stackEnd);
-#endif
-#ifdef CONFIG_INIT_STACKS
-	memset(pStack, 0xaa, stackSize);
 #endif
 #if XCHAL_CP_NUM > 0
 	/* Ensure CP state descriptor is correctly initialized */
@@ -154,14 +153,6 @@ void _new_thread(char *pStack, size_t stackSize,
 #endif
 	thread->callee_saved.topOfStack = pInitCtx;
 	thread->arch.flags = 0;
-	_init_thread_base(&thread->base, prio, _THREAD_PRESTART, options);
-	/* static threads overwrite it afterwards with real value */
-	thread->init_data = NULL;
-	thread->fn_abort = NULL;
-#ifdef CONFIG_THREAD_CUSTOM_DATA
-	/* Initialize custom data field (value is opaque to kernel) */
-	thread->custom_data = NULL;
-#endif
 #ifdef CONFIG_THREAD_MONITOR
 	/*
 	 * In debug mode thread->entry give direct access to the thread entry

arch/arc/core/thread.c
@@ -83,11 +83,9 @@ void _new_thread(char *pStackMem, size_t stackSize,
 	char *stackEnd = pStackMem + stackSize;
 	struct init_stack_frame *pInitCtx;
 
-	struct k_thread *thread = (struct k_thread *) pStackMem;
+	struct k_thread *thread;
 
-#ifdef CONFIG_INIT_STACKS
-	memset(pStackMem, 0xaa, stackSize);
-#endif
+	thread = _new_thread_init(pStackMem, stackSize, priority, options);
 
 	/* carve the thread entry struct from the "base" of the stack */
 
@@ -111,18 +109,6 @@ void _new_thread(char *pStackMem, size_t stackSize,
 	thread->arch.stack_top = (u32_t) stackEnd;
 #else
 	pInitCtx->status32 = _ARC_V2_STATUS32_E(_ARC_V2_DEF_IRQ_LEVEL);
-#endif
-
-	_init_thread_base(&thread->base, priority, _THREAD_PRESTART, options);
-
-	/* static threads overwrite them afterwards with real values */
-	thread->init_data = NULL;
-	thread->fn_abort = NULL;
-
-#ifdef CONFIG_THREAD_CUSTOM_DATA
-	/* Initialize custom data field (value is opaque to kernel) */
-
-	thread->custom_data = NULL;
 #endif
 
 #ifdef CONFIG_THREAD_MONITOR

arch/arm/core/thread.c
@@ -83,9 +83,7 @@ void _new_thread(char *pStackMem, size_t stackSize,
 	struct __esf *pInitCtx;
 	struct k_thread *thread = (struct k_thread *) pStackMem;
 
-#ifdef CONFIG_INIT_STACKS
-	memset(pStackMem, 0xaa, stackSize);
-#endif
+	thread = _new_thread_init(pStackMem, stackSize, priority, options);
 
 	/* carve the thread entry struct from the "base" of the stack */
 
@@ -99,18 +97,6 @@ void _new_thread(char *pStackMem, size_t stackSize,
 	pInitCtx->a4 = (u32_t)parameter3;
 	pInitCtx->xpsr =
 		0x01000000UL; /* clear all, thumb bit is 1, even if RO */
-
-	_init_thread_base(&thread->base, priority, _THREAD_PRESTART, options);
-
-	/* static threads overwrite it afterwards with real value */
-	thread->init_data = NULL;
-	thread->fn_abort = NULL;
-
-#ifdef CONFIG_THREAD_CUSTOM_DATA
-	/* Initialize custom data field (value is opaque to kernel) */
-
-	thread->custom_data = NULL;
-#endif
 
 #ifdef CONFIG_THREAD_MONITOR
 	/*

arch/nios2/core/thread.c
@@ -60,9 +60,8 @@ void _new_thread(char *stack_memory, size_t stack_size,
 	struct k_thread *thread;
 	struct init_stack_frame *iframe;
 
-#ifdef CONFIG_INIT_STACKS
-	memset(stack_memory, 0xaa, stack_size);
-#endif
+	thread = _new_thread_init(stack_memory, stack_size, priority, options);
+
 	/* Initial stack frame data, stored at the base of the stack */
 	iframe = (struct init_stack_frame *)
 		STACK_ROUND_DOWN(stack_memory + stack_size - sizeof(*iframe));
@@ -73,19 +72,6 @@ void _new_thread(char *stack_memory, size_t stack_size,
 	iframe->arg2 = arg2;
 	iframe->arg3 = arg3;
 
-	/* Initialize various struct k_thread members */
-	thread = (struct k_thread *)stack_memory;
-
-	_init_thread_base(&thread->base, priority, _THREAD_PRESTART, options);
-
-	/* static threads overwrite it afterwards with real value */
-	thread->init_data = NULL;
-	thread->fn_abort = NULL;
-
-#ifdef CONFIG_THREAD_CUSTOM_DATA
-	/* Initialize custom data field (value is opaque to kernel) */
-	thread->custom_data = NULL;
-#endif
 	thread->callee_saved.sp = (u32_t)iframe;
 	thread->callee_saved.ra = (u32_t)_thread_entry_wrapper;
 	thread->callee_saved.key = NIOS2_STATUS_PIE_MSK;

arch/riscv32/core/thread.c
@@ -44,9 +44,8 @@ void _new_thread(char *stack_memory, size_t stack_size,
 	struct k_thread *thread;
 	struct __esf *stack_init;
 
-#ifdef CONFIG_INIT_STACKS
-	memset(stack_memory, 0xaa, stack_size);
-#endif
+	thread = _new_thread_init(stack_memory, stack_size, priority, options);
+
 	/* Initial stack frame for thread */
 	stack_init = (struct __esf *)
 		STACK_ROUND_DOWN(stack_memory +
@@ -82,20 +81,6 @@ void _new_thread(char *stack_memory, size_t stack_size,
 	 */
 	stack_init->mstatus = SOC_MSTATUS_DEF_RESTORE;
 	stack_init->mepc = (u32_t)_thread_entry_wrapper;
-
-	/* Initialize various struct k_thread members */
-	thread = (struct k_thread *)stack_memory;
-
-	_init_thread_base(&thread->base, priority, _THREAD_PRESTART, options);
-
-	/* static threads overwrite it afterwards with real value */
-	thread->init_data = NULL;
-	thread->fn_abort = NULL;
-
-#ifdef CONFIG_THREAD_CUSTOM_DATA
-	/* Initialize custom data field (value is opaque to kernel) */
-	thread->custom_data = NULL;
-#endif
 
 	thread->callee_saved.sp = (u32_t)stack_init;
 

arch/x86/core/thread.c
@@ -67,27 +67,14 @@ static ALWAYS_INLINE void thread_monitor_init(struct k_thread *thread)
  */
 static void _new_thread_internal(char *pStackMem, unsigned int stackSize,
 				 int priority,
-				 unsigned int options)
+				 unsigned int options,
+				 struct k_thread *thread)
 {
 	unsigned long *pInitialCtx;
-	/* ptr to the new task's k_thread */
-	struct k_thread *thread = (struct k_thread *)pStackMem;
 
 #if (defined(CONFIG_FP_SHARING) || defined(CONFIG_GDB_INFO))
 	thread->arch.excNestCount = 0;
 #endif /* CONFIG_FP_SHARING || CONFIG_GDB_INFO */
-
-	_init_thread_base(&thread->base, priority, _THREAD_PRESTART, options);
-
-	/* static threads overwrite it afterwards with real value */
-	thread->init_data = NULL;
-	thread->fn_abort = NULL;
-
-#ifdef CONFIG_THREAD_CUSTOM_DATA
-	/* Initialize custom data field (value is opaque to kernel) */
-
-	thread->custom_data = NULL;
-#endif
 
 	/*
 	 * The creation of the initial stack for the task has already been done.
@@ -235,10 +222,9 @@ void _new_thread(char *pStackMem, size_t stackSize,
 	_ASSERT_VALID_PRIO(priority, pEntry);
 
 	unsigned long *pInitialThread;
+	struct k_thread *thread;
 
-#ifdef CONFIG_INIT_STACKS
-	memset(pStackMem, 0xaa, stackSize);
-#endif
+	thread = _new_thread_init(pStackMem, stackSize, priority, options);
 
 	/* carve the thread entry struct from the "base" of the stack */
 
@@ -288,5 +274,5 @@ void _new_thread(char *pStackMem, size_t stackSize,
 	 * aside for the thread's stack.
 	 */
 
-	_new_thread_internal(pStackMem, stackSize, priority, options);
+	_new_thread_internal(pStackMem, stackSize, priority, options, thread);
 }

kernel/include/kernel_structs.h
@@ -12,6 +12,7 @@
 #if !defined(_ASMLANGUAGE)
 #include <atomic.h>
 #include <misc/dlist.h>
+#include <string.h>
 #endif
 
 /*
@@ -238,6 +239,34 @@ _set_thread_return_value_with_data(struct k_thread *thread,
 extern void _init_thread_base(struct _thread_base *thread_base,
 			      int priority, u32_t initial_state,
 			      unsigned int options);
+
+static ALWAYS_INLINE struct k_thread *_new_thread_init(char *pStack,
+						       size_t stackSize,
+						       int prio,
+						       unsigned int options)
+{
+	struct k_thread *thread;
+
+#ifdef CONFIG_INIT_STACKS
+	memset(pStack, 0xaa, stackSize);
+#endif
+
+	/* Initialize various struct k_thread members */
+	thread = (struct k_thread *)pStack;
+
+	_init_thread_base(&thread->base, prio, _THREAD_PRESTART, options);
+
+	/* static threads overwrite it afterwards with real value */
+	thread->init_data = NULL;
+	thread->fn_abort = NULL;
+
+#ifdef CONFIG_THREAD_CUSTOM_DATA
+	/* Initialize custom data field (value is opaque to kernel) */
+	thread->custom_data = NULL;
+#endif
+
+	return thread;
+}
 
 #endif /* _ASMLANGUAGE */
 

commit c91eea766c62064e02ea19ff6c92636525afba1e

xtensa port: Fixed compilation error introduced by recent changes.
The error was introduced by
    e02376c57b290e6c86d7fb76e73040f7b7365542

Change-Id: Ibf930107a7a690e0cb0851b7c247d524e3cb89e5
Signed-off-by: Mazen NEIFER <mazen@nestwave.com>

arch/xtensa/core/thread.c
@@ -35,7 +35,7 @@ extern void _xt_user_exit(void);
  * @param p1 first param to entry point
  * @param p2 second param to entry point
  * @param p3 third param to entry point
- * @param prio thread priority
+ * @param priority thread priority
  * @param options is unused (saved for future expansion)
  *
  * @return N/A
@@ -44,7 +44,7 @@ extern void _xt_user_exit(void);
 void _new_thread(char *pStack, size_t stackSize,
 		void (*pEntry)(void *, void *, void *),
 		void *p1, void *p2, void *p3,
-		int prio, unsigned int options)
+		int priority, unsigned int options)
 {
 	/* Align stack end to maximum alignment requirement. */
 	char *stackEnd = (char *)ROUND_DOWN(pStack + stackSize, 16);