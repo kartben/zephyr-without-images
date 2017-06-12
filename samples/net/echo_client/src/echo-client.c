kartben/zephyr-without-images

commit cd773bc29be113276eb57e152b4a47fbe45837f7

samples: use k_thread_create()
Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

samples/net/echo_client/src/echo-client.c
@@ -160,10 +160,12 @@ static struct sockaddr_in6 peer_addr6 = {
 
 #if defined(CONFIG_NET_UDP)
 static char __noinit __stack ipv6_udp_stack[STACKSIZE];
+static struct k_thread ipv6_udp_thread_data;
 #endif
 
 #if defined(CONFIG_NET_TCP)
 static char __noinit __stack ipv6_tcp_stack[STACKSIZE];
+static struct k_thread ipv6_tcp_thread_data;
 #endif
 
 #endif /* CONFIG_NET_IPV6 */
@@ -189,10 +191,12 @@ static struct sockaddr_in peer_addr4 = {
 
 #if defined(CONFIG_NET_UDP)
 static char __noinit __stack ipv4_udp_stack[STACKSIZE];
+static struct k_thread ipv4_udp_thread_data;
 #endif
 
 #if defined(CONFIG_NET_TCP)
 static char __noinit __stack ipv4_tcp_stack[STACKSIZE];
+static struct k_thread ipv4_tcp_thread_data;
 #endif
 
 #endif /* CONFIG_NET_IPV4 */
@@ -870,27 +874,27 @@ static void event_iface_up(struct net_mgmt_event_callback *cb,
 	}
 
 #if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_UDP)
-	k_thread_spawn(ipv4_udp_stack, STACKSIZE,
-		       (k_thread_entry_t)send_udp_ipv4,
-		       udp_send4, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&ipv4_udp_thread_data, ipv4_udp_stack, STACKSIZE,
+			(k_thread_entry_t)send_udp_ipv4,
+			udp_send4, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 #endif
 
 #if defined(CONFIG_NET_IPV4) && defined(CONFIG_NET_TCP)
-	k_thread_spawn(ipv4_tcp_stack, STACKSIZE,
-		       (k_thread_entry_t)send_tcp_ipv4,
-		       tcp_send4, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&ipv4_tcp_thread_data, ipv4_tcp_stack, STACKSIZE,
+			(k_thread_entry_t)send_tcp_ipv4,
+			tcp_send4, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 #endif
 
 #if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_UDP)
-	k_thread_spawn(ipv6_udp_stack, STACKSIZE,
-		       (k_thread_entry_t)send_udp_ipv6,
-		       udp_send6, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&ipv6_udp_thread_data, ipv6_udp_stack, STACKSIZE,
+			(k_thread_entry_t)send_udp_ipv6,
+			udp_send6, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 #endif
 
 #if defined(CONFIG_NET_IPV6) && defined(CONFIG_NET_TCP)
-	k_thread_spawn(ipv6_tcp_stack, STACKSIZE,
-		       (k_thread_entry_t)send_tcp_ipv6,
-		       tcp_send6, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&ipv6_tcp_thread_data, ipv6_tcp_stack, STACKSIZE,
+			(k_thread_entry_t)send_tcp_ipv6,
+			tcp_send6, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 #endif
 }
 

samples/bluetooth/hci_uart/src/main.c
@@ -30,6 +30,7 @@
 
 static struct device *hci_uart_dev;
 static BT_STACK_NOINIT(tx_thread_stack, CONFIG_BLUETOOTH_HCI_TX_STACK_SIZE);
+static struct k_thread tx_thread_data;
 
 /* HCI command buffers */
 #define CMD_BUF_SIZE BT_BUF_RX_SIZE
@@ -348,8 +349,9 @@ void main(void)
 	/* Spawn the TX thread and start feeding commands and data to the
 	 * controller
 	 */
-	k_thread_spawn(tx_thread_stack, sizeof(tx_thread_stack), tx_thread,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
+	k_thread_create(&tx_thread_data, tx_thread_stack,
+			sizeof(tx_thread_stack), tx_thread,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, K_NO_WAIT);
 
 	while (1) {
 		struct net_buf *buf;

samples/bluetooth/ipsp/src/main.c
@@ -36,6 +36,7 @@ static struct in6_addr in6addr_my = MY_IP6ADDR;
 
 #define STACKSIZE 2000
 char __noinit __stack thread_stack[STACKSIZE];
+static struct k_thread thread_data;
 
 #define MAX_DBG_PRINT 64
 
@@ -357,7 +358,7 @@ void main(void)
 
 	printk("Advertising successfully started\n");
 
-	k_thread_spawn(&thread_stack[0], STACKSIZE,
-		       (k_thread_entry_t)listen,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&thread_data, thread_stack, STACKSIZE,
+			(k_thread_entry_t)listen,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 }

samples/net/coaps_client/src/coaps_client.c
@@ -412,6 +412,7 @@ exit:
 
 #define STACK_SIZE		4096
 u8_t stack[STACK_SIZE];
+static struct k_thread thread_data;
 
 static inline int init_app(void)
 {
@@ -440,6 +441,7 @@ void main(void)
 		return;
 	}
 
-	k_thread_spawn(stack, STACK_SIZE, (k_thread_entry_t) dtls_client,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&thread_data, stack, STACK_SIZE,
+			(k_thread_entry_t) dtls_client,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 }

samples/net/coaps_server/src/coaps_server.c
@@ -633,6 +633,7 @@ exit:
 
 #define STACK_SIZE		4096
 u8_t stack[STACK_SIZE];
+static struct k_thread thread_data;
 
 static inline int init_app(void)
 {
@@ -660,7 +661,8 @@ void main(void)
 		return;
 	}
 
-	k_thread_spawn(stack, STACK_SIZE, (k_thread_entry_t) dtls_server,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&thread_data, stack, STACK_SIZE,
+			(k_thread_entry_t) dtls_server,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 
 }

samples/net/dhcpv4_client/src/main.c
@@ -25,6 +25,7 @@
 
 #define STACKSIZE 2000
 char __noinit __stack thread_stack[STACKSIZE];
+static struct k_thread thread_data;
 
 static struct net_mgmt_event_callback mgmt_cb;
 
@@ -78,7 +79,7 @@ void main(void)
 {
 	NET_INFO("In main");
 
-	k_thread_spawn(&thread_stack[0], STACKSIZE,
-		       (k_thread_entry_t)main_thread,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&thread_data, &thread_stack[0], STACKSIZE,
+			(k_thread_entry_t)main_thread,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 }

samples/net/dns_resolve/src/main.c
@@ -22,6 +22,7 @@
 
 #define STACKSIZE 2000
 char __noinit __stack thread_stack[STACKSIZE];
+static struct k_thread thread_data;
 
 #define DNS_TIMEOUT 2000 /* ms */
 
@@ -281,7 +282,7 @@ void main(void)
 {
 	NET_INFO("Starting DNS resolve sample");
 
-	k_thread_spawn(&thread_stack[0], STACKSIZE,
-		       (k_thread_entry_t)network_setup,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&thread_data, thread_stack, STACKSIZE,
+			(k_thread_entry_t)network_setup,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 }

samples/net/echo_server/src/echo-server.c
@@ -90,6 +90,7 @@ static struct net_buf_pool *data_udp_pool(void)
 
 #define STACKSIZE 2000
 char __noinit __stack thread_stack[STACKSIZE];
+static struct k_thread thread_data;
 
 #define MAX_DBG_PRINT 64
 
@@ -582,7 +583,7 @@ void main(void)
 	}
 #endif
 
-	k_thread_spawn(&thread_stack[0], STACKSIZE,
-		       (k_thread_entry_t)receive,
-		       NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
+	k_thread_create(&thread_data, thread_stack, STACKSIZE,
+			(k_thread_entry_t)receive,
+			NULL, NULL, NULL, K_PRIO_COOP(7), 0, 0);
 }

commit 82090ccf263553c4036f2f3f91702c155d4e8f90

samples: net: echo-client: Fix compile error
This commit fixes compile error caused by commit cd773bc2
"samples: use k_thread_create()"

Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

samples/net/echo_client/src/echo-client.c
@@ -164,7 +164,7 @@ static struct k_thread ipv6_udp_thread_data;
 #endif
 
 #if defined(CONFIG_NET_TCP)
-static K_THREAD_STACK_DEFINE(ipv6_tcp_stack, STACKSIZE)
+static K_THREAD_STACK_DEFINE(ipv6_tcp_stack, STACKSIZE);
 static struct k_thread ipv6_tcp_thread_data;
 #endif
 