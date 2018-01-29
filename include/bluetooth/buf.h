kartben/zephyr-without-images

commit a7fbf0ccbf8b4825f83f162b29ba3454deec86f0

bluetooth: Fix cast in bt_buf_get_type
The function returns an enum, not a u8_t, so we should cast
appropriately to avoid an implicit conversion.

This construct was triggering this compilation error when compiling
with CXX:

/home/sebo/zephyr/include/bluetooth/buf.h:85:9: error: invalid
  conversion from ‘u8_t {aka unsigned char}’ to ‘bt_buf_type’
  [-fpermissive] return *(u8_t *)net_buf_user_data(buf);

Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

include/bluetooth/buf.h
@@ -82,7 +82,7 @@ static inline void bt_buf_set_type(struct net_buf *buf, enum bt_buf_type type)
  */
 static inline enum bt_buf_type bt_buf_get_type(struct net_buf *buf)
 {
-	return *(u8_t *)net_buf_user_data(buf);
+	return *((enum bt_buf_type *)net_buf_user_data(buf));
 }
 
 /**

commit c12f42001d7921db3f4e4c87e7f9ce85bbf7ebf9

bt: Don't rely on 'enum bt_buf_type' being 1 byte
De-referencing the pointer from net_buf_user_data(buf) as a pointer to
an enum causes issues on qemu_x86 because the true size is 8-bit, but
the enum is 32-bit on qemu_x86. So we put in a temporary cast to 8-bit
to ensure only 8 bits are read from the pointer.

This fixes a regression from a7fbf0ccbf8b4825f83f162b29ba3454deec86f0
that broke BT on qemu_x86.

Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

include/bluetooth/buf.h
@@ -82,7 +82,13 @@ static inline void bt_buf_set_type(struct net_buf *buf, enum bt_buf_type type)
  */
 static inline enum bt_buf_type bt_buf_get_type(struct net_buf *buf)
 {
-	return *((enum bt_buf_type *)net_buf_user_data(buf));
+	/* De-referencing the pointer from net_buf_user_data(buf) as a
+	 * pointer to an enum causes issues on qemu_x86 because the true
+	 * size is 8-bit, but the enum is 32-bit on qemu_x86. So we put in
+	 * a temporary cast to 8-bit to ensure only 8 bits are read from
+	 * the pointer.
+	 */
+	return (enum bt_buf_type)(*(u8_t *)net_buf_user_data(buf));
 }
 
 /**