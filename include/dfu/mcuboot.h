ccollins476ad/zephyr

subsys: dfu: boot: mcuboot: Query current img ver
Allow a running image to query its own version number.

Signed-off-by: Christopher Collins <ccollins@apache.org>

include/dfu/mcuboot.h
@@ -28,6 +28,15 @@
 /** Swap failed because image to be run is not valid */
 #define BOOT_SWAP_TYPE_FAIL     5
 
+#define BOOT_IMG_VER_STRLEN_MAX 25  /* 255.255.65535.4294967295\0 */
+
+struct image_version {
+    uint8_t iv_major;
+    uint8_t iv_minor;
+    uint16_t iv_revision;
+    uint32_t iv_build_num;
+};
+
 /**
  * @brief MCUboot image header representation for image version
  *
@@ -136,6 +145,8 @@ bool boot_is_img_confirmed(void);
  * @return 0 on success, negative errno code on fail.
  */
 int boot_write_img_confirmed(void);
+
+int boot_current_image_version(struct image_version *out_ver);
 
 /**
  * @brief Determines the action, if any, that mcuboot will take on the next

kartben/zephyr-without-images

include/dfu/mcuboot.h
@@ -28,6 +28,15 @@
 /** Swap failed because image to be run is not valid */
 #define BOOT_SWAP_TYPE_FAIL     5
 
+#define BOOT_IMG_VER_STRLEN_MAX 25  /* 255.255.65535.4294967295\0 */
+
+struct image_version {
+	u8_t iv_major;
+	u8_t iv_minor;
+	u16_t iv_revision;
+	u32_t iv_build_num;
+};
+
 /**
  * @brief MCUboot image header representation for image version
  *
@@ -137,6 +146,8 @@ bool boot_is_img_confirmed(void);
  */
 int boot_write_img_confirmed(void);
 
+int boot_current_image_version(struct image_version *out_ver);
+
 /**
  * @brief Determines the action, if any, that mcuboot will take on the next
  * reboot.
@@ -165,4 +176,4 @@ int boot_request_upgrade(int permanent);
  */
 int boot_erase_img_bank(u32_t bank_offset);
 
-#endif	/* __MCUBOOT_H__ */
+#endif  /* __MCUBOOT_H__ */