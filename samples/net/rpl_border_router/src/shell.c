rveerama1/zephyr

samples: net: rpl: Enhance RPL border router application
Add CoAP client to RPL BR application. Which can be used to
control leds and get rpl related information from leaf nodes
though CoAP messages. Build toplogy matrics and send it to
Web UI.

Enhanced Web UI components (Interfaces, RPL, Neighbors, Routes
and Topology tabs). Added buttons to control leds on lead nodes.

Added JSON support to handle led on/off requests from UI.

Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>

samples/net/rpl_border_router/src/shell.c
@@ -32,8 +32,48 @@ int br_repair(int argc, char *argv[])
 	return 0;
 }
 
+int coap_send(int argc, char *argv[])
+{
+	struct in6_addr peer_addr;
+	enum coap_request_type type;
+	int r;
+
+	if (argc != 3 || !argv[1] || !argv[2]) {
+		NET_INFO("Invalid arguments");
+		return -EINVAL;
+	}
+
+	r = net_addr_pton(AF_INET6, argv[2], &peer_addr);
+	if (r < 0) {
+		return -EINVAL;
+	}
+
+	if (strcmp(argv[1], "led_on") == 0) {
+		type = COAP_REQ_LED_ON;
+	} else if (strcmp(argv[1], "led_off") == 0) {
+		type = COAP_REQ_LED_OFF;
+	} else if (strcmp(argv[1], "rpl_info") == 0) {
+		type = COAP_REQ_RPL_INFO;
+	} else if (strcmp(argv[1], "rpl_obs") == 0) {
+		type = COAP_REQ_RPL_OBS;
+	} else {
+		NET_INFO("Invalid arguments");
+		return -EINVAL;
+	}
+
+	coap_send_request(&peer_addr, type, NULL, NULL);
+
+	return 0;
+}
+
 static struct shell_cmd br_commands[] = {
 	/* Keep the commands in alphabetical order */
+	{ "coap", coap_send, "\n\tSend CoAP commands to Node\n"
+			"led_on <host>\n\tTurn on LED on host\n"
+			"led_off <host>\n\tTurn off LED on host\n"
+			"rpl_info <host>\n\tGet Node rpl info\n"
+			"rpl_obs <host>\n\tSet RPL observer on Node\n"
+	},
 	{ "repair", br_repair,
 		"\n\tGlobal repair RPL network" },
 	{ NULL, NULL, NULL }

kartben/zephyr-without-images

samples/net/rpl_border_router/src/shell.c
@@ -32,8 +32,42 @@ int br_repair(int argc, char *argv[])
 	return 0;
 }
 
+int coap_send(int argc, char *argv[])
+{
+	struct in6_addr peer_addr;
+	enum coap_request_type type;
+	int r;
+
+	if (argc != 3 || !argv[1] || !argv[2]) {
+		NET_INFO("Invalid arguments");
+		return -EINVAL;
+	}
+
+	r = net_addr_pton(AF_INET6, argv[2], &peer_addr);
+	if (r < 0) {
+		return -EINVAL;
+	}
+
+	if (strcmp(argv[1], "toggle") == 0) {
+		type = COAP_REQ_TOGGLE_LED;
+	} else if (strcmp(argv[1], "rpl_obs") == 0) {
+		type = COAP_REQ_RPL_OBS;
+	} else {
+		NET_INFO("Invalid arguments");
+		return -EINVAL;
+	}
+
+	coap_send_request(&peer_addr, type, NULL, NULL);
+
+	return 0;
+}
+
 static struct shell_cmd br_commands[] = {
 	/* Keep the commands in alphabetical order */
+	{ "coap", coap_send, "\n\tSend CoAP commands to Node\n"
+			"toggle <host>\n\tToggle the LED on Node\n"
+			"rpl_obs <host>\n\tSet RPL observer on Node\n"
+	},
 	{ "repair", br_repair,
 		"\n\tGlobal repair RPL network" },
 	{ NULL, NULL, NULL }