zephyrproject-rtos/zephyr

sam: adc: Updated SAM ADC driver.
Updated to work with new ADC API.

Signed-off-by: Justin Watson <jwatson5@gmail.com>

tests/drivers/adc/adc_api/src/test_adc.c
@@ -94,6 +94,16 @@
 #define ADC_1ST_CHANNEL_ID	1
 #define ADC_1ST_CHANNEL_INPUT	0
 
+#elif defined(CONFIG_BOARD_SAM_E70_XPLAINED)
+
+#define ADC_DEVICE_NAME		CONFIG_ADC_0_NAME
+#define ADC_RESOLUTION		12
+#define ADC_GAIN_1		ADC_GAIN_1
+#define ADC_REFERENCE		ADC_REF_EXTERNAL0
+#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
+#define ADC_1ST_CHANNEL_ID	0
+#define ADC_1ST_CHANNEL_INPUT	0
+
 #else
 #error "Unsupported board."
 #endif

kartben/zephyr-without-images

tests/drivers/adc/adc_api/src/test_adc.c
@@ -94,6 +94,16 @@
 #define ADC_1ST_CHANNEL_ID	1
 #define ADC_1ST_CHANNEL_INPUT	0
 
+#elif defined(CONFIG_BOARD_SAM_E70_XPLAINED)
+
+#define ADC_DEVICE_NAME		CONFIG_ADC_0_NAME
+#define ADC_RESOLUTION		12
+#define ADC_GAIN		ADC_GAIN_1
+#define ADC_REFERENCE		ADC_REF_EXTERNAL0
+#define ADC_ACQUISITION_TIME	ADC_ACQ_TIME_DEFAULT
+#define ADC_1ST_CHANNEL_ID	0
+#define ADC_1ST_CHANNEL_INPUT	0
+
 #else
 #error "Unsupported board."
 #endif