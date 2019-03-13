hillLang/zephyr

kernel/sched: Fix spinlock validation glitch in SMP
In SMP, we are setting the _current pointer while holding the
scheduler spinlock locally, which means that when we try to release it
the validation layer (not the spinlock per se) will scream at us
because the thread that took the lock doesn't match the one releasing
it.

Special case this when validation is enabled.

Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

kernel/sched.c
@@ -585,6 +585,14 @@ void *_get_next_switch_handle(void *interrupted)
 			reset_time_slice();
 			_current_cpu->swap_ok = 0;
 			set_current(th);
+#ifdef SPIN_VALIDATE
+			/* Changed _current!  Update the spinlock
+			 * bookeeping so the validation doesn't get
+			 * confused when the "wrong" thread tries to
+			 * release the lock.
+			 */
+			z_spin_lock_set_owner(&sched_lock);
+#endif
 		}
 	}
 #else

kartben/zephyr-without-images

kernel/sched.c
@@ -585,6 +585,14 @@ void *z_get_next_switch_handle(void *interrupted)
 			reset_time_slice();
 			_current_cpu->swap_ok = 0;
 			set_current(th);
+#ifdef SPIN_VALIDATE
+			/* Changed _current!  Update the spinlock
+			 * bookeeping so the validation doesn't get
+			 * confused when the "wrong" thread tries to
+			 * release the lock.
+			 */
+			z_spin_lock_set_owner(&sched_spinlock);
+#endif
 		}
 	}
 #else