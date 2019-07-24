zephyrproject-rtos/zephyr

soc: samr21: Get CPU clock frequency from DTS
The SoC initialization code used system clock frequency
as a CPU clock frequency. This commit corrects that by
obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arm/atmel_sam0/samr21/soc.h
@@ -34,7 +34,7 @@
 #endif /* _ASMLANGUAGE */
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ DT_CPUS_CPU_0_CLOCK_FREQUENCY
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
 #define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768

kartben/zephyr-without-images

soc/arm/atmel_sam0/samr21/soc.h
@@ -34,7 +34,7 @@
 #endif /* _ASMLANGUAGE */
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ DT_ARM_CORTEX_M0PLUS_0_CLOCK_FREQUENCY
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
 #define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768