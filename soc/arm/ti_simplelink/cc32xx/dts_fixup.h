zephyrproject-rtos/zephyr

drivers: i2c_cc32xx: Get clock frequency from DTS
The i2c_cc32xx driver used system clock frequency
as a base for I2C clock frequency calculation.
This commit corrects that by obtaining the needed value from DTS.

Please note, that for I2C devices the clock-frequency property
specifies SCK frequency, instead of frequency of the clock driving
peripheral. To solve that problem, a new property was added.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arm/ti_simplelink/cc32xx/dts_fixup.h
@@ -8,6 +8,7 @@
 #define DT_I2C_0_LABEL		DT_TI_CC32XX_I2C_40020000_LABEL
 #define DT_I2C_0_BASE_ADDRESS	DT_TI_CC32XX_I2C_40020000_BASE_ADDRESS
 #define DT_I2C_0_BITRATE		DT_TI_CC32XX_I2C_40020000_CLOCK_FREQUENCY
+#define DT_I2C_0_CLOCK_FREQUENCY	DT_TI_CC32XX_I2C_40020000_CC32XX_CLOCK_FREQUENCY
 #define DT_I2C_0_IRQ		DT_TI_CC32XX_I2C_40020000_IRQ_0
 #define DT_I2C_0_IRQ_PRIORITY	DT_TI_CC32XX_I2C_40020000_IRQ_0_PRIORITY
 

kartben/zephyr-without-images

soc/arm/ti_simplelink/cc32xx/dts_fixup.h
@@ -8,6 +8,7 @@
 #define DT_I2C_0_LABEL		DT_TI_CC32XX_I2C_40020000_LABEL
 #define DT_I2C_0_BASE_ADDRESS	DT_TI_CC32XX_I2C_40020000_BASE_ADDRESS
 #define DT_I2C_0_BITRATE		DT_TI_CC32XX_I2C_40020000_CLOCK_FREQUENCY
+#define DT_I2C_0_CLOCK_FREQUENCY	DT_TI_CC32XX_I2C_40020000_CLOCKS_CLOCK_FREQUENCY
 #define DT_I2C_0_IRQ		DT_TI_CC32XX_I2C_40020000_IRQ_0
 #define DT_I2C_0_IRQ_PRIORITY	DT_TI_CC32XX_I2C_40020000_IRQ_0_PRIORITY
 