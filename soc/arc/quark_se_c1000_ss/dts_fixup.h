zephyrproject-rtos/zephyr

drivers: spi_dw: Get clock frequency from DTS
The spi_dw driver used system clock frequency
as a base for SPI bus frequency calculation.
This commit corrects that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arc/quark_se_c1000_ss/dts_fixup.h
@@ -77,6 +77,7 @@
 #define DT_ADC_0_BASE_ADDRESS	DT_SNPS_DW_ADC_80015000_BASE_ADDRESS
 
 #define DT_SPI_0_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_80010000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_80010000_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME		DT_SNPS_DESIGNWARE_SPI_80010000_LABEL
 #define DT_SPI_0_IRQ_ERR_INT	DT_SNPS_DESIGNWARE_SPI_80010000_IRQ_ERR_INT
 #define DT_SPI_0_IRQ_ERR_INT_PRI	DT_SNPS_DESIGNWARE_SPI_80010000_IRQ_ERR_INT_PRIORITY
@@ -86,6 +87,7 @@
 #define DT_SPI_0_IRQ_TX_REQ_PRI	DT_SNPS_DESIGNWARE_SPI_80010000_IRQ_TX_REQ_PRIORITY
 
 #define DT_SPI_1_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_80010100_BASE_ADDRESS
+#define DT_SPI_1_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_80010100_CLOCK_FREQUENCY
 #define DT_SPI_1_NAME		DT_SNPS_DESIGNWARE_SPI_80010100_LABEL
 #define DT_SPI_1_IRQ_ERR_INT	DT_SNPS_DESIGNWARE_SPI_80010100_IRQ_ERR_INT
 #define DT_SPI_1_IRQ_ERR_INT_PRI	DT_SNPS_DESIGNWARE_SPI_80010100_IRQ_ERR_INT_PRIORITY

soc/arc/snps_emsk/dts_fixup.h
@@ -69,18 +69,17 @@
  */
 
 #define DT_SPI_0_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_F0006000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_F0006000_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME		DT_SNPS_DESIGNWARE_SPI_F0006000_LABEL
 #define DT_SPI_0_IRQ		DT_SNPS_DESIGNWARE_SPI_F0006000_IRQ_0
 #define DT_SPI_0_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_F0006000_IRQ_0_PRIORITY
 
 #define DT_SPI_1_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_F0007000_BASE_ADDRESS
+#define DT_SPI_1_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_F0007000_CLOCK_FREQUENCY
 #define DT_SPI_1_NAME		DT_SNPS_DESIGNWARE_SPI_F0007000_LABEL
 #define DT_SPI_1_IRQ		DT_SNPS_DESIGNWARE_SPI_F0007000_IRQ_0
 #define DT_SPI_1_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_F0007000_IRQ_0_PRIORITY
 
 #define DT_SPI_DW_IRQ_FLAGS	0
-
-#define DT_SPI_DW_SPI_CLOCK	DT_NS16550_F0009000_CLOCK_FREQUENCY
-
 
 /* End of SoC Level DTS fixup file */

soc/x86/intel_quark/quark_se/dts_fixup.h
@@ -50,16 +50,19 @@
 #define DT_RTC_0_IRQ_FLAGS		DT_INTEL_QMSI_RTC_B0000400_IRQ_0_SENSE
 
 #define DT_SPI_0_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_B0001000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_B0001000_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME		DT_SNPS_DESIGNWARE_SPI_B0001000_LABEL
 #define DT_SPI_0_IRQ		DT_SNPS_DESIGNWARE_SPI_B0001000_IRQ_0
 #define DT_SPI_0_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_B0001000_IRQ_0_PRIORITY
 
 #define DT_SPI_1_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_B0001400_BASE_ADDRESS
+#define DT_SPI_1_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_B0001400_CLOCK_FREQUENCY
 #define DT_SPI_1_NAME		DT_SNPS_DESIGNWARE_SPI_B0001400_LABEL
 #define DT_SPI_1_IRQ		DT_SNPS_DESIGNWARE_SPI_B0001400_IRQ_0
 #define DT_SPI_1_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_B0001400_IRQ_0_PRIORITY
 
 #define DT_SPI_2_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_B0001800_BASE_ADDRESS
+#define DT_SPI_2_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_B0001800_CLOCK_FREQUENCY
 #define DT_SPI_2_NAME		DT_SNPS_DESIGNWARE_SPI_B0001800_LABEL
 #define DT_SPI_2_IRQ		DT_SNPS_DESIGNWARE_SPI_B0001800_IRQ_0
 #define DT_SPI_2_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_B0001800_IRQ_0_PRIORITY

soc/xtensa/intel_s1000/dts_fixup.h
@@ -37,6 +37,7 @@
 #define DT_DW_ICTL_IRQ_FLAGS			DT_SNPS_DESIGNWARE_INTC_81800_IRQ_0_SENSE
 
 #define DT_SPI_0_BASE_ADDRESS			DT_SNPS_DESIGNWARE_SPI_E000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY		DT_SNPS_DESIGNWARE_SPI_E000_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME			DT_SNPS_DESIGNWARE_SPI_E000_LABEL
 #define DT_SPI_0_IRQ				DT_SNPS_DESIGNWARE_SPI_E000_IRQ_0
 #define DT_SPI_DW_IRQ_FLAGS			DT_SNPS_DESIGNWARE_SPI_E000_IRQ_0_SENSE

kartben/zephyr-without-images

soc/arc/quark_se_c1000_ss/dts_fixup.h
@@ -77,6 +77,7 @@
 #define DT_ADC_0_BASE_ADDRESS	DT_SNPS_DW_ADC_80015000_BASE_ADDRESS
 
 #define DT_SPI_0_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_80010000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_80010000_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME		DT_SNPS_DESIGNWARE_SPI_80010000_LABEL
 #define DT_SPI_0_IRQ_ERR_INT	DT_SNPS_DESIGNWARE_SPI_80010000_IRQ_ERR_INT
 #define DT_SPI_0_IRQ_ERR_INT_PRI	DT_SNPS_DESIGNWARE_SPI_80010000_IRQ_ERR_INT_PRIORITY
@@ -86,6 +87,7 @@
 #define DT_SPI_0_IRQ_TX_REQ_PRI	DT_SNPS_DESIGNWARE_SPI_80010000_IRQ_TX_REQ_PRIORITY
 
 #define DT_SPI_1_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_80010100_BASE_ADDRESS
+#define DT_SPI_1_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_80010100_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_1_NAME		DT_SNPS_DESIGNWARE_SPI_80010100_LABEL
 #define DT_SPI_1_IRQ_ERR_INT	DT_SNPS_DESIGNWARE_SPI_80010100_IRQ_ERR_INT
 #define DT_SPI_1_IRQ_ERR_INT_PRI	DT_SNPS_DESIGNWARE_SPI_80010100_IRQ_ERR_INT_PRIORITY

soc/arc/snps_emsk/dts_fixup.h
@@ -69,18 +69,17 @@
  */
 
 #define DT_SPI_0_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_F0006000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_F0006000_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME		DT_SNPS_DESIGNWARE_SPI_F0006000_LABEL
 #define DT_SPI_0_IRQ		DT_SNPS_DESIGNWARE_SPI_F0006000_IRQ_0
 #define DT_SPI_0_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_F0006000_IRQ_0_PRIORITY
 
 #define DT_SPI_1_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_F0007000_BASE_ADDRESS
+#define DT_SPI_1_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_F0007000_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_1_NAME		DT_SNPS_DESIGNWARE_SPI_F0007000_LABEL
 #define DT_SPI_1_IRQ		DT_SNPS_DESIGNWARE_SPI_F0007000_IRQ_0
 #define DT_SPI_1_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_F0007000_IRQ_0_PRIORITY
 
 #define DT_SPI_DW_IRQ_FLAGS	0
-
-#define DT_SPI_DW_SPI_CLOCK	DT_NS16550_F0009000_CLOCK_FREQUENCY
-
 
 /* End of SoC Level DTS fixup file */

soc/x86/intel_quark/quark_se/dts_fixup.h
@@ -50,16 +50,19 @@
 #define DT_RTC_0_IRQ_FLAGS		DT_INTEL_QMSI_RTC_B0000400_IRQ_0_SENSE
 
 #define DT_SPI_0_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_B0001000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_B0001000_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME		DT_SNPS_DESIGNWARE_SPI_B0001000_LABEL
 #define DT_SPI_0_IRQ		DT_SNPS_DESIGNWARE_SPI_B0001000_IRQ_0
 #define DT_SPI_0_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_B0001000_IRQ_0_PRIORITY
 
 #define DT_SPI_1_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_B0001400_BASE_ADDRESS
+#define DT_SPI_1_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_B0001400_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_1_NAME		DT_SNPS_DESIGNWARE_SPI_B0001400_LABEL
 #define DT_SPI_1_IRQ		DT_SNPS_DESIGNWARE_SPI_B0001400_IRQ_0
 #define DT_SPI_1_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_B0001400_IRQ_0_PRIORITY
 
 #define DT_SPI_2_BASE_ADDRESS	DT_SNPS_DESIGNWARE_SPI_B0001800_BASE_ADDRESS
+#define DT_SPI_2_CLOCK_FREQUENCY DT_SNPS_DESIGNWARE_SPI_B0001800_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_2_NAME		DT_SNPS_DESIGNWARE_SPI_B0001800_LABEL
 #define DT_SPI_2_IRQ		DT_SNPS_DESIGNWARE_SPI_B0001800_IRQ_0
 #define DT_SPI_2_IRQ_PRI		DT_SNPS_DESIGNWARE_SPI_B0001800_IRQ_0_PRIORITY

soc/xtensa/intel_s1000/dts_fixup.h
@@ -42,6 +42,7 @@
 #define DT_DW_ICTL_IRQ_FLAGS			DT_SNPS_DESIGNWARE_INTC_81800_IRQ_0_SENSE
 
 #define DT_SPI_0_BASE_ADDRESS			DT_SNPS_DESIGNWARE_SPI_E000_BASE_ADDRESS
+#define DT_SPI_0_CLOCK_FREQUENCY		DT_SNPS_DESIGNWARE_SPI_E000_CLOCKS_CLOCK_FREQUENCY
 #define DT_SPI_0_NAME			DT_SNPS_DESIGNWARE_SPI_E000_LABEL
 #define DT_SPI_0_IRQ				DT_SNPS_DESIGNWARE_SPI_E000_IRQ_0
 #define DT_SPI_DW_IRQ_FLAGS			DT_SNPS_DESIGNWARE_SPI_E000_IRQ_0_SENSE