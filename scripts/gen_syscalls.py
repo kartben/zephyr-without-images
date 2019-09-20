kartben/zephyr-without-images

commit 891f389ae52f0ad1b75f490323e07f21c887e115

userspace: Support for split 64 bit arguments
System call arguments, at the arch layer, are single words.  So
passing wider values requires splitting them into two registers at
call time.  This gets even more complicated for values (e.g
k_timeout_t) that may have different sizes depending on configuration.
This patch adds a feature to gen_syscalls.py to detect functions with
wide arguments and automatically generates code to split/unsplit them.

Unfortunately the current scheme of Z_SYSCALL_DECLARE_* macros won't
work with functions like this, because for N arguments (our current
maximum N is 10) there are 2^N possible configurations of argument
widths.  So this generates the complete functions for each handler and
wrapper, effectively doing in python what was originally done in the
preprocessor.

Another complexity is that traditional the z_hdlr_*() function for a
system call has taken the raw list of word arguments, which does not
work when some of those arguments must be 64 bit types.  So instead of
using a single Z_SYSCALL_HANDLER macro, this splits the job of
z_hdlr_*() into two steps: An automatically-generated unmarshalling
function, z_mrsh_*(), which then calls a user-supplied verification
function z_vrfy_*().  The verification function is typesafe, and is a
simple C function with exactly the same argument and return signature
as the syscall impl function.  It is also not responsible for
validating the pointers to the extra parameter array or a wide return
value, that code gets automatically generated.

This commit includes new vrfy/msrh handling for all syscalls invoked
during CI runs.  Future commits will port the less testable code.

Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

scripts/gen_syscalls.py
@@ -29,6 +29,20 @@ import argparse
 import os
 import json
 
+types64 = ["s64_t", "u64_t"]
+
+# The kernel linkage is complicated.  These functions from
+# userspace_handlers.c are present in the kernel .a library after
+# userspace.c, which contains the weak fallbacks defined here.  So the
+# linker finds the weak one first and stops searching, and thus won't
+# see the real implementation which should override.  Yet changing the
+# order runs afoul of a comment in CMakeLists.txt that the order is
+# critical.  These are core syscalls that won't ever be unconfigured,
+# just disable the fallback mechanism as a simple workaround.
+noweak = set(["z_mrsh_k_object_release",
+              "z_mrsh_k_object_access_grant",
+              "z_mrsh_k_object_alloc"])
+
 table_template = """/* auto-generated by gen_syscalls.py, don't edit */
 
 /* Weak handler functions that get replaced by the real ones unless a system
@@ -52,16 +66,6 @@ list_template = """
 
 #include <zephyr/types.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-%s
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif /* _ASMLANGUAGE */
 
 #endif /* ZEPHYR_SYSCALL_LIST_H */
@@ -69,11 +73,15 @@ extern "C" {
 
 syscall_template = """
 /* auto-generated by gen_syscalls.py, don't edit */
+%s
 
 #ifndef _ASMLANGUAGE
 
 #include <syscall_list.h>
 #include <syscall_macros.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
 
 #ifdef __cplusplus
 extern "C" {
@@ -85,11 +93,14 @@ extern "C" {
 }
 #endif
 
+#pragma GCC diagnostic pop
+
 #endif
+#endif /* include guard */
 """
 
 handler_template = """
-extern u32_t %s(u32_t arg1, u32_t arg2, u32_t arg3,
+extern u32_t z_hdlr_%s(u32_t arg1, u32_t arg2, u32_t arg3,
                 u32_t arg4, u32_t arg5, u32_t arg6, void *ssf);
 """
 
@@ -124,6 +135,164 @@ def typename_split(item):
     m = mo.groups()
     return (m[0].strip(), m[1])
 
+def need_split(argtype):
+    return argtype in types64
+
+# Note: "lo" and "hi" are named in little endian conventions,
+# but it doesn't matter as long as they are consistently
+# generated.
+def union_decl(type):
+    return "union { struct { u32_t lo, hi; } split; %s val; }" % type
+
+def wrapper_defs(func_name, func_type, args):
+    ret64 = func_type in types64
+    mrsh_args = [] # List of rvalue expressions for the marshalled invocation
+    split_args = []
+    nsplit = 0
+    for i, argrec in enumerate(args):
+        (argtype, argname) = argrec
+        if need_split(argtype):
+            split_args.append((argtype, argname))
+            mrsh_args.append("parm%d.split.lo" % nsplit)
+            mrsh_args.append("parm%d.split.hi" % nsplit)
+            nsplit += 1
+        else:
+            mrsh_args.append("*(u32_t *)&" + argname)
+
+    if ret64:
+        mrsh_args.append("(u32_t)&ret64")
+
+    decl_arglist = ", ".join([" ".join(argrec) for argrec in args])
+
+    wrap = "extern %s z_impl_%s(%s);\n" % (func_type, func_name, decl_arglist)
+    wrap += "static inline %s %s(%s)\n" % (func_type, func_name, decl_arglist)
+    wrap += "{\n"
+    wrap += "#ifdef CONFIG_USERSPACE\n"
+    wrap += ("\t" + "u64_t ret64;\n") if ret64 else ""
+    wrap += "\t" + "if (z_syscall_trap()) {\n"
+
+    for parmnum, rec in enumerate(split_args):
+        (argtype, argname) = rec
+        wrap += "\t\t%s parm%d;\n" % (union_decl(argtype), parmnum)
+        wrap += "\t\t" + "parm%d.val = %s;\n" % (parmnum, argname)
+
+    if len(mrsh_args) > 6:
+        wrap += "\t\t" + "u32_t more[] = {\n"
+        wrap += "\t\t\t" + (",\n\t\t\t".join(mrsh_args[5:])) + "\n"
+        wrap += "\t\t" + "};\n"
+        mrsh_args[5:] = ["(u32_t) &more"]
+
+    syscall_id = "K_SYSCALL_" + func_name.upper()
+    invoke = ("z_arch_syscall_invoke%d(%s)"
+              % (len(mrsh_args),
+                 ", ".join(mrsh_args + [syscall_id])))
+
+    if ret64:
+        wrap += "\t\t" + "(void)%s;\n" % invoke
+        wrap += "\t\t" + "return (%s)ret64;\n" % func_type
+    elif func_type == "void":
+        wrap += "\t\t" + "%s;\n" % invoke
+        wrap += "\t\t" + "return;\n";
+    else:
+        wrap += "\t\t" + "return (%s) %s;\n" % (func_type, invoke)
+
+    wrap += "\t" + "}\n"
+    wrap += "#endif\n"
+
+    # Otherwise fall through to direct invocation of the impl func.
+    # Note the compiler barrier: that is required to prevent code from
+    # the impl call from being hoisted above the check for user
+    # context.
+    impl_arglist = ", ".join([argrec[1] for argrec in args])
+    impl_call = "z_impl_%s(%s)" % (func_name, impl_arglist)
+    wrap += "\t" + "compiler_barrier();\n"
+    wrap += "\t" + "%s%s;\n" % ("return " if func_type != "void" else "",
+                               impl_call)
+
+    wrap += "}\n"
+
+    return wrap
+
+# Returns an expression for the specified (zero-indexed!) marshalled
+# parameter to a syscall, with handling for a final "more" parameter.
+def mrsh_rval(mrsh_num, total):
+    if mrsh_num < 5 or total <= 6:
+        return "arg%d" % mrsh_num
+    else:
+        return "(((u32_t *)more)[%d])" % (mrsh_num - 5)
+
+def marshall_defs(func_name, func_type, args):
+    mrsh_name = "z_mrsh_" + func_name
+
+    nmrsh = 0        # number of marshalled u32_t parameter
+    vrfy_parms = []  # list of (arg_num, mrsh_or_parm_num, bool_is_split)
+    split_parms = [] # list of a (arg_num, mrsh_num) for each split
+    for i, argrec in enumerate(args):
+        (argtype, argname) = argrec
+        if need_split(argtype):
+            vrfy_parms.append((i, len(split_parms), True))
+            split_parms.append((i, nmrsh))
+            nmrsh += 2
+        else:
+            vrfy_parms.append((i, nmrsh, False))
+            nmrsh += 1
+
+    # Final argument for a 64 bit return value?
+    if func_type in types64:
+        nmrsh += 1
+
+    decl_arglist = ", ".join([" ".join(argrec) for argrec in args])
+    mrsh = "extern %s z_vrfy_%s(%s);\n" % (func_type, func_name, decl_arglist)
+
+    mrsh += "u32_t %s(u32_t arg0, u32_t arg1, u32_t arg2,\n" % mrsh_name
+    if nmrsh <= 6:
+        mrsh += "\t\t" + "u32_t arg3, u32_t arg4, u32_t arg5, void *ssf)\n";
+    else:
+        mrsh += "\t\t" + "u32_t arg3, u32_t arg4, void *more, void *ssf)\n";
+    mrsh += "{\n"
+    mrsh += "\t" + "_current_cpu->syscall_frame = ssf;\n";
+
+    for unused_arg in range(nmrsh, 6):
+        mrsh += "\t(void) arg%d;\t/* unused */\n" % unused_arg
+
+    if nmrsh > 6:
+        mrsh += ("\tZ_OOPS(Z_SYSCALL_MEMORY_READ(more, "
+                 + str(nmrsh - 6) + " * sizeof(u32_t)));\n")
+
+    for i, split_rec in enumerate(split_parms):
+        arg_num, mrsh_num = split_rec
+        arg_type = args[arg_num][0];
+        mrsh += "\t%s parm%d;\n" % (union_decl(arg_type), i);
+        mrsh += "\t" + "parm%d.split.lo = %s;\n" % (i, mrsh_rval(mrsh_num,
+                                                                 nmrsh))
+        mrsh += "\t" + "parm%d.split.hi = %s;\n" % (i, mrsh_rval(mrsh_num + 1,
+                                                                 nmrsh))
+    # Finally, invoke the verify function
+    out_args = []
+    for i, argn, is_split in vrfy_parms:
+        if is_split:
+            out_args.append("parm%d.val" % argn)
+        else:
+            out_args.append("*(%s*)&%s" % (args[i][0], mrsh_rval(argn, nmrsh)))
+
+    vrfy_call = "z_vrfy_%s(%s)\n" % (func_name, ", ".join(out_args))
+
+    if func_type == "void":
+        mrsh += "\t" + "%s;\n" % vrfy_call
+        mrsh += "\t" + "return 0;\n"
+    else:
+        mrsh += "\t" + "%s ret = %s;\n" % (func_type, vrfy_call)
+        if func_type in types64:
+            ptr = "((u64_t *)%s)" % mrsh_rval(nmrsh - 1, nmrsh)
+            mrsh += "\t" + "Z_OOPS(Z_SYSCALL_MEMORY_WRITE(%s, 8));\n" % ptr
+            mrsh += "\t" + "*%s = ret;\n" % ptr
+            mrsh += "\t" + "return 0;\n"
+        else:
+            mrsh += "\t" + "return (u32_t) ret;\n"
+
+    mrsh += "}\n"
+
+    return mrsh, mrsh_name
 
 def analyze_fn(match_group):
     func, args = match_group
@@ -141,39 +310,14 @@ def analyze_fn(match_group):
 
     sys_id = "K_SYSCALL_" + func_name.upper()
 
-    if func_type == "void":
-        suffix = "_VOID"
-        is_void = True
-    else:
-        is_void = False
-        if func_type in ["s64_t", "u64_t"]:
-            suffix = "_RET64"
-        else:
-            suffix = ""
-
-    is_void = (func_type == "void")
-
-    # Get the proper system call macro invocation, which depends on the
-    # number of arguments, the return type, and whether the implementation
-    # is an inline function
-    macro = "K_SYSCALL_DECLARE%d%s" % (len(args), suffix)
-
-    # Flatten the argument lists and generate a comma separated list
-    # of t0, p0, t1, p1, ... tN, pN as expected by the macros
-    flat_args = [i for sublist in args for i in sublist]
-    if not is_void:
-        flat_args = [func_type] + flat_args
-    flat_args = [sys_id, func_name] + flat_args
-    argslist = ", ".join(flat_args)
-
-    invocation = "%s(%s)" % (macro, argslist)
-
-    handler = "z_hdlr_" + func_name
+    marshaller = None
+    marshaller, handler = marshall_defs(func_name, func_type, args)
+    invocation = wrapper_defs(func_name, func_type, args)
 
     # Entry in _k_syscall_table
     table_entry = "[%s] = %s" % (sys_id, handler)
 
-    return (handler, invocation, sys_id, table_entry)
+    return (handler, invocation, marshaller, sys_id, table_entry)
 
 def parse_args():
     global args
@@ -189,22 +333,30 @@ def parse_args():
                         help="output C system call list header")
     parser.add_argument("-o", "--base-output", required=True,
                         help="Base output directory for syscall macro headers")
+    parser.add_argument("-s", "--split-type", action="append",
+                        help="A long type that must be split/marshalled")
     args = parser.parse_args()
 
 
 def main():
     parse_args()
 
+    if args.split_type != None:
+        for t in args.split_type:
+            types64.append(t)
+
     with open(args.json_file, 'r') as fd:
         syscalls = json.load(fd)
 
     invocations = {}
+    mrsh_defs = {}
+    mrsh_includes = {}
     ids = []
     table_entries = []
     handlers = []
 
     for match_group, fn in syscalls:
-        handler, inv, sys_id, entry = analyze_fn(match_group)
+        handler, inv, mrsh, sys_id, entry = analyze_fn(match_group)
 
         if fn not in invocations:
             invocations[fn] = []
@@ -214,12 +366,24 @@ def main():
         table_entries.append(entry)
         handlers.append(handler)
 
+        if mrsh:
+            syscall = typename_split(match_group[0])[1]
+            mrsh_defs[syscall] = mrsh
+            mrsh_includes[syscall] = "#include <syscalls/%s>" % fn
+
     with open(args.syscall_dispatch, "w") as fp:
         table_entries.append("[K_SYSCALL_BAD] = handler_bad_syscall")
 
-        weak_defines = "".join([weak_template % name for name in handlers])
+        weak_defines = "".join([weak_template % name
+                                for name in handlers
+                                if not name in noweak])
 
-        fp.write(table_template % (weak_defines, ",\n\t".join(table_entries)))
+        # The "noweak" ones just get a regular declaration
+        weak_defines += "\n".join(["extern u32_t %s(u32_t arg1, u32_t arg2, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf);"
+                                   % s for s in noweak])
+
+        fp.write(table_template % (weak_defines,
+                                   ",\n\t".join(table_entries)))
 
     # Listing header emitted to stdout
     ids.sort()
@@ -229,19 +393,32 @@ def main():
     for i, item in enumerate(ids):
         ids_as_defines += "#define {} {}\n".format(item, i)
 
-    handler_defines = "".join([handler_template % name for name in handlers])
     with open(args.syscall_list, "w") as fp:
-        fp.write(list_template % (ids_as_defines, handler_defines))
+        fp.write(list_template % ids_as_defines)
 
     os.makedirs(args.base_output, exist_ok=True)
     for fn, invo_list in invocations.items():
         out_fn = os.path.join(args.base_output, fn)
 
-        header = syscall_template % "\n\n".join(invo_list)
+        ig = re.sub("[^a-zA-Z0-9]", "_", "Z_INCLUDE_SYSCALLS_" + fn).upper()
+        include_guard = "#ifndef %s\n#define %s\n" % (ig, ig)
+        header = syscall_template % (include_guard, "\n\n".join(invo_list))
 
         with open(out_fn, "w") as fp:
             fp.write(header)
 
+    # Likewise emit _mrsh.c files for syscall inclusion
+    for fn in mrsh_defs:
+        mrsh_fn = os.path.join(args.base_output, fn + "_mrsh.c")
+
+        with open(mrsh_fn, "w") as fp:
+            fp.write("/* auto-generated by gen_syscalls.py, don't edit */\n")
+            fp.write("#pragma GCC diagnostic push\n")
+            fp.write("#pragma GCC diagnostic ignored \"-Wstrict-aliasing\"\n")
+            fp.write(mrsh_includes[fn] + "\n")
+            fp.write("\n")
+            fp.write(mrsh_defs[fn] + "\n")
+            fp.write("#pragma GCC diagnostic pop\n")
 
 if __name__ == "__main__":
     main()

drivers/ptp_clock/ptp_clock.c
@@ -8,7 +8,8 @@
 #include <ptp_clock.h>
 
 #ifdef CONFIG_USERSPACE
-Z_SYSCALL_HANDLER(ptp_clock_get, dev, tm)
+int z_vrfy_ptp_clock_get(struct device *dev,
+			 struct net_ptp_time *tm)
 {
 	struct net_ptp_time ptp_time;
 	int ret;
@@ -25,6 +26,7 @@ Z_SYSCALL_HANDLER(ptp_clock_get, dev, tm)
 		return 0;
 	}
 
-	return (u32_t)ret;
+	return ret;
 }
+#include <syscalls/ptp_clock_get_mrsh.c>
 #endif /* CONFIG_USERSPACE */

include/syscall.h
@@ -34,44 +34,17 @@ extern "C" {
  * - Mixed or indeterminate code, these inlines will do a runtime check
  *   to determine what course of action is needed.
  *
- * All system calls require a handler function and an implementation function.
- * These must follow a naming convention. For a system call named k_foo():
+ * All system calls require a verifier function and an implementation
+ * function.  These must follow a naming convention. For a system call
+ * named k_foo():
  *
- * - The handler function will be named z_hdlr_k_foo(). Handler functions
- *   are always of type _k_syscall_handler_t, verify arguments passed up
- *   from userspace, and call the implementation function. See
- *   documentation for that typedef for more information.
- * - The implementation function will be named z_impl_k_foo(). This is the
- *   actual implementation of the system call.
- *
- * The basic declartion macros are as follows. System calls with 0 to 10
- * parameters are supported. For a system call with N parameters, that returns
- * a value and is* not implemented inline, the macro is as follows (N noted
- * as {N} for clarity):
- *
- * K_SYSCALL_DECLARE{N}(id, name, ret, t0, p0, ... , t{N-1}, p{N-1})
-
- * @param id System call ID, one of K_SYSCALL_* defines
- * @param name Symbol name of the system call used to invoke it
- * @param ret Data type of return value
- * @param tX Data type of parameter X
- * @param pX Name of parameter x
- *
- * For system calls that return no value:
- *
- * K_SYSCALL_DECLARE{n}_VOID(id, name, t0, p0, .... , t{N-1}, p{N-1})
- *
- * This is identical to above except there is no 'ret' parameter.
- *
- * For system calls where the implementation is an inline function, we have
- *
- * K_SYSCALL_DECLARE{n}_INLINE(id, name, ret, t0, p0, ... , t{N-1}, p{N-1})
- * K_SYSCALL_DECLARE{n}_VOID_INLINE(id, name, t0, p0, ... , t{N-1}, p{N-1})
- *
- * These are used in the same way as their non-INLINE counterparts.
- *
- * These macros are generated by scripts/gen_syscall_header.py and can be
- * found in $OUTDIR/include/generated/syscall_macros.h
+ * - The handler function will be named z_vrfy_k_foo(). Handler
+ *   functions have the same type signature as the wrapped call,
+ *   verify arguments passed up from userspace, and call the
+ *   implementation function. See documentation for that typedef for
+ *   more information.  - The implementation function will be named
+ *   z_impl_k_foo(). This is the actual implementation of the system
+ *   call.
  */
 
 /**
@@ -116,36 +89,6 @@ typedef u32_t (*_k_syscall_handler_t)(u32_t arg1, u32_t arg2, u32_t arg3,
 #ifdef CONFIG_USERSPACE
 
 /*
- * Helper data structures for system calls with large argument lists
- */
-
-struct _syscall_7_args {
-	u32_t arg6;
-	u32_t arg7;
-};
-
-struct _syscall_8_args {
-	u32_t arg6;
-	u32_t arg7;
-	u32_t arg8;
-};
-
-struct _syscall_9_args {
-	u32_t arg6;
-	u32_t arg7;
-	u32_t arg8;
-	u32_t arg9;
-};
-
-struct _syscall_10_args {
-	u32_t arg6;
-	u32_t arg7;
-	u32_t arg8;
-	u32_t arg9;
-	u32_t arg10;
-};
-
-/*
  * Interfaces for invoking system calls
  */
 
@@ -170,98 +113,36 @@ static inline u32_t z_arch_syscall_invoke6(u32_t arg1, u32_t arg2, u32_t arg3,
 					  u32_t arg4, u32_t arg5, u32_t arg6,
 					  u32_t call_id);
 
-static inline u32_t z_syscall_invoke7(u32_t arg1, u32_t arg2, u32_t arg3,
-				    u32_t arg4, u32_t arg5, u32_t arg6,
-				    u32_t arg7, u32_t call_id) {
-	struct _syscall_7_args args = {
-		.arg6 = arg6,
-		.arg7 = arg7,
-	};
-
-	return z_arch_syscall_invoke6(arg1, arg2, arg3, arg4, arg5, (u32_t)&args,
-				     call_id);
-}
-
-static inline u32_t z_syscall_invoke8(u32_t arg1, u32_t arg2, u32_t arg3,
-				    u32_t arg4, u32_t arg5, u32_t arg6,
-				    u32_t arg7, u32_t arg8, u32_t call_id)
-{
-	struct _syscall_8_args args = {
-		.arg6 = arg6,
-		.arg7 = arg7,
-		.arg8 = arg8,
-	};
-
-	return z_arch_syscall_invoke6(arg1, arg2, arg3, arg4, arg5, (u32_t)&args,
-				     call_id);
-}
-
-static inline u32_t z_syscall_invoke9(u32_t arg1, u32_t arg2, u32_t arg3,
-				    u32_t arg4, u32_t arg5, u32_t arg6,
-				    u32_t arg7, u32_t arg8, u32_t arg9,
-				    u32_t call_id)
-{
-	struct _syscall_9_args args = {
-		.arg6 = arg6,
-		.arg7 = arg7,
-		.arg8 = arg8,
-		.arg9 = arg9,
-	};
-
-	return z_arch_syscall_invoke6(arg1, arg2, arg3, arg4, arg5, (u32_t)&args,
-				     call_id);
-}
-
-static inline u32_t z_syscall_invoke10(u32_t arg1, u32_t arg2, u32_t arg3,
-				     u32_t arg4, u32_t arg5, u32_t arg6,
-				     u32_t arg7, u32_t arg8, u32_t arg9,
-				     u32_t arg10, u32_t call_id)
-{
-	struct _syscall_10_args args = {
-		.arg6 = arg6,
-		.arg7 = arg7,
-		.arg8 = arg8,
-		.arg9 = arg9,
-		.arg10 = arg10
-	};
-
-	return z_arch_syscall_invoke6(arg1, arg2, arg3, arg4, arg5, (u32_t)&args,
-				     call_id);
-}
-
-static inline u64_t z_syscall_ret64_invoke0(u32_t call_id)
-{
-	u64_t ret;
-
-	(void)z_arch_syscall_invoke1((u32_t)&ret, call_id);
-	return ret;
-}
-
-static inline u64_t z_syscall_ret64_invoke1(u32_t arg1, u32_t call_id)
-{
-	u64_t ret;
-
-	(void)z_arch_syscall_invoke2(arg1, (u32_t)&ret, call_id);
-	return ret;
-}
-
-static inline u64_t z_syscall_ret64_invoke2(u32_t arg1, u32_t arg2,
-					   u32_t call_id)
-{
-	u64_t ret;
-
-	(void)z_arch_syscall_invoke3(arg1, arg2, (u32_t)&ret, call_id);
-	return ret;
-}
+#endif /* CONFIG_USERSPACE */
 
 /**
  * Indicate whether we are currently running in user mode
  *
  * @return true if the CPU is currently running with user permissions
  */
+#ifdef CONFIG_USERSPACE
 static inline bool z_arch_is_user_context(void);
+#else
+#define z_arch_is_user_context() (true)
+#endif
 
-#endif /* CONFIG_USERSPACE */
+/* True if a syscall function must trap to the kernel, usually a
+ * compile-time decision.
+ */
+static ALWAYS_INLINE bool z_syscall_trap(void)
+{
+        bool ret = false;
+#ifdef CONFIG_USERSPACE
+#if defined(__ZEPHYR_SUPERVISOR__)
+        ret = false;
+#elif defined(__ZEPHYR_USER__)
+        ret = true;
+#else
+        ret = z_arch_is_user_context();
+#endif
+#endif
+        return ret;
+}
 
 /**
  * Indicate whether the CPU is currently in user mode

kernel/device.c
@@ -95,7 +95,7 @@ struct device *z_impl_device_get_binding(const char *name)
 }
 
 #ifdef CONFIG_USERSPACE
-Z_SYSCALL_HANDLER(device_get_binding, name)
+static inline struct device *z_vrfy_device_get_binding(const char *name)
 {
 	char name_copy[Z_DEVICE_MAX_NAME_LEN];
 
@@ -104,8 +104,9 @@ Z_SYSCALL_HANDLER(device_get_binding, name)
 		return 0;
 	}
 
-	return (u32_t)z_impl_device_get_binding(name_copy);
+	return z_impl_device_get_binding(name_copy);
 }
+#include <syscalls/device_get_binding_mrsh.c>
 #endif /* CONFIG_USERSPACE */
 
 #ifdef CONFIG_DEVICE_POWER_MANAGEMENT

kernel/errno.c
@@ -32,7 +32,12 @@ int *z_impl_z_errno(void)
 	return &_current->userspace_local_data->errno_var;
 }
 
-Z_SYSCALL_HANDLER0_SIMPLE(z_errno);
+static inline int *z_vrfy_z_errno(void)
+{
+	return z_impl_z_errno();
+}
+#include <syscalls/z_errno_mrsh.c>
+
 #else
 int *z_impl_z_errno(void)
 {

kernel/futex.c
@@ -52,14 +52,15 @@ int z_impl_k_futex_wake(struct k_futex *futex, bool wake_all)
 	return woken;
 }
 
-Z_SYSCALL_HANDLER(k_futex_wake, futex, wake_all)
+static inline int z_vrfy_k_futex_wake(struct k_futex *futex, bool wake_all)
 {
 	if (Z_SYSCALL_MEMORY_WRITE(futex, sizeof(struct k_futex)) != 0) {
 		return -EACCES;
 	}
 
-	return z_impl_k_futex_wake((struct k_futex *)futex, (bool)wake_all);
+	return z_impl_k_futex_wake(futex, wake_all);
 }
+#include <syscalls/k_futex_wake_mrsh.c>
 
 int z_impl_k_futex_wait(struct k_futex *futex, int expected, s32_t timeout)
 {
@@ -88,12 +89,12 @@ int z_impl_k_futex_wait(struct k_futex *futex, int expected, s32_t timeout)
 	return ret;
 }
 
-Z_SYSCALL_HANDLER(k_futex_wait, futex, expected, timeout)
+static inline int z_vrfy_k_futex_wait(struct k_futex *futex, int expected, s32_t timeout)
 {
 	if (Z_SYSCALL_MEMORY_WRITE(futex, sizeof(struct k_futex)) != 0) {
 		return -EACCES;
 	}
 
-	return z_impl_k_futex_wait((struct k_futex *)futex,
-			expected, (s32_t)timeout);
+	return z_impl_k_futex_wait(futex, expected, timeout);
 }
+#include <syscalls/k_futex_wait_mrsh.c>

kernel/include/kernel_structs.h
@@ -99,6 +99,11 @@ struct _cpu {
 	/* one assigned idle thread per CPU */
 	struct k_thread *idle_thread;
 
+#ifdef CONFIG_USERSPACE
+	/* current syscall frame pointer */
+	void *syscall_frame;
+#endif
+
 #ifdef CONFIG_TIMESLICING
 	/* number of ticks remaining in current time slice */
 	int slice_ticks;

kernel/include/syscall_handler.h
@@ -15,6 +15,7 @@
 #include <sys/printk.h>
 #include <sys/math_extras.h>
 #include <kernel_internal.h>
+#include <kernel_structs.h>
 #include <stdbool.h>
 
 extern const _k_syscall_handler_t _k_syscall_table[K_SYSCALL_LIMIT];
@@ -259,7 +260,7 @@ extern int z_user_string_copy(char *dst, const char *src, size_t maxlen);
 #define Z_OOPS(expr) \
 	do { \
 		if (expr) { \
-			z_arch_syscall_oops(ssf); \
+			z_arch_syscall_oops(_current_cpu->syscall_frame); \
 		} \
 	} while (false)
 
@@ -506,129 +507,6 @@ static inline int z_obj_validation_check(struct _k_object *ko,
 
 #define Z_SYSCALL_OBJ_NEVER_INIT(ptr, type) \
 	Z_SYSCALL_IS_OBJ(ptr, type, _OBJ_INIT_FALSE)
-
-/*
- * Handler definition macros
- *
- * All handlers have the same prototype:
- *
- * u32_t _handler_APINAME(u32_t arg1, u32_t arg2, u32_t arg3,
- *			  u32_t arg4, u32_t arg5, u32_t arg6, void *ssf);
- *
- * These make it much simpler to define handlers instead of typing out
- * the bolierplate. The macros ensure that the seventh argument is named
- * "ssf" as this is now referenced by various other Z_SYSCALL macros.
- *
- * Use the Z_SYSCALL_HANDLER(name_, arg1, ..., arg6) variant, as it will
- * automatically deduce the correct version of Z__SYSCALL_HANDLERn() to
- * use depending on the number of arguments.
- */
-
-#define Z__SYSCALL_HANDLER0(name_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1 __unused, \
-				 u32_t arg2 __unused, \
-				 u32_t arg3 __unused, \
-				 u32_t arg4 __unused, \
-				 u32_t arg5 __unused, \
-				 u32_t arg6 __unused, \
-				 void *ssf)
-
-#define Z__SYSCALL_HANDLER1(name_, arg1_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1_, \
-				 u32_t arg2 __unused, \
-				 u32_t arg3 __unused, \
-				 u32_t arg4 __unused, \
-				 u32_t arg5 __unused, \
-				 u32_t arg6 __unused, \
-				 void *ssf)
-
-#define Z__SYSCALL_HANDLER2(name_, arg1_, arg2_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1_, \
-				 u32_t arg2_, \
-				 u32_t arg3 __unused, \
-				 u32_t arg4 __unused, \
-				 u32_t arg5 __unused, \
-				 u32_t arg6 __unused, \
-				 void *ssf)
-
-#define Z__SYSCALL_HANDLER3(name_, arg1_, arg2_, arg3_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1_, \
-				 u32_t arg2_, \
-				 u32_t arg3_, \
-				 u32_t arg4 __unused, \
-				 u32_t arg5 __unused, \
-				 u32_t arg6 __unused, \
-				 void *ssf)
-
-#define Z__SYSCALL_HANDLER4(name_, arg1_, arg2_, arg3_, arg4_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1_, \
-				 u32_t arg2_, \
-				 u32_t arg3_, \
-				 u32_t arg4_, \
-				 u32_t arg5 __unused, \
-				 u32_t arg6 __unused, \
-				 void *ssf)
-
-#define Z__SYSCALL_HANDLER5(name_, arg1_, arg2_, arg3_, arg4_, arg5_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1_, \
-				 u32_t arg2_, \
-				 u32_t arg3_, \
-				 u32_t arg4_, \
-				 u32_t arg5_, \
-				 u32_t arg6 __unused, \
-				 void *ssf)
-
-#define Z__SYSCALL_HANDLER6(name_, arg1_, arg2_, arg3_, arg4_, arg5_, arg6_) \
-	u32_t z_hdlr_ ## name_(u32_t arg1_, \
-				 u32_t arg2_, \
-				 u32_t arg3_, \
-				 u32_t arg4_, \
-				 u32_t arg5_, \
-				 u32_t arg6_, \
-				 void *ssf)
-
-#define Z_SYSCALL_CONCAT(arg1, arg2) Z__SYSCALL_CONCAT(arg1, arg2)
-#define Z__SYSCALL_CONCAT(arg1, arg2) Z___SYSCALL_CONCAT(arg1, arg2)
-#define Z___SYSCALL_CONCAT(arg1, arg2) arg1##arg2
-
-#define Z_SYSCALL_NARG(...) Z__SYSCALL_NARG(__VA_ARGS__, Z__SYSCALL_RSEQ_N())
-#define Z__SYSCALL_NARG(...) Z__SYSCALL_ARG_N(__VA_ARGS__)
-#define Z__SYSCALL_ARG_N(_1, _2, _3, _4, _5, _6, _7, N, ...) N
-#define Z__SYSCALL_RSEQ_N() 6, 5, 4, 3, 2, 1, 0
-
-#define Z_SYSCALL_HANDLER(...) \
-	Z_SYSCALL_CONCAT(Z__SYSCALL_HANDLER, \
-			 Z_SYSCALL_NARG(__VA_ARGS__))(__VA_ARGS__)
-
-/*
- * Helper macros for a very common case: calls which just take one argument
- * which is an initialized kernel object of a specific type. Verify the object
- * and call the implementation.
- */
-
-#define Z_SYSCALL_HANDLER1_SIMPLE(name_, obj_enum_, obj_type_) \
-	Z__SYSCALL_HANDLER1(name_, arg1) { \
-		Z_OOPS(Z_SYSCALL_OBJ(arg1, obj_enum_)); \
-		return (u32_t)z_impl_ ## name_((obj_type_)arg1); \
-	}
-
-#define Z_SYSCALL_HANDLER1_SIMPLE_VOID(name_, obj_enum_, obj_type_) \
-	Z__SYSCALL_HANDLER1(name_, arg1) { \
-		Z_OOPS(Z_SYSCALL_OBJ(arg1, obj_enum_)); \
-		z_impl_ ## name_((obj_type_)arg1); \
-		return 0; \
-	}
-
-#define Z_SYSCALL_HANDLER0_SIMPLE(name_) \
-	Z__SYSCALL_HANDLER0(name_) { \
-		return (u32_t)z_impl_ ## name_(); \
-	}
-
-#define Z_SYSCALL_HANDLER0_SIMPLE_VOID(name_) \
-	Z__SYSCALL_HANDLER0(name_) { \
-		z_impl_ ## name_(); \
-		return 0; \
-	}
 
 #include <driver-validation.h>
 

commit 99811ff28b09940ff4ba23d6dc920a3e22943b80

gen_syscalls.py: make 'noweak' a list instead of a random order set
Python's Sets are not deterministic. This causes the following lines to
be emitted in random order in generated/syscall_dispatch.c

extern u32_t z_mrsh_k_object_release(u32_t arg1, ...
extern u32_t z_mrsh_k_object_access_grant(u32_t arg1, ...
extern u32_t z_mrsh_k_object_alloc(u32_t arg1, ...

Change noweak to a basic list.

Reproducibility regression introduced by commit 891f389ae52f0

Signed-off-by: Marc Herbert <marc.herbert@intel.com>

scripts/gen_syscalls.py
@@ -39,9 +39,9 @@ types64 = ["s64_t", "u64_t"]
 # order runs afoul of a comment in CMakeLists.txt that the order is
 # critical.  These are core syscalls that won't ever be unconfigured,
 # just disable the fallback mechanism as a simple workaround.
-noweak = set(["z_mrsh_k_object_release",
-              "z_mrsh_k_object_access_grant",
-              "z_mrsh_k_object_alloc"])
+noweak = ["z_mrsh_k_object_release",
+          "z_mrsh_k_object_access_grant",
+          "z_mrsh_k_object_alloc"]
 
 table_template = """/* auto-generated by gen_syscalls.py, don't edit */
 