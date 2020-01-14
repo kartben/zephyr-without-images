kartben/zephyr-without-images

commit 88546433adf0efc68b50896121640acb24237b90

Bluetooth: host: Add config option for discardable buffer size.
Add option to set the size of the discardable buffer pool. This saves
memory for the MESH use case where we expect a large number of advertise
reports.

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

subsys/bluetooth/host/Kconfig
@@ -60,6 +60,16 @@ config BT_DISCARDABLE_BUF_COUNT
 	  it will not cause the allocation for other critical events to
 	  block and may even eliminate deadlocks in some cases.
 
+config BT_DISCARDABLE_BUF_SIZE
+	int "Size of discardable event buffers"
+	range 44 257
+	default 257 if BT_BREDR
+	default 44
+	help
+	  Size of buffers in the separate discardable event buffer pool.
+	  The minimum size is set based on the Advertising Report. Setting
+	  the buffer size different than BT_RX_BUF_LEN can save memory.
+
 config BT_HCI_TX_STACK_SIZE
 	# NOTE: This value is derived from other symbols and should only be
 	# changed if required by architecture

subsys/bluetooth/host/hci_core.c
@@ -168,8 +168,9 @@ NET_BUF_POOL_FIXED_DEFINE(num_complete_pool, 1, BT_BUF_RX_SIZE, NULL);
 #endif /* CONFIG_BT_CONN */
 
 #if defined(CONFIG_BT_DISCARDABLE_BUF_COUNT)
+#define DISCARDABLE_EVENT_SIZE BT_BUF_SIZE(CONFIG_BT_DISCARDABLE_BUF_SIZE)
 NET_BUF_POOL_FIXED_DEFINE(discardable_pool, CONFIG_BT_DISCARDABLE_BUF_COUNT,
-			  BT_BUF_RX_SIZE, NULL);
+			  DISCARDABLE_EVENT_SIZE, NULL);
 #endif /* CONFIG_BT_DISCARDABLE_BUF_COUNT */
 
 struct event_handler {

commit de023b8630d65ab12f354420ef20cb3a9d2cc132

Bluetooth: Kconfig: Fix error in buffer pool sizes
Fix error in calculation of the minimum discardable buffer size. For
the LE Advertising Report the maximum payload is 31 bytes plus
additional data in the event gives an event size of 41 bytes (given that
num_reports is 1). Since this is a meta event we need to include the
sub-event code, plus the event header of 2 bytes. Total of 44 bytes.

This is a regression from 88546433ad where we forgot th 1 byte for the
RSSI that is appended after the data. Easy to miss since it is not part
of the struct.

Fix error in calculation of the num complete buffer size. Here we forgot
to include the 2 byte event header.

This is a regression from b54453ab9f.

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

subsys/bluetooth/host/Kconfig
@@ -62,9 +62,9 @@ config BT_DISCARDABLE_BUF_COUNT
 
 config BT_DISCARDABLE_BUF_SIZE
 	int "Size of discardable event buffers"
-	range 44 257
+	range 45 257
 	default 257 if BT_BREDR
-	default 44
+	default 45
 	help
 	  Size of buffers in the separate discardable event buffer pool.
 	  The minimum size is set based on the Advertising Report. Setting

subsys/bluetooth/host/hci_core.c
@@ -160,6 +160,7 @@ NET_BUF_POOL_FIXED_DEFINE(hci_rx_pool, CONFIG_BT_RX_BUF_COUNT,
 
 #if defined(CONFIG_BT_CONN)
 #define NUM_COMLETE_EVENT_SIZE BT_BUF_SIZE(                            \
+	sizeof(struct bt_hci_evt_hdr) +                                \
 	sizeof(struct bt_hci_cp_host_num_completed_packets) +          \
 	CONFIG_BT_MAX_CONN * sizeof(struct bt_hci_handle_count))
 /* Dedicated pool for HCI_Number_of_Completed_Packets. This event is always