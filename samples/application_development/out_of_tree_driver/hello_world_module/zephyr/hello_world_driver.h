zephyrproject-rtos/zephyr

samples: Add sample 'application_development/out_of_tree_driver'
Add a sample that demonstrates (and tests) that custom drivers can be
maintained outside of Zephyr.

The sample is fairly minimal with few dependencies and should
therefore be very portable. It also includes a sample.yaml that should
ensure that it does not regress.

Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>

samples/application_development/out_of_tree_driver/src/hello_world_driver.h
@@ -1,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __HELLO_WORLD_DRIVER_H__
+#define __HELLO_WORLD_DRIVER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <device.h>
+
+/*
+ * This 'Hello World' driver has a 'print' syscall that prints the
+ * famous 'Hello World!' string.
+ *
+ * The string is formatted with some internal driver data to
+ * demonstrate that drivers are initialized during the boot process.
+ *
+ * The driver exists to demonstrate (and test) custom drivers that are
+ * maintained outside of Zephyr.
+ */
+
+struct hello_world_driver_api {
+	/* This struct has a member called 'print'. 'print' is function
+	 * pointer to a function that takes 'struct device *dev' as an
+	 * argument and returns 'void'.
+	 */
+	void (*print)(struct device *dev);
+};
+
+__syscall void hello_world_print(struct device *dev);
+static inline void _impl_hello_world_print(struct device *dev)
+{
+	const struct hello_world_driver_api *api = dev->driver_api;
+	__ASSERT(api->print, "Callback pointer should not be NULL");
+	api->print(dev);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <syscalls/hello_world_driver.h>
+
+#endif /* __HELLO_WORLD_DRIVER_H__ */

kartben/zephyr-without-images

samples/application_development/out_of_tree_driver/hello_world_module/zephyr/hello_world_driver.h
@@ -1,0 +1,50 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __HELLO_WORLD_DRIVER_H__
+#define __HELLO_WORLD_DRIVER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <device.h>
+
+/*
+ * This 'Hello World' driver has a 'print' syscall that prints the
+ * famous 'Hello World!' string.
+ *
+ * The string is formatted with some internal driver data to
+ * demonstrate that drivers are initialized during the boot process.
+ *
+ * The driver exists to demonstrate (and test) custom drivers that are
+ * maintained outside of Zephyr.
+ */
+
+struct hello_world_driver_api {
+	/* This struct has a member called 'print'. 'print' is function
+	 * pointer to a function that takes 'struct device *dev' as an
+	 * argument and returns 'void'.
+	 */
+	void (*print)(struct device *dev);
+};
+
+__syscall     void        hello_world_print(struct device *dev);
+static inline void z_impl_hello_world_print(struct device *dev)
+{
+	const struct hello_world_driver_api *api = dev->driver_api;
+
+	__ASSERT(api->print, "Callback pointer should not be NULL");
+
+	api->print(dev);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <syscalls/hello_world_driver.h>
+
+#endif /* __HELLO_WORLD_DRIVER_H__ */