zephyrproject-rtos/zephyr

Bluetooth: controller: split: Initial extended scanning code
Initial extended scanning code added.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan.h
@@ -44,6 +44,18 @@ struct lll_scan {
 #endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
 };
 
+struct lll_scan_aux {
+	struct lll_hdr hdr;
+
+	struct lll_scan *scan;
+
+	u8_t chan:6;
+
+	u8_t phy:3;
+
+	u32_t window_widening_us;
+};
+
 int lll_scan_init(void);
 int lll_scan_reset(void);
 

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_aux.h
@@ -1,0 +1,11 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int lll_scan_aux_init(void);
+int lll_scan_aux_reset(void);
+void lll_scan_aux_prepare(void *param);
+
+extern u8_t ull_scan_aux_lll_handle_get(struct lll_scan_aux *lll);

subsys/bluetooth/controller/ll_sw/ull.c
@@ -91,8 +91,15 @@
 
 #if defined(CONFIG_BT_OBSERVER)
 #define BT_SCAN_TICKER_NODES ((TICKER_ID_SCAN_LAST) - (TICKER_ID_SCAN_STOP) + 1)
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+#define BT_SCAN_AUX_TICKER_NODES ((TICKER_ID_SCAN_AUX_LAST) - \
+				  (TICKER_ID_SCAN_AUX_BASE) + 1)
+#else /* !CONFIG_BT_CTLR_ADV_EXT */
+#define BT_SCAN_AUX_TICKER_NODES 0
+#endif /* !CONFIG_BT_CTLR_ADV_EXT */
 #else
 #define BT_SCAN_TICKER_NODES 0
+#define BT_SCAN_AUX_TICKER_NODES 0
 #endif
 
 #if defined(CONFIG_BT_CONN)
@@ -120,6 +127,7 @@
 				   BT_ADV_AUX_TICKER_NODES + \
 				   BT_ADV_SYNC_TICKER_NODES + \
 				   BT_SCAN_TICKER_NODES + \
+				   BT_SCAN_AUX_TICKER_NODES + \
 				   BT_CONN_TICKER_NODES + \
 				   FLASH_TICKER_NODES + \
 				   USER_TICKER_NODES)
@@ -629,7 +637,9 @@ void ll_rx_dequeue(void)
 #endif /* CONFIG_BT_OBSERVER */
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
+		/* fallthrough */
 	case NODE_RX_TYPE_EXT_1M_REPORT:
+	case NODE_RX_TYPE_EXT_2M_REPORT:
 	case NODE_RX_TYPE_EXT_CODED_REPORT:
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
@@ -788,7 +798,9 @@ void ll_rx_mem_release(void **node_rx)
 #endif /* CONFIG_BT_OBSERVER */
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
+			/* fallthrough */
 		case NODE_RX_TYPE_EXT_1M_REPORT:
+		case NODE_RX_TYPE_EXT_2M_REPORT:
 		case NODE_RX_TYPE_EXT_CODED_REPORT:
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
@@ -1563,6 +1575,39 @@ static inline int rx_demux_rx(memq_link_t *link, struct node_rx_hdr *rx)
 	}
 	break;
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+	case NODE_RX_TYPE_EXT_1M_REPORT:
+	case NODE_RX_TYPE_EXT_2M_REPORT:
+	case NODE_RX_TYPE_EXT_CODED_REPORT:
+	{
+		u8_t phy = 0U;
+
+		memq_dequeue(memq_ull_rx.tail, &memq_ull_rx.head, NULL);
+
+		switch (rx->type) {
+		case NODE_RX_TYPE_EXT_1M_REPORT:
+			phy = BIT(0);
+			break;
+		case NODE_RX_TYPE_EXT_2M_REPORT:
+			phy = BIT(1);
+			break;
+		case NODE_RX_TYPE_EXT_CODED_REPORT:
+			phy = BIT(2);
+			break;
+		default:
+			LL_ASSERT(0);
+			break;
+		}
+
+		/* TODO: below interface is WIP */
+		ull_scan_aux_setup(rx, phy, NULL);
+
+		ll_rx_put(link, rx);
+		ll_rx_sched();
+	}
+	break;
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
+
 #if defined(CONFIG_BT_CONN)
 	case NODE_RX_TYPE_CONNECTION:
 	{
@@ -1602,11 +1647,6 @@ static inline int rx_demux_rx(memq_link_t *link, struct node_rx_hdr *rx)
 #if defined(CONFIG_BT_OBSERVER)
 	case NODE_RX_TYPE_REPORT:
 #endif /* CONFIG_BT_OBSERVER */
-
-#if defined(CONFIG_BT_CTLR_ADV_EXT)
-	case NODE_RX_TYPE_EXT_1M_REPORT:
-	case NODE_RX_TYPE_EXT_CODED_REPORT:
-#endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 #if defined(CONFIG_BT_CTLR_SCAN_REQ_NOTIFY)
 	case NODE_RX_TYPE_SCAN_REQ:
@@ -1671,6 +1711,15 @@ static inline void rx_demux_event_done(memq_link_t *link,
 		ull_conn_done(done);
 		break;
 #endif /* CONFIG_BT_CONN */
+
+#if defined(CONFIG_BT_OBSERVER)
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+		/* fallthrough checkpatch workaround! */
+	case EVENT_DONE_EXTRA_TYPE_SCAN_AUX:
+		ull_scan_aux_done(done);
+		break;
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
+#endif /* CONFIG_BT_OBSERVER */
 
 #if defined(CONFIG_BT_CTLR_USER_EXT)
 	case EVENT_DONE_EXTRA_TYPE_USER_START

subsys/bluetooth/controller/ll_sw/ull_scan_aux.c
@@ -1,0 +1,293 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <sys/util.h>
+
+#include "util/mem.h"
+#include "util/memq.h"
+#include "util/mayfly.h"
+
+#include "hal/ticker.h"
+
+#include "ticker/ticker.h"
+
+#include "pdu.h"
+
+#include "lll.h"
+#include "lll_vendor.h"
+#include "lll_scan.h"
+#include "lll_scan_aux.h"
+
+#include "ull_scan_types.h"
+
+#include "ull_internal.h"
+#include "ull_scan_internal.h"
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_DRIVER)
+#define LOG_MODULE_NAME bt_ctlr_ull_scan_aux
+#include "common/log.h"
+#include <soc.h>
+#include "hal/debug.h"
+
+static int init_reset(void);
+static inline struct ll_scan_aux_set *aux_acquire(void);
+static inline void aux_release(struct ll_scan_aux_set *aux);
+static inline u16_t aux_handle_get(struct ll_scan_aux_set *aux);
+static void ticker_cb(u32_t ticks_at_expire, u32_t remainder, u16_t lazy,
+		      void *param);
+static void ticker_op_cb(u32_t status, void *param);
+static void ticker_op_aux_failure(void *param);
+
+static struct ll_scan_aux_set ll_scan_aux_pool[CONFIG_BT_CTLR_SCAN_AUX_SET];
+static void *scan_aux_free;
+
+int ull_scan_aux_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int ull_scan_aux_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+void ull_scan_aux_setup(struct node_rx_hdr *rx, u8_t phy,
+			struct ll_scan_aux_set *aux)
+{
+	struct ext_adv_aux_ptr *aux_ptr;
+	struct pdu_adv_com_ext_adv *p;
+	u32_t ticks_slot_overhead;
+	struct lll_scan_aux *lll;
+	struct node_rx_ftr *ftr;
+	u32_t ticks_slot_offset;
+	struct ext_adv_hdr *h;
+	u32_t ready_delay_us;
+	struct pdu_adv *pdu;
+	u32_t aux_offset_us;
+	u32_t ticker_status;
+	u8_t aux_handle;
+	u8_t *ptr;
+
+	pdu = (void *)((struct node_rx_pdu *)rx)->pdu;
+	if ((pdu->type != PDU_ADV_TYPE_EXT_IND) || !pdu->len) {
+		return;
+	}
+
+	p = (void *)&pdu->adv_ext_ind;
+	if (!p->ext_hdr_len) {
+		return;
+	}
+
+	h = (void *)p->ext_hdr_adi_adv_data;
+	if (!h->aux_ptr) {
+		return;
+	}
+
+	if (!aux) {
+		aux = aux_acquire();
+		if (!aux) {
+			return;
+		}
+
+		lll = &aux->lll;
+
+		ull_hdr_init((void *)lll);
+		lll_hdr_init(lll, aux);
+	} else {
+		lll = &aux->lll;
+	}
+
+	ptr = (u8_t *)h + sizeof(*h);
+
+	if (h->adv_addr) {
+		ptr += BDADDR_SIZE;
+	}
+
+	if (h->tgt_addr) {
+		ptr += BDADDR_SIZE;
+	}
+
+	if (h->adi) {
+		struct ext_adv_adi *adi;
+
+		adi = (void *)ptr;
+		ptr += sizeof(*adi);
+	}
+
+	aux_ptr = (void *)ptr;
+
+	ftr = &rx->rx_ftr;
+	lll->scan = ftr->param;
+	lll->chan = aux_ptr->chan_idx;
+	lll->phy = BIT(aux_ptr->phy);
+
+	/* TODO: apply clock accuracy to the aux_offset_us and calculate
+	 * window widening. Use aux_ptr->ca value.
+	 */
+	lll->window_widening_us = 1000;
+
+	aux_offset_us = ftr->radio_end_us - PKT_AC_US(pdu->len, 0, phy);
+	if (aux_ptr->offs_units) {
+		aux_offset_us += (u32_t)aux_ptr->offs * 300U;
+	} else {
+		aux_offset_us += (u32_t)aux_ptr->offs * 30U;
+	}
+
+	/* TODO: apply clock accuracy to the aux_offset_us and calculate
+	 * window widening. Use aux_ptr->ca value.
+	 */
+
+	ready_delay_us = lll_radio_rx_ready_delay_get(lll->phy, 1);
+
+	aux_offset_us -= EVENT_OVERHEAD_START_US;
+	aux_offset_us -= EVENT_JITTER_US;
+	aux_offset_us -= ready_delay_us;
+
+	/* TODO: active_to_start feature port */
+	aux->evt.ticks_active_to_start = 0;
+	aux->evt.ticks_xtal_to_start =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_XTAL_US);
+	aux->evt.ticks_preempt_to_start =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_PREEMPT_MIN_US);
+	aux->evt.ticks_slot =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_START_US +
+				       ready_delay_us + 10000 +
+				       EVENT_OVERHEAD_END_US);
+
+	ticks_slot_offset = MAX(aux->evt.ticks_active_to_start,
+				aux->evt.ticks_xtal_to_start);
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_LOW_LAT)) {
+		ticks_slot_overhead = ticks_slot_offset;
+	} else {
+		ticks_slot_overhead = 0U;
+	}
+
+	/* TODO: unreserve the primary scan window ticks in ticker */
+
+	aux_handle = aux_handle_get(aux);
+
+	ticker_status = ticker_start(TICKER_INSTANCE_ID_CTLR,
+				     TICKER_USER_ID_ULL_HIGH,
+				     TICKER_ID_SCAN_AUX_BASE + aux_handle,
+				     ftr->ticks_anchor - ticks_slot_offset,
+				     HAL_TICKER_US_TO_TICKS(aux_offset_us),
+				     TICKER_NULL_PERIOD,
+				     TICKER_NULL_REMAINDER,
+				     TICKER_NULL_LAZY,
+				     (aux->evt.ticks_slot +
+				      ticks_slot_overhead),
+				     ticker_cb, aux, ticker_op_cb, aux);
+	LL_ASSERT((ticker_status == TICKER_STATUS_SUCCESS) ||
+		  (ticker_status == TICKER_STATUS_BUSY));
+}
+
+void ull_scan_aux_done(struct node_rx_event_done *done)
+{
+	struct lll_scan_aux *lll = (void *)HDR_ULL2LLL(done->param);
+	struct ll_scan_aux_set *aux = (void *)HDR_LLL2EVT(lll);
+
+	aux_release(aux);
+}
+
+u8_t ull_scan_aux_lll_handle_get(struct lll_scan_aux *lll)
+{
+	return aux_handle_get((void *)lll->hdr.parent);
+}
+
+static int init_reset(void)
+{
+	/* Initialize adv aux pool. */
+	mem_init(ll_scan_aux_pool, sizeof(struct ll_scan_aux_set),
+		 sizeof(ll_scan_aux_pool) / sizeof(struct ll_scan_aux_set),
+		 &scan_aux_free);
+
+	return 0;
+}
+
+static inline struct ll_scan_aux_set *aux_acquire(void)
+{
+	return mem_acquire(&scan_aux_free);
+}
+
+static inline void aux_release(struct ll_scan_aux_set *aux)
+{
+	mem_release(aux, &scan_aux_free);
+}
+
+static inline u16_t aux_handle_get(struct ll_scan_aux_set *aux)
+{
+	return mem_index_get(aux, ll_scan_aux_pool,
+			     sizeof(struct ll_scan_aux_set));
+}
+
+static void ticker_cb(u32_t ticks_at_expire, u32_t remainder, u16_t lazy,
+		      void *param)
+{
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, lll_scan_aux_prepare};
+	struct ll_scan_aux_set *aux = param;
+	static struct lll_prepare_param p;
+	u32_t ret;
+	u8_t ref;
+
+	DEBUG_RADIO_PREPARE_O(1);
+
+	/* Increment prepare reference count */
+	ref = ull_ref_inc(&aux->ull);
+	LL_ASSERT(ref);
+
+	/* Append timing parameters */
+	p.ticks_at_expire = ticks_at_expire;
+	p.remainder = 0; /* FIXME: remainder; */
+	p.lazy = lazy;
+	p.param = &aux->lll;
+	mfy.param = &p;
+
+	/* Kick LLL prepare */
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH, TICKER_USER_ID_LLL,
+			     0, &mfy);
+	LL_ASSERT(!ret);
+
+	DEBUG_RADIO_PREPARE_O(1);
+}
+
+static void ticker_op_cb(u32_t status, void *param)
+{
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, ticker_op_aux_failure};
+	u32_t ret;
+
+	if (status == TICKER_STATUS_SUCCESS) {
+		return;
+	}
+
+	mfy.param = param;
+
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH, TICKER_USER_ID_LLL,
+			     0, &mfy);
+	LL_ASSERT(!ret);
+}
+
+static void ticker_op_aux_failure(void *param)
+{
+	aux_release(param);
+}

subsys/bluetooth/controller/ll_sw/ull_scan_internal.h
@@ -39,3 +39,13 @@ u32_t ull_scan_is_enabled(u8_t handle);
 
 /* Return filter policy used */
 u32_t ull_scan_filter_pol_get(u8_t handle);
+
+int ull_scan_aux_init(void);
+int ull_scan_aux_reset(void);
+
+/* Helper to setup scanning on auxiliary channel */
+void ull_scan_aux_setup(struct node_rx_hdr *rx, u8_t phy,
+			struct ll_scan_aux_set *aux);
+
+/* Helper to clean up auxiliary channel scanning */
+void ull_scan_aux_done(struct node_rx_event_done *done);

kartben/zephyr-without-images

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan.h
@@ -44,6 +44,18 @@ struct lll_scan {
 #endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
 };
 
+struct lll_scan_aux {
+	struct lll_hdr hdr;
+
+	struct lll_scan *scan;
+
+	uint8_t chan:6;
+
+	uint8_t phy:3;
+
+	uint32_t window_widening_us;
+};
+
 int lll_scan_init(void);
 int lll_scan_reset(void);
 

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_scan_aux.h
@@ -1,0 +1,11 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int lll_scan_aux_init(void);
+int lll_scan_aux_reset(void);
+void lll_scan_aux_prepare(void *param);
+
+extern uint8_t ull_scan_aux_lll_handle_get(struct lll_scan_aux *lll);

subsys/bluetooth/controller/ll_sw/ull.c
@@ -91,8 +91,15 @@
 
 #if defined(CONFIG_BT_OBSERVER)
 #define BT_SCAN_TICKER_NODES ((TICKER_ID_SCAN_LAST) - (TICKER_ID_SCAN_STOP) + 1)
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+#define BT_SCAN_AUX_TICKER_NODES ((TICKER_ID_SCAN_AUX_LAST) - \
+				  (TICKER_ID_SCAN_AUX_BASE) + 1)
+#else /* !CONFIG_BT_CTLR_ADV_EXT */
+#define BT_SCAN_AUX_TICKER_NODES 0
+#endif /* !CONFIG_BT_CTLR_ADV_EXT */
 #else
 #define BT_SCAN_TICKER_NODES 0
+#define BT_SCAN_AUX_TICKER_NODES 0
 #endif
 
 #if defined(CONFIG_BT_CONN)
@@ -120,6 +127,7 @@
 				   BT_ADV_AUX_TICKER_NODES + \
 				   BT_ADV_SYNC_TICKER_NODES + \
 				   BT_SCAN_TICKER_NODES + \
+				   BT_SCAN_AUX_TICKER_NODES + \
 				   BT_CONN_TICKER_NODES + \
 				   FLASH_TICKER_NODES + \
 				   USER_TICKER_NODES)
@@ -629,7 +637,9 @@ void ll_rx_dequeue(void)
 #endif /* CONFIG_BT_OBSERVER */
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
+		/* fallthrough */
 	case NODE_RX_TYPE_EXT_1M_REPORT:
+	case NODE_RX_TYPE_EXT_2M_REPORT:
 	case NODE_RX_TYPE_EXT_CODED_REPORT:
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
@@ -788,7 +798,9 @@ void ll_rx_mem_release(void **node_rx)
 #endif /* CONFIG_BT_OBSERVER */
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
+			/* fallthrough */
 		case NODE_RX_TYPE_EXT_1M_REPORT:
+		case NODE_RX_TYPE_EXT_2M_REPORT:
 		case NODE_RX_TYPE_EXT_CODED_REPORT:
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
@@ -1563,6 +1575,39 @@ static inline int rx_demux_rx(memq_link_t *link, struct node_rx_hdr *rx)
 	}
 	break;
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+	case NODE_RX_TYPE_EXT_1M_REPORT:
+	case NODE_RX_TYPE_EXT_2M_REPORT:
+	case NODE_RX_TYPE_EXT_CODED_REPORT:
+	{
+		uint8_t phy = 0U;
+
+		memq_dequeue(memq_ull_rx.tail, &memq_ull_rx.head, NULL);
+
+		switch (rx->type) {
+		case NODE_RX_TYPE_EXT_1M_REPORT:
+			phy = BIT(0);
+			break;
+		case NODE_RX_TYPE_EXT_2M_REPORT:
+			phy = BIT(1);
+			break;
+		case NODE_RX_TYPE_EXT_CODED_REPORT:
+			phy = BIT(2);
+			break;
+		default:
+			LL_ASSERT(0);
+			break;
+		}
+
+		/* TODO: below interface is WIP */
+		ull_scan_aux_setup(rx, phy, NULL);
+
+		ll_rx_put(link, rx);
+		ll_rx_sched();
+	}
+	break;
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
+
 #if defined(CONFIG_BT_CONN)
 	case NODE_RX_TYPE_CONNECTION:
 	{
@@ -1602,11 +1647,6 @@ static inline int rx_demux_rx(memq_link_t *link, struct node_rx_hdr *rx)
 #if defined(CONFIG_BT_OBSERVER)
 	case NODE_RX_TYPE_REPORT:
 #endif /* CONFIG_BT_OBSERVER */
-
-#if defined(CONFIG_BT_CTLR_ADV_EXT)
-	case NODE_RX_TYPE_EXT_1M_REPORT:
-	case NODE_RX_TYPE_EXT_CODED_REPORT:
-#endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 #if defined(CONFIG_BT_CTLR_SCAN_REQ_NOTIFY)
 	case NODE_RX_TYPE_SCAN_REQ:
@@ -1671,6 +1711,15 @@ static inline void rx_demux_event_done(memq_link_t *link,
 		ull_conn_done(done);
 		break;
 #endif /* CONFIG_BT_CONN */
+
+#if defined(CONFIG_BT_OBSERVER)
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+		/* fallthrough checkpatch workaround! */
+	case EVENT_DONE_EXTRA_TYPE_SCAN_AUX:
+		ull_scan_aux_done(done);
+		break;
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
+#endif /* CONFIG_BT_OBSERVER */
 
 #if defined(CONFIG_BT_CTLR_USER_EXT)
 	case EVENT_DONE_EXTRA_TYPE_USER_START

subsys/bluetooth/controller/ll_sw/ull_scan_aux.c
@@ -1,0 +1,293 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <sys/util.h>
+
+#include "util/mem.h"
+#include "util/memq.h"
+#include "util/mayfly.h"
+
+#include "hal/ticker.h"
+
+#include "ticker/ticker.h"
+
+#include "pdu.h"
+
+#include "lll.h"
+#include "lll_vendor.h"
+#include "lll_scan.h"
+#include "lll_scan_aux.h"
+
+#include "ull_scan_types.h"
+
+#include "ull_internal.h"
+#include "ull_scan_internal.h"
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_DRIVER)
+#define LOG_MODULE_NAME bt_ctlr_ull_scan_aux
+#include "common/log.h"
+#include <soc.h>
+#include "hal/debug.h"
+
+static int init_reset(void);
+static inline struct ll_scan_aux_set *aux_acquire(void);
+static inline void aux_release(struct ll_scan_aux_set *aux);
+static inline uint16_t aux_handle_get(struct ll_scan_aux_set *aux);
+static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
+		      uint16_t lazy, void *param);
+static void ticker_op_cb(uint32_t status, void *param);
+static void ticker_op_aux_failure(void *param);
+
+static struct ll_scan_aux_set ll_scan_aux_pool[CONFIG_BT_CTLR_SCAN_AUX_SET];
+static void *scan_aux_free;
+
+int ull_scan_aux_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int ull_scan_aux_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+void ull_scan_aux_setup(struct node_rx_hdr *rx, uint8_t phy,
+			struct ll_scan_aux_set *aux)
+{
+	struct ext_adv_aux_ptr *aux_ptr;
+	struct pdu_adv_com_ext_adv *p;
+	uint32_t ticks_slot_overhead;
+	struct lll_scan_aux *lll;
+	struct node_rx_ftr *ftr;
+	uint32_t ticks_slot_offset;
+	struct ext_adv_hdr *h;
+	uint32_t ready_delay_us;
+	struct pdu_adv *pdu;
+	uint32_t aux_offset_us;
+	uint32_t ticker_status;
+	uint8_t aux_handle;
+	uint8_t *ptr;
+
+	pdu = (void *)((struct node_rx_pdu *)rx)->pdu;
+	if ((pdu->type != PDU_ADV_TYPE_EXT_IND) || !pdu->len) {
+		return;
+	}
+
+	p = (void *)&pdu->adv_ext_ind;
+	if (!p->ext_hdr_len) {
+		return;
+	}
+
+	h = (void *)p->ext_hdr_adi_adv_data;
+	if (!h->aux_ptr) {
+		return;
+	}
+
+	if (!aux) {
+		aux = aux_acquire();
+		if (!aux) {
+			return;
+		}
+
+		lll = &aux->lll;
+
+		ull_hdr_init((void *)lll);
+		lll_hdr_init(lll, aux);
+	} else {
+		lll = &aux->lll;
+	}
+
+	ptr = (uint8_t *)h + sizeof(*h);
+
+	if (h->adv_addr) {
+		ptr += BDADDR_SIZE;
+	}
+
+	if (h->tgt_addr) {
+		ptr += BDADDR_SIZE;
+	}
+
+	if (h->adi) {
+		struct ext_adv_adi *adi;
+
+		adi = (void *)ptr;
+		ptr += sizeof(*adi);
+	}
+
+	aux_ptr = (void *)ptr;
+
+	ftr = &rx->rx_ftr;
+	lll->scan = ftr->param;
+	lll->chan = aux_ptr->chan_idx;
+	lll->phy = BIT(aux_ptr->phy);
+
+	/* TODO: apply clock accuracy to the aux_offset_us and calculate
+	 * window widening. Use aux_ptr->ca value.
+	 */
+	lll->window_widening_us = 1000;
+
+	aux_offset_us = ftr->radio_end_us - PKT_AC_US(pdu->len, 0, phy);
+	if (aux_ptr->offs_units) {
+		aux_offset_us += (uint32_t)aux_ptr->offs * 300U;
+	} else {
+		aux_offset_us += (uint32_t)aux_ptr->offs * 30U;
+	}
+
+	/* TODO: apply clock accuracy to the aux_offset_us and calculate
+	 * window widening. Use aux_ptr->ca value.
+	 */
+
+	ready_delay_us = lll_radio_rx_ready_delay_get(lll->phy, 1);
+
+	aux_offset_us -= EVENT_OVERHEAD_START_US;
+	aux_offset_us -= EVENT_JITTER_US;
+	aux_offset_us -= ready_delay_us;
+
+	/* TODO: active_to_start feature port */
+	aux->evt.ticks_active_to_start = 0;
+	aux->evt.ticks_xtal_to_start =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_XTAL_US);
+	aux->evt.ticks_preempt_to_start =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_PREEMPT_MIN_US);
+	aux->evt.ticks_slot =
+		HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_START_US +
+				       ready_delay_us + 10000 +
+				       EVENT_OVERHEAD_END_US);
+
+	ticks_slot_offset = MAX(aux->evt.ticks_active_to_start,
+				aux->evt.ticks_xtal_to_start);
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_LOW_LAT)) {
+		ticks_slot_overhead = ticks_slot_offset;
+	} else {
+		ticks_slot_overhead = 0U;
+	}
+
+	/* TODO: unreserve the primary scan window ticks in ticker */
+
+	aux_handle = aux_handle_get(aux);
+
+	ticker_status = ticker_start(TICKER_INSTANCE_ID_CTLR,
+				     TICKER_USER_ID_ULL_HIGH,
+				     TICKER_ID_SCAN_AUX_BASE + aux_handle,
+				     ftr->ticks_anchor - ticks_slot_offset,
+				     HAL_TICKER_US_TO_TICKS(aux_offset_us),
+				     TICKER_NULL_PERIOD,
+				     TICKER_NULL_REMAINDER,
+				     TICKER_NULL_LAZY,
+				     (aux->evt.ticks_slot +
+				      ticks_slot_overhead),
+				     ticker_cb, aux, ticker_op_cb, aux);
+	LL_ASSERT((ticker_status == TICKER_STATUS_SUCCESS) ||
+		  (ticker_status == TICKER_STATUS_BUSY));
+}
+
+void ull_scan_aux_done(struct node_rx_event_done *done)
+{
+	struct lll_scan_aux *lll = (void *)HDR_ULL2LLL(done->param);
+	struct ll_scan_aux_set *aux = (void *)HDR_LLL2EVT(lll);
+
+	aux_release(aux);
+}
+
+uint8_t ull_scan_aux_lll_handle_get(struct lll_scan_aux *lll)
+{
+	return aux_handle_get((void *)lll->hdr.parent);
+}
+
+static int init_reset(void)
+{
+	/* Initialize adv aux pool. */
+	mem_init(ll_scan_aux_pool, sizeof(struct ll_scan_aux_set),
+		 sizeof(ll_scan_aux_pool) / sizeof(struct ll_scan_aux_set),
+		 &scan_aux_free);
+
+	return 0;
+}
+
+static inline struct ll_scan_aux_set *aux_acquire(void)
+{
+	return mem_acquire(&scan_aux_free);
+}
+
+static inline void aux_release(struct ll_scan_aux_set *aux)
+{
+	mem_release(aux, &scan_aux_free);
+}
+
+static inline uint16_t aux_handle_get(struct ll_scan_aux_set *aux)
+{
+	return mem_index_get(aux, ll_scan_aux_pool,
+			     sizeof(struct ll_scan_aux_set));
+}
+
+static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
+		      uint16_t lazy, void *param)
+{
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, lll_scan_aux_prepare};
+	struct ll_scan_aux_set *aux = param;
+	static struct lll_prepare_param p;
+	uint32_t ret;
+	uint8_t ref;
+
+	DEBUG_RADIO_PREPARE_O(1);
+
+	/* Increment prepare reference count */
+	ref = ull_ref_inc(&aux->ull);
+	LL_ASSERT(ref);
+
+	/* Append timing parameters */
+	p.ticks_at_expire = ticks_at_expire;
+	p.remainder = 0; /* FIXME: remainder; */
+	p.lazy = lazy;
+	p.param = &aux->lll;
+	mfy.param = &p;
+
+	/* Kick LLL prepare */
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH, TICKER_USER_ID_LLL,
+			     0, &mfy);
+	LL_ASSERT(!ret);
+
+	DEBUG_RADIO_PREPARE_O(1);
+}
+
+static void ticker_op_cb(uint32_t status, void *param)
+{
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, ticker_op_aux_failure};
+	uint32_t ret;
+
+	if (status == TICKER_STATUS_SUCCESS) {
+		return;
+	}
+
+	mfy.param = param;
+
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH, TICKER_USER_ID_LLL,
+			     0, &mfy);
+	LL_ASSERT(!ret);
+}
+
+static void ticker_op_aux_failure(void *param)
+{
+	aux_release(param);
+}

subsys/bluetooth/controller/ll_sw/ull_scan_internal.h
@@ -39,3 +39,13 @@ uint32_t ull_scan_is_enabled(uint8_t handle);
 
 /* Return filter policy used */
 uint32_t ull_scan_filter_pol_get(uint8_t handle);
+
+int ull_scan_aux_init(void);
+int ull_scan_aux_reset(void);
+
+/* Helper to setup scanning on auxiliary channel */
+void ull_scan_aux_setup(struct node_rx_hdr *rx, uint8_t phy,
+			struct ll_scan_aux_set *aux);
+
+/* Helper to clean up auxiliary channel scanning */
+void ull_scan_aux_done(struct node_rx_event_done *done);