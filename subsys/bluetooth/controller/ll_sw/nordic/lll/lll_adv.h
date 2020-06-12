zephyrproject-rtos/zephyr

Bluetooth: controller: split: Add periodic adv ULL implementation
Add ULL implementation to perform periodic advertising.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv.h
@@ -13,6 +13,15 @@ struct lll_adv_pdu {
 	u8_t pdu[DOUBLE_BUFFER_SIZE][PDU_AC_SIZE_MAX];
 };
 
+struct lll_adv_sync {
+	struct lll_hdr hdr;
+
+	u8_t access_addr[4];
+	u8_t crc_init[3];
+
+	struct lll_adv_pdu data;
+};
+
 struct lll_adv {
 	struct lll_hdr hdr;
 
@@ -43,6 +52,10 @@ struct lll_adv {
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 	struct lll_adv_pdu aux_data;
+
+#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
+	struct lll_adv_sync *sync;
+#endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 #if defined(CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL)
@@ -126,6 +139,24 @@ static inline struct pdu_adv *lll_adv_aux_data_peek(struct lll_adv *lll)
 {
 	return (void *)lll->aux_data.pdu[lll->aux_data.last];
 }
+
+#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
+static inline struct pdu_adv *lll_adv_sync_data_alloc(struct lll_adv_sync *lll,
+						     u8_t *idx)
+{
+	return lll_adv_pdu_alloc(&lll->data, idx);
+}
+
+static inline void lll_adv_sync_data_enqueue(struct lll_adv_sync *lll, u8_t idx)
+{
+	lll_adv_pdu_enqueue(&lll->data, idx);
+}
+
+static inline struct pdu_adv *lll_adv_sync_data_peek(struct lll_adv_sync *lll)
+{
+	return (void *)lll->data.pdu[lll->data.last];
+}
+#endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 extern u16_t ull_adv_lll_handle_get(struct lll_adv *lll);

subsys/bluetooth/controller/ll_sw/ull_adv_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Nordic Semiconductor ASA
+ * Copyright (c) 2017-2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,16 +8,20 @@ int ull_adv_init(void);
 int ull_adv_reset(void);
 
 /* Return ll_adv_set context (unconditional) */
-struct ll_adv_set *ull_adv_set_get(u16_t handle);
+struct ll_adv_set *ull_adv_set_get(u8_t handle);
 
 /* Return the adv set handle given the adv set instance */
 u16_t ull_adv_handle_get(struct ll_adv_set *adv);
 
 /* Return ll_adv_set context if enabled */
-struct ll_adv_set *ull_adv_is_enabled_get(u16_t handle);
+struct ll_adv_set *ull_adv_is_enabled_get(u8_t handle);
 
 /* Return flags, for now just: enabled */
-u32_t ull_adv_is_enabled(u16_t handle);
+u32_t ull_adv_is_enabled(u8_t handle);
 
 /* Return filter policy used */
-u32_t ull_adv_filter_pol_get(u16_t handle);
+u32_t ull_adv_filter_pol_get(u8_t handle);
+
+/* helper function to start periodic advertising */
+u32_t ull_adv_sync_start(struct ll_adv_sync_set *sync, u32_t ticks_anchor,
+			 u32_t volatile *ret_cb);

subsys/bluetooth/controller/ll_sw/ull_adv_sync.c
@@ -1,0 +1,260 @@
+/*
+ * Copyright (c) 2017-2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <bluetooth/hci.h>
+
+#include "hal/ticker.h"
+
+#include "util/util.h"
+#include "util/mem.h"
+#include "util/memq.h"
+#include "util/mayfly.h"
+
+#include "ticker/ticker.h"
+
+#include "pdu.h"
+#include "ll.h"
+#include "lll.h"
+#include "lll_vendor.h"
+#include "lll_adv.h"
+
+#include "ull_adv_types.h"
+
+#include "ull_internal.h"
+#include "ull_adv_internal.h"
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_DRIVER)
+#define LOG_MODULE_NAME bt_ctlr_ull_adv_sync
+#include "common/log.h"
+#include <soc.h>
+#include "hal/debug.h"
+
+static int init_reset(void);
+static inline struct ll_adv_sync_set *sync_acquire(void);
+static inline void sync_release(struct ll_adv_sync_set *sync);
+static inline u16_t sync_handle_get(struct ll_adv_sync_set *sync);
+static void ticker_cb(u32_t ticks_at_expire, u32_t remainder, u16_t lazy,
+		      void *param);
+
+static struct ll_adv_sync_set ll_adv_sync_pool[CONFIG_BT_CTLR_ADV_SYNC_SET];
+static void *adv_sync_free;
+
+u8_t ll_adv_sync_param_set(u8_t handle, u16_t interval, u16_t flags)
+{
+	struct lll_adv_sync *lll;
+	struct ll_adv_sync_set *sync;
+	struct ll_adv_set *adv;
+	struct pdu_adv *pdu;
+
+	adv = ull_adv_is_enabled_get(handle);
+	if (!adv) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	lll = adv->lll.sync;
+	if (!lll) {
+		sync = sync_acquire();
+		if (!sync) {
+			return BT_HCI_ERR_MEM_CAPACITY_EXCEEDED;
+		}
+
+		lll = &sync->lll;
+		adv->lll.sync = lll;
+	} else {
+		sync = (void *)HDR_LLL2EVT(lll);
+	}
+
+	sync->interval = interval;
+
+	pdu = lll_adv_sync_data_peek(lll);
+	pdu->type = PDU_ADV_TYPE_AUX_SYNC_IND;
+	pdu->rfu = 0U;
+	pdu->chan_sel = 0U;
+	pdu->tx_addr = 0U;
+	pdu->rx_addr = 0U;
+
+	if (flags & BIT(6)) {
+		/* TODO: add/remove Tx Power in AUX_SYNC_IND PDU */
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	return 0;
+}
+
+u8_t ll_adv_sync_ad_data_set(u8_t handle, u8_t op, u8_t frag_pref, u8_t len,
+			     u8_t *data)
+{
+	/* TODO */
+	return BT_HCI_ERR_CMD_DISALLOWED;
+}
+
+u8_t ll_adv_sync_enable(u8_t handle, u8_t enable)
+{
+	struct lll_adv_sync *lll_sync;
+	struct ll_adv_sync_set *sync;
+	struct ll_adv_set *adv;
+
+	adv = ull_adv_is_enabled_get(handle);
+	if (!adv) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	lll_sync = adv->lll.sync;
+	if (!lll_sync) {
+		return BT_HCI_ERR_UNKNOWN_ADV_IDENTIFIER;
+	}
+
+	sync = (void *)HDR_LLL2EVT(lll_sync);
+
+	if (!enable) {
+		/* TODO */
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	/* TODO: Check for periodic data being complete */
+
+	/* TODO: Check packet too long */
+
+	if (sync->is_enabled) {
+		/* TODO: Enabling an already enabled advertising changes its
+		 * random address.
+		 */
+	} else {
+		sync->is_enabled = 1U;
+	}
+
+	if (!sync->is_started) {
+	}
+
+	return 0;
+}
+
+int ull_adv_sync_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int ull_adv_sync_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+u32_t ull_adv_sync_start(struct ll_adv_sync_set *sync, u32_t ticks_anchor,
+			 u32_t volatile *ret_cb)
+{
+	u32_t slot_us, ticks_slot_offset, ticks_slot_overhead;
+	u8_t sync_handle;
+	u32_t ret;
+
+	/* TODO: Calc AUX_SYNC_IND slot_us */
+	slot_us = 0;
+
+	/* TODO: active_to_start feature port */
+	sync->evt.ticks_active_to_start = 0;
+	sync->evt.ticks_xtal_to_start =
+	HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_XTAL_US);
+	sync->evt.ticks_preempt_to_start =
+	HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_PREEMPT_MIN_US);
+	sync->evt.ticks_slot = HAL_TICKER_US_TO_TICKS(slot_us);
+
+	ticks_slot_offset = MAX(sync->evt.ticks_active_to_start,
+	sync->evt.ticks_xtal_to_start);
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_LOW_LAT)) {
+		ticks_slot_overhead = ticks_slot_offset;
+	} else {
+		ticks_slot_overhead = 0;
+	}
+
+	sync_handle = sync_handle_get(sync);
+
+	*ret_cb = TICKER_STATUS_BUSY;
+	ret = ticker_start(TICKER_INSTANCE_ID_CTLR, TICKER_USER_ID_THREAD,
+			   (TICKER_ID_ADV_SYNC_BASE + sync_handle),
+			   ticks_anchor, 0,
+			   (sync->evt.ticks_slot + ticks_slot_overhead),
+			   TICKER_NULL_REMAINDER, TICKER_NULL_LAZY,
+			   (sync->evt.ticks_slot + ticks_slot_overhead),
+			   ticker_cb, sync,
+			   ull_ticker_status_give, (void *)ret_cb);
+
+	return ret;
+}
+
+static int init_reset(void)
+{
+	/* Initialize adv sync pool. */
+	mem_init(ll_adv_sync_pool, sizeof(struct ll_adv_sync_set),
+		 sizeof(ll_adv_sync_pool) / sizeof(struct ll_adv_sync_set),
+		 &adv_sync_free);
+
+	return 0;
+}
+
+static inline struct ll_adv_sync_set *sync_acquire(void)
+{
+	return mem_acquire(&adv_sync_free);
+}
+
+static inline void sync_release(struct ll_adv_sync_set *sync)
+{
+	mem_release(sync, &adv_sync_free);
+}
+
+static inline u16_t sync_handle_get(struct ll_adv_sync_set *sync)
+{
+	return mem_index_get(sync, ll_adv_sync_pool,
+			     sizeof(struct ll_adv_sync_set));
+}
+
+static void ticker_cb(u32_t ticks_at_expire, u32_t remainder, u16_t lazy,
+		      void *param)
+{
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, lll_adv_prepare};
+	static struct lll_prepare_param p;
+	struct ll_adv_sync_set *sync = param;
+	struct lll_adv_sync *lll;
+	u32_t ret;
+	u8_t ref;
+
+	DEBUG_RADIO_PREPARE_A(1);
+
+	lll = &sync->lll;
+
+	/* Increment prepare reference count */
+	ref = ull_ref_inc(&sync->ull);
+	LL_ASSERT(ref);
+
+	/* Append timing parameters */
+	p.ticks_at_expire = ticks_at_expire;
+	p.remainder = remainder;
+	p.lazy = lazy;
+	p.param = lll;
+	mfy.param = &p;
+
+	/* Kick LLL prepare */
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH,
+			     TICKER_USER_ID_LLL, 0, &mfy);
+	LL_ASSERT(!ret);
+
+	DEBUG_RADIO_PREPARE_A(1);
+}

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -30,3 +30,14 @@ struct ll_adv_set {
 	u8_t  id_addr[BDADDR_SIZE];
 #endif /* CONFIG_BT_CTLR_PRIVACY */
 };
+
+struct ll_adv_sync_set {
+	struct evt_hdr      evt;
+	struct ull_hdr      ull;
+	struct lll_adv_sync lll;
+
+	u16_t interval;
+
+	u8_t is_enabled:1;
+	u8_t is_started:1;
+};

subsys/bluetooth/controller/ll_sw/ull_conn_internal.h
@@ -60,6 +60,7 @@ void ll_conn_release(struct ll_conn *conn);
 u16_t ll_conn_handle_get(struct ll_conn *conn);
 struct ll_conn *ll_conn_get(u16_t handle);
 struct ll_conn *ll_connected_get(u16_t handle);
+void ll_tx_ack_put(u16_t handle, struct node_tx *node_tx);
 int ull_conn_init(void);
 int ull_conn_reset(void);
 u8_t ull_conn_chan_map_cpy(u8_t *chan_map);

kartben/zephyr-without-images

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv.h
@@ -13,6 +13,15 @@ struct lll_adv_pdu {
 	uint8_t pdu[DOUBLE_BUFFER_SIZE][PDU_AC_SIZE_MAX];
 };
 
+struct lll_adv_sync {
+	struct lll_hdr hdr;
+
+	uint8_t access_addr[4];
+	uint8_t crc_init[3];
+
+	struct lll_adv_pdu data;
+};
+
 struct lll_adv {
 	struct lll_hdr hdr;
 
@@ -43,6 +52,10 @@ struct lll_adv {
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
 	struct lll_adv_pdu aux_data;
+
+#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
+	struct lll_adv_sync *sync;
+#endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 #if defined(CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL)
@@ -126,6 +139,25 @@ static inline struct pdu_adv *lll_adv_aux_data_peek(struct lll_adv *lll)
 {
 	return (void *)lll->aux_data.pdu[lll->aux_data.last];
 }
+
+#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
+static inline struct pdu_adv *lll_adv_sync_data_alloc(struct lll_adv_sync *lll,
+						     uint8_t *idx)
+{
+	return lll_adv_pdu_alloc(&lll->data, idx);
+}
+
+static inline void lll_adv_sync_data_enqueue(struct lll_adv_sync *lll,
+					     uint8_t idx)
+{
+	lll_adv_pdu_enqueue(&lll->data, idx);
+}
+
+static inline struct pdu_adv *lll_adv_sync_data_peek(struct lll_adv_sync *lll)
+{
+	return (void *)lll->data.pdu[lll->data.last];
+}
+#endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 extern uint16_t ull_adv_lll_handle_get(struct lll_adv *lll);

subsys/bluetooth/controller/ll_sw/ull_adv_internal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Nordic Semiconductor ASA
+ * Copyright (c) 2017-2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,16 +8,20 @@ int ull_adv_init(void);
 int ull_adv_reset(void);
 
 /* Return ll_adv_set context (unconditional) */
-struct ll_adv_set *ull_adv_set_get(uint16_t handle);
+struct ll_adv_set *ull_adv_set_get(uint8_t handle);
 
 /* Return the adv set handle given the adv set instance */
 uint16_t ull_adv_handle_get(struct ll_adv_set *adv);
 
 /* Return ll_adv_set context if enabled */
-struct ll_adv_set *ull_adv_is_enabled_get(uint16_t handle);
+struct ll_adv_set *ull_adv_is_enabled_get(uint8_t handle);
 
 /* Return flags, for now just: enabled */
-uint32_t ull_adv_is_enabled(uint16_t handle);
+uint32_t ull_adv_is_enabled(uint8_t handle);
 
 /* Return filter policy used */
-uint32_t ull_adv_filter_pol_get(uint16_t handle);
+uint32_t ull_adv_filter_pol_get(uint8_t handle);
+
+/* helper function to start periodic advertising */
+uint32_t ull_adv_sync_start(struct ll_adv_sync_set *sync, uint32_t ticks_anchor,
+			 uint32_t volatile *ret_cb);

subsys/bluetooth/controller/ll_sw/ull_adv_sync.c
@@ -1,0 +1,260 @@
+/*
+ * Copyright (c) 2017-2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <bluetooth/hci.h>
+
+#include "hal/ticker.h"
+
+#include "util/util.h"
+#include "util/mem.h"
+#include "util/memq.h"
+#include "util/mayfly.h"
+
+#include "ticker/ticker.h"
+
+#include "pdu.h"
+#include "ll.h"
+#include "lll.h"
+#include "lll_vendor.h"
+#include "lll_adv.h"
+
+#include "ull_adv_types.h"
+
+#include "ull_internal.h"
+#include "ull_adv_internal.h"
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_DRIVER)
+#define LOG_MODULE_NAME bt_ctlr_ull_adv_sync
+#include "common/log.h"
+#include <soc.h>
+#include "hal/debug.h"
+
+static int init_reset(void);
+static inline struct ll_adv_sync_set *sync_acquire(void);
+static inline void sync_release(struct ll_adv_sync_set *sync);
+static inline uint16_t sync_handle_get(struct ll_adv_sync_set *sync);
+static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
+		      uint16_t lazy, void *param);
+
+static struct ll_adv_sync_set ll_adv_sync_pool[CONFIG_BT_CTLR_ADV_SYNC_SET];
+static void *adv_sync_free;
+
+uint8_t ll_adv_sync_param_set(uint8_t handle, uint16_t interval, uint16_t flags)
+{
+	struct lll_adv_sync *lll;
+	struct ll_adv_sync_set *sync;
+	struct ll_adv_set *adv;
+	struct pdu_adv *pdu;
+
+	adv = ull_adv_is_enabled_get(handle);
+	if (!adv) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	lll = adv->lll.sync;
+	if (!lll) {
+		sync = sync_acquire();
+		if (!sync) {
+			return BT_HCI_ERR_MEM_CAPACITY_EXCEEDED;
+		}
+
+		lll = &sync->lll;
+		adv->lll.sync = lll;
+	} else {
+		sync = (void *)HDR_LLL2EVT(lll);
+	}
+
+	sync->interval = interval;
+
+	pdu = lll_adv_sync_data_peek(lll);
+	pdu->type = PDU_ADV_TYPE_AUX_SYNC_IND;
+	pdu->rfu = 0U;
+	pdu->chan_sel = 0U;
+	pdu->tx_addr = 0U;
+	pdu->rx_addr = 0U;
+
+	if (flags & BIT(6)) {
+		/* TODO: add/remove Tx Power in AUX_SYNC_IND PDU */
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	return 0;
+}
+
+uint8_t ll_adv_sync_ad_data_set(uint8_t handle, uint8_t op, uint8_t frag_pref,
+				uint8_t len, uint8_t *data)
+{
+	/* TODO */
+	return BT_HCI_ERR_CMD_DISALLOWED;
+}
+
+uint8_t ll_adv_sync_enable(uint8_t handle, uint8_t enable)
+{
+	struct lll_adv_sync *lll_sync;
+	struct ll_adv_sync_set *sync;
+	struct ll_adv_set *adv;
+
+	adv = ull_adv_is_enabled_get(handle);
+	if (!adv) {
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	lll_sync = adv->lll.sync;
+	if (!lll_sync) {
+		return BT_HCI_ERR_UNKNOWN_ADV_IDENTIFIER;
+	}
+
+	sync = (void *)HDR_LLL2EVT(lll_sync);
+
+	if (!enable) {
+		/* TODO */
+		return BT_HCI_ERR_CMD_DISALLOWED;
+	}
+
+	/* TODO: Check for periodic data being complete */
+
+	/* TODO: Check packet too long */
+
+	if (sync->is_enabled) {
+		/* TODO: Enabling an already enabled advertising changes its
+		 * random address.
+		 */
+	} else {
+		sync->is_enabled = 1U;
+	}
+
+	if (!sync->is_started) {
+	}
+
+	return 0;
+}
+
+int ull_adv_sync_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int ull_adv_sync_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+uint32_t ull_adv_sync_start(struct ll_adv_sync_set *sync, uint32_t ticks_anchor,
+			 uint32_t volatile *ret_cb)
+{
+	uint32_t slot_us, ticks_slot_offset, ticks_slot_overhead;
+	uint8_t sync_handle;
+	uint32_t ret;
+
+	/* TODO: Calc AUX_SYNC_IND slot_us */
+	slot_us = 0;
+
+	/* TODO: active_to_start feature port */
+	sync->evt.ticks_active_to_start = 0;
+	sync->evt.ticks_xtal_to_start =
+	HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_XTAL_US);
+	sync->evt.ticks_preempt_to_start =
+	HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_PREEMPT_MIN_US);
+	sync->evt.ticks_slot = HAL_TICKER_US_TO_TICKS(slot_us);
+
+	ticks_slot_offset = MAX(sync->evt.ticks_active_to_start,
+	sync->evt.ticks_xtal_to_start);
+
+	if (IS_ENABLED(CONFIG_BT_CTLR_LOW_LAT)) {
+		ticks_slot_overhead = ticks_slot_offset;
+	} else {
+		ticks_slot_overhead = 0;
+	}
+
+	sync_handle = sync_handle_get(sync);
+
+	*ret_cb = TICKER_STATUS_BUSY;
+	ret = ticker_start(TICKER_INSTANCE_ID_CTLR, TICKER_USER_ID_THREAD,
+			   (TICKER_ID_ADV_SYNC_BASE + sync_handle),
+			   ticks_anchor, 0,
+			   (sync->evt.ticks_slot + ticks_slot_overhead),
+			   TICKER_NULL_REMAINDER, TICKER_NULL_LAZY,
+			   (sync->evt.ticks_slot + ticks_slot_overhead),
+			   ticker_cb, sync,
+			   ull_ticker_status_give, (void *)ret_cb);
+
+	return ret;
+}
+
+static int init_reset(void)
+{
+	/* Initialize adv sync pool. */
+	mem_init(ll_adv_sync_pool, sizeof(struct ll_adv_sync_set),
+		 sizeof(ll_adv_sync_pool) / sizeof(struct ll_adv_sync_set),
+		 &adv_sync_free);
+
+	return 0;
+}
+
+static inline struct ll_adv_sync_set *sync_acquire(void)
+{
+	return mem_acquire(&adv_sync_free);
+}
+
+static inline void sync_release(struct ll_adv_sync_set *sync)
+{
+	mem_release(sync, &adv_sync_free);
+}
+
+static inline uint16_t sync_handle_get(struct ll_adv_sync_set *sync)
+{
+	return mem_index_get(sync, ll_adv_sync_pool,
+			     sizeof(struct ll_adv_sync_set));
+}
+
+static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
+		      uint16_t lazy, void *param)
+{
+	static memq_link_t link;
+	static struct mayfly mfy = {0, 0, &link, NULL, lll_adv_prepare};
+	static struct lll_prepare_param p;
+	struct ll_adv_sync_set *sync = param;
+	struct lll_adv_sync *lll;
+	uint32_t ret;
+	uint8_t ref;
+
+	DEBUG_RADIO_PREPARE_A(1);
+
+	lll = &sync->lll;
+
+	/* Increment prepare reference count */
+	ref = ull_ref_inc(&sync->ull);
+	LL_ASSERT(ref);
+
+	/* Append timing parameters */
+	p.ticks_at_expire = ticks_at_expire;
+	p.remainder = remainder;
+	p.lazy = lazy;
+	p.param = lll;
+	mfy.param = &p;
+
+	/* Kick LLL prepare */
+	ret = mayfly_enqueue(TICKER_USER_ID_ULL_HIGH,
+			     TICKER_USER_ID_LLL, 0, &mfy);
+	LL_ASSERT(!ret);
+
+	DEBUG_RADIO_PREPARE_A(1);
+}

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -30,3 +30,14 @@ struct ll_adv_set {
 	uint8_t  id_addr[BDADDR_SIZE];
 #endif /* CONFIG_BT_CTLR_PRIVACY */
 };
+
+struct ll_adv_sync_set {
+	struct evt_hdr      evt;
+	struct ull_hdr      ull;
+	struct lll_adv_sync lll;
+
+	uint16_t interval;
+
+	uint8_t is_enabled:1;
+	uint8_t is_started:1;
+};

subsys/bluetooth/controller/ll_sw/ull_conn_internal.h
@@ -60,6 +60,7 @@ void ll_conn_release(struct ll_conn *conn);
 uint16_t ll_conn_handle_get(struct ll_conn *conn);
 struct ll_conn *ll_conn_get(uint16_t handle);
 struct ll_conn *ll_connected_get(uint16_t handle);
+void ll_tx_ack_put(uint16_t handle, struct node_tx *node_tx);
 int ull_conn_init(void);
 int ull_conn_reset(void);
 uint8_t ull_conn_chan_map_cpy(uint8_t *chan_map);