zephyrproject-rtos/zephyr

Bluetooth: controller: split: Implement Aux Sets
Implement use of configurable Auxiliary advertising sets.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.c
@@ -8,9 +8,11 @@
 
 #include <toolchain.h>
 #include <zephyr/types.h>
+#include <sys/byteorder.h>
 
 #include "hal/ccm.h"
 #include "hal/radio.h"
+#include "hal/ticker.h"
 
 #include "util/util.h"
 #include "util/memq.h"
@@ -18,9 +20,11 @@
 #include "pdu.h"
 
 #include "lll.h"
+#include "lll_vendor.h"
 #include "lll_clock.h"
-#include "lll_adv.h"
 #include "lll_chan.h"
+#include "lll_adv.h"
+#include "lll_adv_aux.h"
 
 #include "lll_internal.h"
 #include "lll_adv_internal.h"
@@ -31,28 +35,107 @@
 #include <soc.h>
 #include "hal/debug.h"
 
-void lll_adv_aux_prepare(struct lll_adv *lll)
+static int init_reset(void);
+static int prepare_cb(struct lll_prepare_param *prepare_param);
+static void abort_cb(struct lll_prepare_param *prepare_param, void *param);
+
+int lll_adv_aux_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int lll_adv_aux_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+void lll_adv_aux_prepare(void *param)
+{
+	struct lll_prepare_param *p = param;
+	int err;
+
+	err = lll_hfclock_on();
+	LL_ASSERT(!err || err == -EINPROGRESS);
+
+	err = lll_prepare(lll_is_abort_cb, abort_cb, prepare_cb, 0, p);
+	LL_ASSERT(!err || err == -EINPROGRESS);
+}
+
+void lll_adv_aux_offset_fill(u32_t ticks_offset, u32_t start_us,
+			     struct pdu_adv *pdu)
 {
 	struct pdu_adv_com_ext_adv *p;
 	struct ext_adv_aux_ptr *aux;
-	struct pdu_adv *pri, *sec;
 	struct ext_adv_hdr *h;
-	u32_t start_us;
 	u8_t *ptr;
-	u8_t upd = 0U;
 
-	/* TODO: if coded we use S8? */
-	radio_phy_set(lll->phy_s, 1);
-	radio_pkt_configure(8, PDU_AC_PAYLOAD_SIZE_MAX, (lll->phy_s << 1));
+	p = (void *)&pdu->adv_ext_ind;
+	h = (void *)p->ext_hdr_adi_adv_data;
+	ptr = (u8_t *)h + sizeof(*h);
 
-	/* AUX_ADV_IND PDU buffer get */
+	if (h->adv_addr) {
+		ptr += BDADDR_SIZE;
+	}
+
+	if (h->adi) {
+		ptr += sizeof(struct ext_adv_adi);
+	}
+
+	aux = (void *)ptr;
+	aux->offs = (HAL_TICKER_TICKS_TO_US(ticks_offset) - start_us) / 30;
+	if (aux->offs_units) {
+		aux->offs /= 10;
+	}
+}
+
+void lll_adv_aux_pback_prepare(void *param)
+{
+}
+
+static int init_reset(void)
+{
+	return 0;
+}
+
+static int prepare_cb(struct lll_prepare_param *prepare_param)
+{
+	struct lll_adv_aux *lll = prepare_param->param;
+	u32_t aa = sys_cpu_to_le32(PDU_AC_ACCESS_ADDR);
+	u32_t ticks_at_event, ticks_at_start;
+	struct pdu_adv_com_ext_adv *p;
+	struct ext_adv_aux_ptr *aux;
+	struct pdu_adv *pri, *sec;
+	struct lll_adv *lll_adv;
+	struct ext_adv_hdr *h;
+	struct evt_hdr *evt;
+	u32_t remainder;
+	u32_t start_us;
+	u8_t phy_s;
+	u8_t *ptr;
+	u8_t upd;
+
+	DEBUG_RADIO_START_A(1);
+
 	/* FIXME: get latest only when primary PDU without Aux PDUs */
 	sec = lll_adv_aux_data_latest_get(lll, &upd);
 
-	radio_pkt_tx_set(sec);
-
 	/* Get reference to primary PDU aux_ptr */
-	pri = lll_adv_data_curr_get(lll);
+	lll_adv = lll->adv;
+	pri = lll_adv_data_curr_get(lll_adv);
 	p = (void *)&pri->adv_ext_ind;
 	h = (void *)p->ext_hdr_adi_adv_data;
 	ptr = (u8_t *)h + sizeof(*h);
@@ -68,23 +151,109 @@ void lll_adv_aux_prepare(struct lll_adv *lll)
 	}
 	aux = (void *)ptr;
 
+#if !defined(CONFIG_BT_CTLR_ADV_EXT_PBACK)
+	/* Set up Radio H/W */
+	radio_reset();
+#endif  /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
+
+#if defined(CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL)
+	radio_tx_power_set(lll->tx_pwr_lvl);
+#else
+	radio_tx_power_set(RADIO_TXP_DEFAULT);
+#endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
+
+	phy_s = lll_adv->phy_s;
+
+	/* TODO: if coded we use S8? */
+	radio_phy_set(phy_s, 1);
+	radio_pkt_configure(8, PDU_AC_PAYLOAD_SIZE_MAX, (phy_s << 1));
+
+#if !defined(CONFIG_BT_CTLR_ADV_EXT_PBACK)
+	/* Access address and CRC */
+	radio_aa_set((u8_t *)&aa);
+	radio_crc_configure(((0x5bUL) | ((0x06UL) << 8) | ((0x00UL) << 16)),
+			    0x555555);
+#endif  /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
+
 	/* Use channel idx in aux_ptr */
 	lll_chan_set(aux->chan_idx);
+
+	/* Set the Radio Tx Packet */
+	radio_pkt_tx_set(sec);
 
 	/* TODO: Based on adv_mode switch to Rx, if needed */
 	radio_isr_set(lll_isr_done, lll);
 	radio_switch_complete_and_disable();
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT_PBACK)
 	start_us = 1000;
 	radio_tmr_start_us(1, start_us);
+#else /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
+
+	ticks_at_event = prepare_param->ticks_at_expire;
+	evt = HDR_LLL2EVT(lll);
+	ticks_at_event += lll_evt_offset_get(evt);
+
+	ticks_at_start = ticks_at_event;
+	ticks_at_start += HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_START_US);
+
+	remainder = prepare_param->remainder;
+	start_us = radio_tmr_start(1, ticks_at_start, remainder);
+#endif /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
 
 	/* capture end of Tx-ed PDU, used to calculate HCTO. */
 	radio_tmr_end_capture();
 
 #if defined(CONFIG_BT_CTLR_GPIO_PA_PIN)
 	radio_gpio_pa_setup();
-	radio_gpio_pa_lna_enable(start_us +
-				 radio_tx_ready_delay_get(0, 0) -
+	radio_gpio_pa_lna_enable(start_us + radio_tx_ready_delay_get(phy_s, 1) -
 				 CONFIG_BT_CTLR_GPIO_PA_OFFSET);
+#else /* !CONFIG_BT_CTLR_GPIO_PA_PIN */
+	ARG_UNUSED(start_us);
 #endif /* !CONFIG_BT_CTLR_GPIO_PA_PIN */
+
+#if defined(CONFIG_BT_CTLR_XTAL_ADVANCED) && \
+	(EVENT_OVERHEAD_PREEMPT_US <= EVENT_OVERHEAD_PREEMPT_MIN_US)
+	/* check if preempt to start has changed */
+	if (lll_preempt_calc(evt, (TICKER_ID_ADV_AUX_BASE +
+				   ull_adv_aux_lll_handle_get(lll)),
+			     ticks_at_event)) {
+		radio_isr_set(lll_isr_abort, lll);
+		radio_disable();
+	} else
+#endif /* CONFIG_BT_CTLR_XTAL_ADVANCED */
+	{
+		u32_t ret;
+
+		ret = lll_prepare_done(lll);
+		LL_ASSERT(!ret);
+	}
+
+	DEBUG_RADIO_START_A(1);
+
+	return 0;
+}
+
+static void abort_cb(struct lll_prepare_param *prepare_param, void *param)
+{
+	int err;
+
+	/* NOTE: This is not a prepare being cancelled */
+	if (!prepare_param) {
+		/* Perform event abort here.
+		 * After event has been cleanly aborted, clean up resources
+		 * and dispatch event done.
+		 */
+		radio_isr_set(lll_isr_done, param);
+		radio_disable();
+		return;
+	}
+
+	/* NOTE: Else clean the top half preparations of the aborted event
+	 * currently in preparation pipeline.
+	 */
+	err = lll_hfclock_off();
+	LL_ASSERT(!err || err == -EBUSY);
+
+	lll_done(param);
 }

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.h
@@ -4,4 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-void lll_adv_aux_prepare(struct lll_adv *lll);
+int lll_adv_aux_init(void);
+int lll_adv_aux_reset(void);
+void lll_adv_aux_prepare(void *param);
+void lll_adv_aux_offset_fill(u32_t ticks_offset, u32_t start_us,
+			     struct pdu_adv *pdu);
+
+
+extern u16_t ull_adv_aux_lll_handle_get(struct lll_adv_aux *lll);

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_internal.h
@@ -45,15 +45,15 @@ static inline struct pdu_adv *lll_adv_scan_rsp_curr_get(struct lll_adv *lll)
 }
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
-static inline struct pdu_adv *lll_adv_aux_data_latest_get(struct lll_adv *lll,
-							  u8_t *is_modified)
+static inline struct pdu_adv *
+lll_adv_aux_data_latest_get(struct lll_adv_aux *lll, u8_t *is_modified)
 {
-	return lll_adv_pdu_latest_get(&lll->aux_data, is_modified);
+	return lll_adv_pdu_latest_get(&lll->data, is_modified);
 }
 
-static inline struct pdu_adv *lll_adv_aux_data_curr_get(struct lll_adv *lll)
+static inline struct pdu_adv *lll_adv_aux_data_curr_get(struct lll_adv_aux *lll)
 {
-	return (void *)lll->aux_data.pdu[lll->aux_data.first];
+	return (void *)lll->data.pdu[lll->data.first];
 }
 
 #if defined(CONFIG_BT_CTLR_ADV_PERIODIC)

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_sync.h
@@ -7,5 +7,7 @@
 int lll_adv_sync_init(void);
 int lll_adv_sync_reset(void);
 void lll_adv_sync_prepare(void *param);
+void lll_adv_sync_offset_fill(u32_t ticks_offset, u32_t start_us,
+			     struct pdu_adv *pdu);
 
 extern u16_t ull_adv_sync_lll_handle_get(struct lll_adv_sync *lll);

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -50,8 +50,6 @@
 #include <soc.h>
 #include "hal/debug.h"
 
-#define ULL_ADV_RANDOM_DELAY HAL_TICKER_US_TO_TICKS(10000)
-
 inline struct ll_adv_set *ull_adv_set_get(u8_t handle);
 inline u16_t ull_adv_handle_get(struct ll_adv_set *adv);
 
@@ -468,6 +466,8 @@ u8_t ll_adv_enable(u8_t handle, u8_t enable)
 	struct ll_adv_sync_set *sync;
 	u8_t sync_is_started = 0U;
 #endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
+	struct ll_adv_aux_set *aux;
+	u8_t aux_is_started = 0U;
 	u32_t ticks_anchor;
 #endif /* !CONFIG_BT_HCI_MESH_EXT */
 #else /* !CONFIG_BT_CTLR_ADV_EXT || !CONFIG_BT_HCI_MESH_EXT */
@@ -530,7 +530,7 @@ u8_t ll_adv_enable(u8_t enable)
 			struct ext_adv_hdr *hs;
 			u8_t *ps;
 
-			pdu_aux = lll_adv_aux_data_peek(lll);
+			pdu_aux = lll_adv_aux_data_peek(lll->aux);
 
 			s = (void *)&pdu_aux->adv_ext_ind;
 			hs = (void *)s->ext_hdr_adi_adv_data;
@@ -984,24 +984,48 @@ u8_t ll_adv_enable(u8_t enable)
 #endif /* CONFIG_BT_TICKER_EXT */
 				   );
 
-#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
-		if (lll->sync) {
-			struct lll_adv_sync *lll_sync = lll->sync;
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+		if (lll->aux) {
+			struct lll_adv_aux *lll_aux = lll->aux;
 
-			sync = (void *)HDR_LLL2EVT(lll_sync);
-			if (sync->is_enabled && !sync->is_started) {
-				ret = ull_ticker_status_take(ret, &ret_cb);
-				if (ret != TICKER_STATUS_SUCCESS) {
-					goto failure_cleanup;
-				}
-
-				ret = ull_adv_sync_start(sync, ticks_anchor,
-							 &ret_cb);
-
-				sync_is_started = 1U;
+			ret = ull_ticker_status_take(ret, &ret_cb);
+			if (ret != TICKER_STATUS_SUCCESS) {
+				goto failure_cleanup;
 			}
-		}
+
+			aux = (void *)HDR_LLL2EVT(lll_aux);
+			ull_hdr_init(&aux->ull);
+			aux->interval =
+				adv->interval +
+				(HAL_TICKER_TICKS_TO_US(ULL_ADV_RANDOM_DELAY) /
+				 625U);
+
+			ret = ull_adv_aux_start(aux, ticks_anchor, &ret_cb);
+
+			aux_is_started = 1U;
+
+#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
+			if (lll->sync) {
+				struct lll_adv_sync *lll_sync = lll->sync;
+
+				sync = (void *)HDR_LLL2EVT(lll_sync);
+				if (sync->is_enabled && !sync->is_started) {
+					ret = ull_ticker_status_take(ret,
+								     &ret_cb);
+					if (ret != TICKER_STATUS_SUCCESS) {
+						goto failure_cleanup;
+					}
+
+					ret = ull_adv_sync_start(sync,
+								 ticks_anchor,
+								 &ret_cb);
+
+					sync_is_started = 1U;
+				}
+			}
 #endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
+		}
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
 	}
 
 	ret = ull_ticker_status_take(ret, &ret_cb);
@@ -1009,11 +1033,17 @@ u8_t ll_adv_enable(u8_t enable)
 		goto failure_cleanup;
 	}
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+	if (aux_is_started) {
+		aux->is_started = aux_is_started;
+
 #if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
-	if (sync_is_started) {
-		sync->is_started = sync_is_started;
-	}
+		if (sync_is_started) {
+			sync->is_started = sync_is_started;
+		}
 #endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
+	}
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 	adv->is_enabled = 1;
 
@@ -1048,10 +1078,21 @@ int ull_adv_init(void)
 {
 	int err;
 
-	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
-		err = ull_adv_sync_init();
-		if (err) {
-			return err;
+	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_EXT)) {
+#if defined(CONFIG_BT_CTLR_ADV_AUX_SET)
+		if (CONFIG_BT_CTLR_ADV_AUX_SET > 0) {
+			err = ull_adv_aux_init();
+			if (err) {
+				return err;
+			}
+		}
+#endif
+
+		if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
+			err = ull_adv_sync_init();
+			if (err) {
+				return err;
+			}
 		}
 	}
 
@@ -1068,10 +1109,21 @@ int ull_adv_reset(void)
 	u8_t handle;
 	int err;
 
-	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
-		err = ull_adv_sync_reset();
-		if (err) {
-			return err;
+	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_EXT)) {
+#if defined(CONFIG_BT_CTLR_ADV_AUX_SET)
+		if (CONFIG_BT_CTLR_ADV_AUX_SET > 0) {
+			err = ull_adv_aux_reset();
+			if (err) {
+				return err;
+			}
+		}
+#endif
+
+		if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
+			err = ull_adv_sync_reset();
+			if (err) {
+				return err;
+			}
 		}
 	}
 
@@ -1230,6 +1282,12 @@ static void ticker_cb(u32_t ticks_at_expire, u32_t remainder, u16_t lazy,
 			  (ret == TICKER_STATUS_BUSY));
 	}
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT) && (CONFIG_BT_CTLR_ADV_AUX_SET > 0)
+	if (adv->lll.aux) {
+		ull_adv_aux_offset_get(adv);
+	}
+#endif /* CONFIG_BT_CTLR_ADV_EXT && (CONFIG_BT_CTLR_ADV_AUX_SET > 0) */
+
 	DEBUG_RADIO_PREPARE_A(1);
 }
 
@@ -1377,6 +1435,22 @@ static inline u8_t disable(u8_t handle)
 	if (!adv) {
 		return BT_HCI_ERR_CMD_DISALLOWED;
 	}
+
+#if defined(CONFIG_BT_CTLR_ADV_EXT) && (CONFIG_BT_CTLR_ADV_AUX_SET > 0)
+	struct lll_adv_aux *lll_aux = adv->lll.aux;
+
+	if (lll_aux) {
+		struct ll_adv_aux_set *aux;
+		u8_t err;
+
+		aux = (void *)HDR_LLL2EVT(lll_aux);
+
+		err = ull_adv_aux_stop(aux);
+		if (err) {
+			return err;
+		}
+	}
+#endif /* CONFIG_BT_CTLR_ADV_EXT && (CONFIG_BT_CTLR_ADV_AUX_SET > 0) */
 
 	mark = ull_disable_mark(adv);
 	LL_ASSERT(mark == adv);

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -31,6 +31,16 @@ struct ll_adv_set {
 #endif /* CONFIG_BT_CTLR_PRIVACY */
 };
 
+struct ll_adv_aux_set {
+	struct evt_hdr     evt;
+	struct ull_hdr     ull;
+	struct lll_adv_aux lll;
+
+	u16_t interval;
+
+	u8_t is_started:1;
+};
+
 struct ll_adv_sync_set {
 	struct evt_hdr      evt;
 	struct ull_hdr      ull;

kartben/zephyr-without-images

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.c
@@ -8,9 +8,11 @@
 
 #include <toolchain.h>
 #include <zephyr/types.h>
+#include <sys/byteorder.h>
 
 #include "hal/ccm.h"
 #include "hal/radio.h"
+#include "hal/ticker.h"
 
 #include "util/util.h"
 #include "util/memq.h"
@@ -18,9 +20,11 @@
 #include "pdu.h"
 
 #include "lll.h"
+#include "lll_vendor.h"
 #include "lll_clock.h"
-#include "lll_adv.h"
 #include "lll_chan.h"
+#include "lll_adv.h"
+#include "lll_adv_aux.h"
 
 #include "lll_internal.h"
 #include "lll_adv_internal.h"
@@ -31,28 +35,107 @@
 #include <soc.h>
 #include "hal/debug.h"
 
-void lll_adv_aux_prepare(struct lll_adv *lll)
+static int init_reset(void);
+static int prepare_cb(struct lll_prepare_param *prepare_param);
+static void abort_cb(struct lll_prepare_param *prepare_param, void *param);
+
+int lll_adv_aux_init(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+int lll_adv_aux_reset(void)
+{
+	int err;
+
+	err = init_reset();
+	if (err) {
+		return err;
+	}
+
+	return 0;
+}
+
+void lll_adv_aux_prepare(void *param)
+{
+	struct lll_prepare_param *p = param;
+	int err;
+
+	err = lll_hfclock_on();
+	LL_ASSERT(!err || err == -EINPROGRESS);
+
+	err = lll_prepare(lll_is_abort_cb, abort_cb, prepare_cb, 0, p);
+	LL_ASSERT(!err || err == -EINPROGRESS);
+}
+
+void lll_adv_aux_offset_fill(uint32_t ticks_offset, uint32_t start_us,
+			     struct pdu_adv *pdu)
 {
 	struct pdu_adv_com_ext_adv *p;
 	struct ext_adv_aux_ptr *aux;
-	struct pdu_adv *pri, *sec;
 	struct ext_adv_hdr *h;
-	uint32_t start_us;
 	uint8_t *ptr;
-	uint8_t upd = 0U;
 
-	/* TODO: if coded we use S8? */
-	radio_phy_set(lll->phy_s, 1);
-	radio_pkt_configure(8, PDU_AC_PAYLOAD_SIZE_MAX, (lll->phy_s << 1));
+	p = (void *)&pdu->adv_ext_ind;
+	h = (void *)p->ext_hdr_adi_adv_data;
+	ptr = (uint8_t *)h + sizeof(*h);
 
-	/* AUX_ADV_IND PDU buffer get */
+	if (h->adv_addr) {
+		ptr += BDADDR_SIZE;
+	}
+
+	if (h->adi) {
+		ptr += sizeof(struct ext_adv_adi);
+	}
+
+	aux = (void *)ptr;
+	aux->offs = (HAL_TICKER_TICKS_TO_US(ticks_offset) - start_us) / 30;
+	if (aux->offs_units) {
+		aux->offs /= 10;
+	}
+}
+
+void lll_adv_aux_pback_prepare(void *param)
+{
+}
+
+static int init_reset(void)
+{
+	return 0;
+}
+
+static int prepare_cb(struct lll_prepare_param *prepare_param)
+{
+	struct lll_adv_aux *lll = prepare_param->param;
+	uint32_t aa = sys_cpu_to_le32(PDU_AC_ACCESS_ADDR);
+	uint32_t ticks_at_event, ticks_at_start;
+	struct pdu_adv_com_ext_adv *p;
+	struct ext_adv_aux_ptr *aux;
+	struct pdu_adv *pri, *sec;
+	struct lll_adv *lll_adv;
+	struct ext_adv_hdr *h;
+	struct evt_hdr *evt;
+	uint32_t remainder;
+	uint32_t start_us;
+	uint8_t phy_s;
+	uint8_t *ptr;
+	uint8_t upd;
+
+	DEBUG_RADIO_START_A(1);
+
 	/* FIXME: get latest only when primary PDU without Aux PDUs */
 	sec = lll_adv_aux_data_latest_get(lll, &upd);
 
-	radio_pkt_tx_set(sec);
-
 	/* Get reference to primary PDU aux_ptr */
-	pri = lll_adv_data_curr_get(lll);
+	lll_adv = lll->adv;
+	pri = lll_adv_data_curr_get(lll_adv);
 	p = (void *)&pri->adv_ext_ind;
 	h = (void *)p->ext_hdr_adi_adv_data;
 	ptr = (uint8_t *)h + sizeof(*h);
@@ -68,23 +151,109 @@ void lll_adv_aux_prepare(struct lll_adv *lll)
 	}
 	aux = (void *)ptr;
 
+#if !defined(CONFIG_BT_CTLR_ADV_EXT_PBACK)
+	/* Set up Radio H/W */
+	radio_reset();
+#endif  /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
+
+#if defined(CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL)
+	radio_tx_power_set(lll->tx_pwr_lvl);
+#else
+	radio_tx_power_set(RADIO_TXP_DEFAULT);
+#endif /* CONFIG_BT_CTLR_TX_PWR_DYNAMIC_CONTROL */
+
+	phy_s = lll_adv->phy_s;
+
+	/* TODO: if coded we use S8? */
+	radio_phy_set(phy_s, 1);
+	radio_pkt_configure(8, PDU_AC_PAYLOAD_SIZE_MAX, (phy_s << 1));
+
+#if !defined(CONFIG_BT_CTLR_ADV_EXT_PBACK)
+	/* Access address and CRC */
+	radio_aa_set((uint8_t *)&aa);
+	radio_crc_configure(((0x5bUL) | ((0x06UL) << 8) | ((0x00UL) << 16)),
+			    0x555555);
+#endif  /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
+
 	/* Use channel idx in aux_ptr */
 	lll_chan_set(aux->chan_idx);
+
+	/* Set the Radio Tx Packet */
+	radio_pkt_tx_set(sec);
 
 	/* TODO: Based on adv_mode switch to Rx, if needed */
 	radio_isr_set(lll_isr_done, lll);
 	radio_switch_complete_and_disable();
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT_PBACK)
 	start_us = 1000;
 	radio_tmr_start_us(1, start_us);
+#else /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
+
+	ticks_at_event = prepare_param->ticks_at_expire;
+	evt = HDR_LLL2EVT(lll);
+	ticks_at_event += lll_evt_offset_get(evt);
+
+	ticks_at_start = ticks_at_event;
+	ticks_at_start += HAL_TICKER_US_TO_TICKS(EVENT_OVERHEAD_START_US);
+
+	remainder = prepare_param->remainder;
+	start_us = radio_tmr_start(1, ticks_at_start, remainder);
+#endif /* !CONFIG_BT_CTLR_ADV_EXT_PBACK */
 
 	/* capture end of Tx-ed PDU, used to calculate HCTO. */
 	radio_tmr_end_capture();
 
 #if defined(CONFIG_BT_CTLR_GPIO_PA_PIN)
 	radio_gpio_pa_setup();
-	radio_gpio_pa_lna_enable(start_us +
-				 radio_tx_ready_delay_get(0, 0) -
+	radio_gpio_pa_lna_enable(start_us + radio_tx_ready_delay_get(phy_s, 1) -
 				 CONFIG_BT_CTLR_GPIO_PA_OFFSET);
+#else /* !CONFIG_BT_CTLR_GPIO_PA_PIN */
+	ARG_UNUSED(start_us);
 #endif /* !CONFIG_BT_CTLR_GPIO_PA_PIN */
+
+#if defined(CONFIG_BT_CTLR_XTAL_ADVANCED) && \
+	(EVENT_OVERHEAD_PREEMPT_US <= EVENT_OVERHEAD_PREEMPT_MIN_US)
+	/* check if preempt to start has changed */
+	if (lll_preempt_calc(evt, (TICKER_ID_ADV_AUX_BASE +
+				   ull_adv_aux_lll_handle_get(lll)),
+			     ticks_at_event)) {
+		radio_isr_set(lll_isr_abort, lll);
+		radio_disable();
+	} else
+#endif /* CONFIG_BT_CTLR_XTAL_ADVANCED */
+	{
+		uint32_t ret;
+
+		ret = lll_prepare_done(lll);
+		LL_ASSERT(!ret);
+	}
+
+	DEBUG_RADIO_START_A(1);
+
+	return 0;
+}
+
+static void abort_cb(struct lll_prepare_param *prepare_param, void *param)
+{
+	int err;
+
+	/* NOTE: This is not a prepare being cancelled */
+	if (!prepare_param) {
+		/* Perform event abort here.
+		 * After event has been cleanly aborted, clean up resources
+		 * and dispatch event done.
+		 */
+		radio_isr_set(lll_isr_done, param);
+		radio_disable();
+		return;
+	}
+
+	/* NOTE: Else clean the top half preparations of the aborted event
+	 * currently in preparation pipeline.
+	 */
+	err = lll_hfclock_off();
+	LL_ASSERT(!err || err == -EBUSY);
+
+	lll_done(param);
 }

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_aux.h
@@ -4,4 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-void lll_adv_aux_prepare(struct lll_adv *lll);
+int lll_adv_aux_init(void);
+int lll_adv_aux_reset(void);
+void lll_adv_aux_prepare(void *param);
+void lll_adv_aux_offset_fill(uint32_t ticks_offset, uint32_t start_us,
+			     struct pdu_adv *pdu);
+
+
+extern uint16_t ull_adv_aux_lll_handle_get(struct lll_adv_aux *lll);

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_internal.h
@@ -45,15 +45,15 @@ static inline struct pdu_adv *lll_adv_scan_rsp_curr_get(struct lll_adv *lll)
 }
 
 #if defined(CONFIG_BT_CTLR_ADV_EXT)
-static inline struct pdu_adv *lll_adv_aux_data_latest_get(struct lll_adv *lll,
-							  uint8_t *is_modified)
+static inline struct pdu_adv *
+lll_adv_aux_data_latest_get(struct lll_adv_aux *lll, uint8_t *is_modified)
 {
-	return lll_adv_pdu_latest_get(&lll->aux_data, is_modified);
+	return lll_adv_pdu_latest_get(&lll->data, is_modified);
 }
 
-static inline struct pdu_adv *lll_adv_aux_data_curr_get(struct lll_adv *lll)
+static inline struct pdu_adv *lll_adv_aux_data_curr_get(struct lll_adv_aux *lll)
 {
-	return (void *)lll->aux_data.pdu[lll->aux_data.first];
+	return (void *)lll->data.pdu[lll->data.first];
 }
 
 #if defined(CONFIG_BT_CTLR_ADV_PERIODIC)

subsys/bluetooth/controller/ll_sw/nordic/lll/lll_adv_sync.h
@@ -7,5 +7,7 @@
 int lll_adv_sync_init(void);
 int lll_adv_sync_reset(void);
 void lll_adv_sync_prepare(void *param);
+void lll_adv_sync_offset_fill(uint32_t ticks_offset, uint32_t start_us,
+			     struct pdu_adv *pdu);
 
 extern uint16_t ull_adv_sync_lll_handle_get(struct lll_adv_sync *lll);

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -50,8 +50,6 @@
 #include <soc.h>
 #include "hal/debug.h"
 
-#define ULL_ADV_RANDOM_DELAY HAL_TICKER_US_TO_TICKS(10000)
-
 inline struct ll_adv_set *ull_adv_set_get(uint8_t handle);
 inline uint16_t ull_adv_handle_get(struct ll_adv_set *adv);
 
@@ -469,6 +467,8 @@ uint8_t ll_adv_enable(uint8_t handle, uint8_t enable)
 	struct ll_adv_sync_set *sync;
 	uint8_t sync_is_started = 0U;
 #endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
+	struct ll_adv_aux_set *aux;
+	uint8_t aux_is_started = 0U;
 	uint32_t ticks_anchor;
 #endif /* !CONFIG_BT_HCI_MESH_EXT */
 #else /* !CONFIG_BT_CTLR_ADV_EXT || !CONFIG_BT_HCI_MESH_EXT */
@@ -531,7 +531,7 @@ uint8_t ll_adv_enable(uint8_t enable)
 			struct ext_adv_hdr *hs;
 			uint8_t *ps;
 
-			pdu_aux = lll_adv_aux_data_peek(lll);
+			pdu_aux = lll_adv_aux_data_peek(lll->aux);
 
 			s = (void *)&pdu_aux->adv_ext_ind;
 			hs = (void *)s->ext_hdr_adi_adv_data;
@@ -986,24 +986,48 @@ uint8_t ll_adv_enable(uint8_t enable)
 #endif /* CONFIG_BT_TICKER_EXT */
 				   );
 
-#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
-		if (lll->sync) {
-			struct lll_adv_sync *lll_sync = lll->sync;
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+		if (lll->aux) {
+			struct lll_adv_aux *lll_aux = lll->aux;
 
-			sync = (void *)HDR_LLL2EVT(lll_sync);
-			if (sync->is_enabled && !sync->is_started) {
-				ret = ull_ticker_status_take(ret, &ret_cb);
-				if (ret != TICKER_STATUS_SUCCESS) {
-					goto failure_cleanup;
-				}
-
-				ret = ull_adv_sync_start(sync, ticks_anchor,
-							 &ret_cb);
-
-				sync_is_started = 1U;
+			ret = ull_ticker_status_take(ret, &ret_cb);
+			if (ret != TICKER_STATUS_SUCCESS) {
+				goto failure_cleanup;
 			}
-		}
+
+			aux = (void *)HDR_LLL2EVT(lll_aux);
+			ull_hdr_init(&aux->ull);
+			aux->interval =
+				adv->interval +
+				(HAL_TICKER_TICKS_TO_US(ULL_ADV_RANDOM_DELAY) /
+				 625U);
+
+			ret = ull_adv_aux_start(aux, ticks_anchor, &ret_cb);
+
+			aux_is_started = 1U;
+
+#if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
+			if (lll->sync) {
+				struct lll_adv_sync *lll_sync = lll->sync;
+
+				sync = (void *)HDR_LLL2EVT(lll_sync);
+				if (sync->is_enabled && !sync->is_started) {
+					ret = ull_ticker_status_take(ret,
+								     &ret_cb);
+					if (ret != TICKER_STATUS_SUCCESS) {
+						goto failure_cleanup;
+					}
+
+					ret = ull_adv_sync_start(sync,
+								 ticks_anchor,
+								 &ret_cb);
+
+					sync_is_started = 1U;
+				}
+			}
 #endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
+		}
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
 	}
 
 	ret = ull_ticker_status_take(ret, &ret_cb);
@@ -1011,11 +1035,17 @@ uint8_t ll_adv_enable(uint8_t enable)
 		goto failure_cleanup;
 	}
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+	if (aux_is_started) {
+		aux->is_started = aux_is_started;
+
 #if defined(CONFIG_BT_CTLR_ADV_PERIODIC)
-	if (sync_is_started) {
-		sync->is_started = sync_is_started;
-	}
+		if (sync_is_started) {
+			sync->is_started = sync_is_started;
+		}
 #endif /* CONFIG_BT_CTLR_ADV_PERIODIC */
+	}
+#endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 	adv->is_enabled = 1;
 
@@ -1050,10 +1080,21 @@ int ull_adv_init(void)
 {
 	int err;
 
-	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
-		err = ull_adv_sync_init();
-		if (err) {
-			return err;
+	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_EXT)) {
+#if defined(CONFIG_BT_CTLR_ADV_AUX_SET)
+		if (CONFIG_BT_CTLR_ADV_AUX_SET > 0) {
+			err = ull_adv_aux_init();
+			if (err) {
+				return err;
+			}
+		}
+#endif
+
+		if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
+			err = ull_adv_sync_init();
+			if (err) {
+				return err;
+			}
 		}
 	}
 
@@ -1070,10 +1111,21 @@ int ull_adv_reset(void)
 	uint8_t handle;
 	int err;
 
-	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
-		err = ull_adv_sync_reset();
-		if (err) {
-			return err;
+	if (IS_ENABLED(CONFIG_BT_CTLR_ADV_EXT)) {
+#if defined(CONFIG_BT_CTLR_ADV_AUX_SET)
+		if (CONFIG_BT_CTLR_ADV_AUX_SET > 0) {
+			err = ull_adv_aux_reset();
+			if (err) {
+				return err;
+			}
+		}
+#endif
+
+		if (IS_ENABLED(CONFIG_BT_CTLR_ADV_PERIODIC)) {
+			err = ull_adv_sync_reset();
+			if (err) {
+				return err;
+			}
 		}
 	}
 
@@ -1232,6 +1284,12 @@ static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder, uint16_t laz
 			  (ret == TICKER_STATUS_BUSY));
 	}
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT) && (CONFIG_BT_CTLR_ADV_AUX_SET > 0)
+	if (adv->lll.aux) {
+		ull_adv_aux_offset_get(adv);
+	}
+#endif /* CONFIG_BT_CTLR_ADV_EXT && (CONFIG_BT_CTLR_ADV_AUX_SET > 0) */
+
 	DEBUG_RADIO_PREPARE_A(1);
 }
 
@@ -1379,6 +1437,22 @@ static inline uint8_t disable(uint8_t handle)
 	if (!adv) {
 		return BT_HCI_ERR_CMD_DISALLOWED;
 	}
+
+#if defined(CONFIG_BT_CTLR_ADV_EXT) && (CONFIG_BT_CTLR_ADV_AUX_SET > 0)
+	struct lll_adv_aux *lll_aux = adv->lll.aux;
+
+	if (lll_aux) {
+		struct ll_adv_aux_set *aux;
+		uint8_t err;
+
+		aux = (void *)HDR_LLL2EVT(lll_aux);
+
+		err = ull_adv_aux_stop(aux);
+		if (err) {
+			return err;
+		}
+	}
+#endif /* CONFIG_BT_CTLR_ADV_EXT && (CONFIG_BT_CTLR_ADV_AUX_SET > 0) */
 
 	mark = ull_disable_mark(adv);
 	LL_ASSERT(mark == adv);

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -31,6 +31,16 @@ struct ll_adv_set {
 #endif /* CONFIG_BT_CTLR_PRIVACY */
 };
 
+struct ll_adv_aux_set {
+	struct evt_hdr     evt;
+	struct ull_hdr     ull;
+	struct lll_adv_aux lll;
+
+	uint16_t interval;
+
+	uint8_t is_started:1;
+};
+
 struct ll_adv_sync_set {
 	struct evt_hdr      evt;
 	struct ull_hdr      ull;