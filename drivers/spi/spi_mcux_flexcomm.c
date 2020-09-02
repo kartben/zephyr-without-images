pabigot/zephyr

drivers: spi: Fix device instance const qualifier loss
Some needed to wrap the device pointer into device's data, where others
needed only device's data to be passed to HAL callback function.

Fixes #27399

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/spi/spi_mcux_flexcomm.c
@@ -25,6 +25,7 @@ struct spi_mcux_config {
 };
 
 struct spi_mcux_data {
+	const struct device *dev;
 	spi_master_handle_t handle;
 	struct spi_context ctx;
 	size_t transfer_len;
@@ -103,15 +104,15 @@ static void spi_mcux_isr(void *arg)
 }
 
 static void spi_mcux_master_transfer_callback(SPI_Type *base,
-		spi_master_handle_t *handle, status_t status, void *userData)
+					      spi_master_handle_t *handle,
+					      status_t status, void *userData)
 {
-	const struct device *dev = userData;
-	struct spi_mcux_data *data = dev->data;
+	struct spi_mcux_data *data = userData;
 
 	spi_context_update_tx(&data->ctx, 1, data->transfer_len);
 	spi_context_update_rx(&data->ctx, 1, data->transfer_len);
 
-	spi_mcux_transfer_next_packet(dev);
+	spi_mcux_transfer_next_packet(data->dev);
 }
 
 static int spi_mcux_configure(const struct device *dev,
@@ -174,7 +175,7 @@ static int spi_mcux_configure(const struct device *dev,
 	SPI_MasterInit(base, &master_config, clock_freq);
 
 	SPI_MasterTransferCreateHandle(base, &data->handle,
-				       spi_mcux_master_transfer_callback, dev);
+				       spi_mcux_master_transfer_callback, data);
 
 	SPI_SetDummyData(base, 0);
 
@@ -249,6 +250,8 @@ static int spi_mcux_init(const struct device *dev)
 	struct spi_mcux_data *data = dev->data;
 
 	config->irq_config_func(dev);
+
+	data->dev = dev;
 
 	spi_context_unlock_unconditionally(&data->ctx);
 

kartben/zephyr-without-images

drivers/spi/spi_mcux_flexcomm.c
@@ -25,6 +25,7 @@ struct spi_mcux_config {
 };
 
 struct spi_mcux_data {
+	const struct device *dev;
 	spi_master_handle_t handle;
 	struct spi_context ctx;
 	size_t transfer_len;
@@ -105,13 +106,12 @@ static void spi_mcux_isr(void *arg)
 static void spi_mcux_transfer_callback(SPI_Type *base,
 		spi_master_handle_t *handle, status_t status, void *userData)
 {
-	const struct device *dev = userData;
-	struct spi_mcux_data *data = dev->data;
+	struct spi_mcux_data *data = userData;
 
 	spi_context_update_tx(&data->ctx, 1, data->transfer_len);
 	spi_context_update_rx(&data->ctx, 1, data->transfer_len);
 
-	spi_mcux_transfer_next_packet(dev);
+	spi_mcux_transfer_next_packet(data->dev);
 }
 
 static int spi_mcux_configure(const struct device *dev,
@@ -180,7 +180,7 @@ static int spi_mcux_configure(const struct device *dev,
 		SPI_MasterInit(base, &master_config, clock_freq);
 
 		SPI_MasterTransferCreateHandle(base, &data->handle,
-					       spi_mcux_transfer_callback, dev);
+					       spi_mcux_transfer_callback, data);
 
 		SPI_SetDummyData(base, 0);
 
@@ -212,7 +212,7 @@ static int spi_mcux_configure(const struct device *dev,
 		SPI_SlaveInit(base, &slave_config);
 
 		SPI_SlaveTransferCreateHandle(base, &data->handle,
-					      spi_mcux_transfer_callback, dev);
+					      spi_mcux_transfer_callback, data);
 	}
 
 	data->ctx.config = spi_cfg;
@@ -285,6 +285,8 @@ static int spi_mcux_init(const struct device *dev)
 	struct spi_mcux_data *data = dev->data;
 
 	config->irq_config_func(dev);
+
+	data->dev = dev;
 
 	spi_context_unlock_unconditionally(&data->ctx);
 