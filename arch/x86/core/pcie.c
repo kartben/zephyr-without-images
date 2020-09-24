kartben/zephyr-without-images

commit e650e70364e2e905fb75acc695735d1fcf5f0a5d

x86: pcie: fallback to config via PIO
When probing for PCI-E device resources, it is possible that
configuration via MMIO is not available. This may caused by
BIOS or its settings. So when CONFIG_PCIE_MMIO_CFG=y, have
a fallback path to config devices via PIO. The inability to
config via MMIO has been observed on a couple UP Squared
boards.

Fixes #27339

Signed-off-by: Daniel Leung <daniel.leung@intel.com>

arch/x86/core/pcie.c
@@ -17,6 +17,7 @@
 #endif
 
 /* PCI Express Extended Configuration Mechanism (MMIO) */
+#ifdef CONFIG_PCIE_MMIO_CFG
 
 #define MAX_PCI_BUS_SEGMENTS 4
 
@@ -25,6 +26,8 @@ static struct {
 	uint32_t n_buses;
 	uint8_t *mmio;
 } bus_segs[MAX_PCI_BUS_SEGMENTS];
+
+static bool do_pcie_mmio_cfg;
 
 static void pcie_mm_init(void)
 {
@@ -49,6 +52,8 @@ static void pcie_mm_init(void)
 			device_map((mm_reg_t *)&bus_segs[i].mmio, phys_addr,
 				   size, K_MEM_CACHE_NONE);
 		}
+
+		do_pcie_mmio_cfg = true;
 	}
 #endif
 }
@@ -58,6 +63,10 @@ static inline void pcie_mm_conf(pcie_bdf_t bdf, unsigned int reg,
 {
 	if (bus_segs[0].mmio == NULL) {
 		pcie_mm_init();
+	}
+
+	if (do_pcie_mmio_cfg == false) {
+		return;
 	}
 
 	for (int i = 0; i < ARRAY_SIZE(bus_segs); i++) {
@@ -79,6 +88,8 @@ static inline void pcie_mm_conf(pcie_bdf_t bdf, unsigned int reg,
 		}
 	}
 }
+
+#endif /* CONFIG_PCIE_MMIO_CFG */
 
 /* Traditional Configuration Mechanism */
 
@@ -122,10 +133,13 @@ static inline void pcie_conf(pcie_bdf_t bdf, unsigned int reg,
 
 {
 #ifdef CONFIG_PCIE_MMIO_CFG
-	pcie_mm_conf(bdf, reg, write, data);
-#else
-	pcie_io_conf(bdf, reg, write, data);
+	if (do_pcie_mmio_cfg) {
+		pcie_mm_conf(bdf, reg, write, data);
+	} else
 #endif
+	{
+		pcie_io_conf(bdf, reg, write, data);
+	}
 }
 
 /* these functions are explained in include/drivers/pcie/pcie.h */

commit 991883c8c84256e8af49f2bd7b2a19f6c87c3fc9

x86: pcie: Fix calling pcie_mm_init()
Commit e650e70364e introduced an issue where in order to use MMIO
configuration:

 - do_pcie_mmio_cfg is required to be true
 - Only set to true in pcie_mm_init()
 - Which is only called from pcie_mm_conf()
 - Which is only called from pcie_conf() if do_pcie_mmio_cfg is
   already true!

The end result is that MMIO configuration will never be used.

Fix the situation by moving the initialization check to pcie_conf().

Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

arch/x86/core/pcie.c
@@ -61,14 +61,6 @@ static void pcie_mm_init(void)
 static inline void pcie_mm_conf(pcie_bdf_t bdf, unsigned int reg,
 				bool write, uint32_t *data)
 {
-	if (bus_segs[0].mmio == NULL) {
-		pcie_mm_init();
-	}
-
-	if (do_pcie_mmio_cfg == false) {
-		return;
-	}
-
 	for (int i = 0; i < ARRAY_SIZE(bus_segs); i++) {
 		int off = PCIE_BDF_TO_BUS(bdf) - bus_segs[i].start_bus;
 
@@ -133,6 +125,10 @@ static inline void pcie_conf(pcie_bdf_t bdf, unsigned int reg,
 
 {
 #ifdef CONFIG_PCIE_MMIO_CFG
+	if (bus_segs[0].mmio == NULL) {
+		pcie_mm_init();
+	}
+
 	if (do_pcie_mmio_cfg) {
 		pcie_mm_conf(bdf, reg, write, data);
 	} else