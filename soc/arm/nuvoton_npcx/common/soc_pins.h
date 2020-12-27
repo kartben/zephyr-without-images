sglass68/zephyr

driver: npcx: add glue module support in npcx series.
The System Glue module includes the three major functions:
— Power Switch Logic (PSL)
— SMBus multi-bus, wake-up support
— Simple Debug Port (SDP)

In NPCX7 series, the SMB5 and SMB6 modules contain a two-way switch to
support two separate SMBus/I2C buses (ports) with one SMB module
(controller). Since a single SMB module is able to serve only one
SMBus/I2C bus at a time, SMB_SEL registerin Glue module is used to
control theconnection of I2Cn_0 and I2Cn_1 interface pins to the SMBn
module (where n is 5, 6).

This CL provides a soc specific pin-control function called
"soc_pinctrl_i2c_port_sel" to switch buses (port) of the same SMB module
(controller). It will be used in the following i2c driver.

Signed-off-by: Mulin Chao <MLChao@nuvoton.com>

soc/arm/nuvoton_npcx/common/soc_pins.h
@@ -59,6 +59,14 @@ struct npcx_alt {
 void soc_pinctrl_mux_configure(const struct npcx_alt *alts_list,
 			uint8_t alts_size, int altfunc);
 
+/**
+ * @brief Select i2c port pads of i2c controller
+ *
+ * @param controller i2c controller device
+ * @param port index for i2c port pads
+ */
+void soc_pinctrl_i2c_port_sel(int controller, int port);
+
 #ifdef __cplusplus
 }
 #endif

kartben/zephyr-without-images

soc/arm/nuvoton_npcx/common/soc_pins.h
@@ -59,6 +59,14 @@ struct npcx_alt {
 void npcx_pinctrl_mux_configure(const struct npcx_alt *alts_list,
 			uint8_t alts_size, int altfunc);
 
+/**
+ * @brief Select i2c port pads of i2c controller
+ *
+ * @param controller i2c controller device
+ * @param port index for i2c port pads
+ */
+void npcx_pinctrl_i2c_port_sel(int controller, int port);
+
 #ifdef __cplusplus
 }
 #endif