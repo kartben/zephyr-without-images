MrVan/zephyr

cache: libmetal: Redirect sys_cache_flush()
Libmetal is still using the old sys_cache_flush(). Use an helper until
libmetal is properly fixed.

Signed-off-by: Carlo Caione <ccaione@baylibre.com>

include/cache.h
@@ -185,6 +185,13 @@ static inline int z_impl_sys_icache_range(void *addr, size_t size, int op)
 	return -ENOTSUP;
 }
 
+#ifdef CONFIG_LIBMETAL
+static inline void sys_cache_flush(void *addr, size_t size)
+{
+	sys_dcache_range(addr, size,  K_CACHE_FLUSH);
+}
+#endif
+
 /**
  *
  * @brief Get the d-cache line size.

kartben/zephyr-without-images

include/cache.h
@@ -168,6 +168,13 @@ static inline int z_impl_sys_icache_range(void *addr, size_t size, int op)
 	return -ENOTSUP;
 }
 
+#ifdef CONFIG_LIBMETAL
+static inline void sys_cache_flush(void *addr, size_t size)
+{
+	sys_dcache_range(addr, size, K_CACHE_WB);
+}
+#endif
+
 #define CPU DT_PATH(cpus, cpu_0)
 
 /**