Casper-Bonde-Bose/zephyr

Bluetooth: host: Initial support for BIS
Added initial support for BIS (broadcast ISO stream), which
adds support for creating BISes as both broadcaster and receiver,
as well as managing and creating BIGs.

Extends PA sync to handle BIGInfo adveritising reports.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

subsys/bluetooth/host/hci_core.c
@@ -4702,6 +4702,47 @@ static void le_past_received(struct net_buf *buf)
 	}
 }
 #endif /* CONFIG_BT_CONN */
+
+#if defined(CONFIG_BT_ISO_BROADCAST)
+static void hci_le_biginfo_adv_report(struct net_buf *buf)
+{
+	struct bt_hci_evt_le_biginfo_adv_report *evt;
+	struct bt_le_per_adv_sync *per_adv_sync;
+	struct bt_le_per_adv_sync_cb *listener;
+	struct bt_iso_biginfo biginfo;
+
+	evt = net_buf_pull_mem(buf, sizeof(*evt));
+
+	per_adv_sync = get_per_adv_sync(sys_le16_to_cpu(evt->sync_handle));
+
+	if (!per_adv_sync) {
+		BT_ERR("Unknown handle 0x%04X for periodic advertising report",
+		       sys_le16_to_cpu(evt->sync_handle));
+		return;
+	}
+
+	biginfo.addr = &per_adv_sync->addr;
+	biginfo.sid = per_adv_sync->sid;
+	biginfo.num_bis = evt->num_bis;
+	biginfo.sub_evt_count = evt->nse;
+	biginfo.iso_interval = sys_le16_to_cpu(evt->iso_interval);
+	biginfo.burst_number = evt->bn;
+	biginfo.offset = evt->pto;
+	biginfo.rep_count = evt->irc;
+	biginfo.max_pdu = sys_le16_to_cpu(evt->max_pdu);
+	biginfo.sdu_interval = sys_get_le24(evt->sdu_interval);
+	biginfo.max_sdu = sys_le16_to_cpu(evt->max_sdu);
+	biginfo.phy = evt->phy;
+	biginfo.framing = evt->framing;
+	biginfo.encryption = evt->encryption ? true : false;
+
+	SYS_SLIST_FOR_EACH_CONTAINER(&pa_sync_cbs, listener, node) {
+		if (listener->state_changed) {
+			listener->biginfo(per_adv_sync, &biginfo);
+		}
+	}
+}
+#endif /* defined(CONFIG_BT_ISO_BROADCAST) */
 #endif /* defined(CONFIG_BT_PER_ADV_SYNC) */
 #endif /* defined(CONFIG_BT_EXT_ADV) */
 
@@ -4990,6 +5031,23 @@ static const struct event_handler meta_events[] = {
 		      sizeof(struct bt_hci_evt_le_cis_established)),
 	EVENT_HANDLER(BT_HCI_EVT_LE_CIS_REQ, hci_le_cis_req,
 		      sizeof(struct bt_hci_evt_le_cis_req)),
+#if defined(CONFIG_BT_ISO_BROADCAST)
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_COMPLETE,
+		      hci_le_big_complete,
+		      sizeof(struct bt_hci_evt_le_big_complete)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_TERMINATE,
+		      hci_le_big_termimate,
+		      sizeof(struct bt_hci_evt_le_big_terminate)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_SYNC_ESTABLISHED,
+		      hci_le_big_sync_established,
+		      sizeof(struct bt_hci_evt_le_big_sync_established)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_SYNC_LOST,
+		      hci_le_big_sync_lost,
+		      sizeof(struct bt_hci_evt_le_big_sync_lost)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIGINFO_ADV_REPORT,
+		      hci_le_biginfo_adv_report,
+		      sizeof(struct bt_hci_evt_le_biginfo_adv_report)),
+#endif /* (CONFIG_BT_ISO_BROADCAST) */
 #endif /* (CONFIG_BT_ISO) */
 };
 
@@ -5540,6 +5598,20 @@ static int le_set_event_mask(void)
 		mask |= BT_EVT_MASK_LE_CIS_ESTABLISHED;
 		if (BT_FEAT_LE_CIS_SLAVE(bt_dev.le.features)) {
 			mask |= BT_EVT_MASK_LE_CIS_REQ;
+		}
+	}
+
+	/* Enable BIS events for broadcaster and/or receiver */
+	if (IS_ENABLED(CONFIG_BT_ISO_BROADCAST) &&
+	    BT_FEAT_LE_BIS(bt_dev.le.features)) {
+		if (BT_FEAT_LE_ISO_BROADCASTER(bt_dev.le.features)) {
+			mask |= BT_EVT_MASK_LE_BIG_COMPLETE;
+			mask |= BT_EVT_MASK_LE_BIG_TERMINATED;
+		}
+		if (BT_FEAT_LE_SYNC_RECEIVER(bt_dev.le.features)) {
+			mask |= BT_EVT_MASK_LE_BIG_SYNC_ESTABLISHED;
+			mask |= BT_EVT_MASK_LE_BIG_SYNC_LOST;
+			mask |= BT_EVT_MASK_LE_BIGINFO_ADV_REPORT;
 		}
 	}
 

kartben/zephyr-without-images

subsys/bluetooth/host/hci_core.c
@@ -4737,6 +4737,47 @@ static void le_past_received(struct net_buf *buf)
 	}
 }
 #endif /* CONFIG_BT_CONN */
+
+#if defined(CONFIG_BT_ISO_BROADCAST)
+static void hci_le_biginfo_adv_report(struct net_buf *buf)
+{
+	struct bt_hci_evt_le_biginfo_adv_report *evt;
+	struct bt_le_per_adv_sync *per_adv_sync;
+	struct bt_le_per_adv_sync_cb *listener;
+	struct bt_iso_biginfo biginfo;
+
+	evt = net_buf_pull_mem(buf, sizeof(*evt));
+
+	per_adv_sync = get_per_adv_sync(sys_le16_to_cpu(evt->sync_handle));
+
+	if (!per_adv_sync) {
+		BT_ERR("Unknown handle 0x%04X for periodic advertising report",
+		       sys_le16_to_cpu(evt->sync_handle));
+		return;
+	}
+
+	biginfo.addr = &per_adv_sync->addr;
+	biginfo.sid = per_adv_sync->sid;
+	biginfo.num_bis = evt->num_bis;
+	biginfo.sub_evt_count = evt->nse;
+	biginfo.iso_interval = sys_le16_to_cpu(evt->iso_interval);
+	biginfo.burst_number = evt->bn;
+	biginfo.offset = evt->pto;
+	biginfo.rep_count = evt->irc;
+	biginfo.max_pdu = sys_le16_to_cpu(evt->max_pdu);
+	biginfo.sdu_interval = sys_get_le24(evt->sdu_interval);
+	biginfo.max_sdu = sys_le16_to_cpu(evt->max_sdu);
+	biginfo.phy = evt->phy;
+	biginfo.framing = evt->framing;
+	biginfo.encryption = evt->encryption ? true : false;
+
+	SYS_SLIST_FOR_EACH_CONTAINER(&pa_sync_cbs, listener, node) {
+		if (listener->biginfo) {
+			listener->biginfo(per_adv_sync, &biginfo);
+		}
+	}
+}
+#endif /* defined(CONFIG_BT_ISO_BROADCAST) */
 #endif /* defined(CONFIG_BT_PER_ADV_SYNC) */
 #endif /* defined(CONFIG_BT_EXT_ADV) */
 
@@ -5025,6 +5066,23 @@ static const struct event_handler meta_events[] = {
 		      sizeof(struct bt_hci_evt_le_cis_established)),
 	EVENT_HANDLER(BT_HCI_EVT_LE_CIS_REQ, hci_le_cis_req,
 		      sizeof(struct bt_hci_evt_le_cis_req)),
+#if defined(CONFIG_BT_ISO_BROADCAST)
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_COMPLETE,
+		      hci_le_big_complete,
+		      sizeof(struct bt_hci_evt_le_big_complete)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_TERMINATE,
+		      hci_le_big_termimate,
+		      sizeof(struct bt_hci_evt_le_big_terminate)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_SYNC_ESTABLISHED,
+		      hci_le_big_sync_established,
+		      sizeof(struct bt_hci_evt_le_big_sync_established)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIG_SYNC_LOST,
+		      hci_le_big_sync_lost,
+		      sizeof(struct bt_hci_evt_le_big_sync_lost)),
+	EVENT_HANDLER(BT_HCI_EVT_LE_BIGINFO_ADV_REPORT,
+		      hci_le_biginfo_adv_report,
+		      sizeof(struct bt_hci_evt_le_biginfo_adv_report)),
+#endif /* (CONFIG_BT_ISO_BROADCAST) */
 #endif /* (CONFIG_BT_ISO) */
 };
 
@@ -5575,6 +5633,20 @@ static int le_set_event_mask(void)
 		mask |= BT_EVT_MASK_LE_CIS_ESTABLISHED;
 		if (BT_FEAT_LE_CIS_SLAVE(bt_dev.le.features)) {
 			mask |= BT_EVT_MASK_LE_CIS_REQ;
+		}
+	}
+
+	/* Enable BIS events for broadcaster and/or receiver */
+	if (IS_ENABLED(CONFIG_BT_ISO_BROADCAST) &&
+	    BT_FEAT_LE_BIS(bt_dev.le.features)) {
+		if (BT_FEAT_LE_ISO_BROADCASTER(bt_dev.le.features)) {
+			mask |= BT_EVT_MASK_LE_BIG_COMPLETE;
+			mask |= BT_EVT_MASK_LE_BIG_TERMINATED;
+		}
+		if (BT_FEAT_LE_SYNC_RECEIVER(bt_dev.le.features)) {
+			mask |= BT_EVT_MASK_LE_BIG_SYNC_ESTABLISHED;
+			mask |= BT_EVT_MASK_LE_BIG_SYNC_LOST;
+			mask |= BT_EVT_MASK_LE_BIGINFO_ADV_REPORT;
 		}
 	}
 