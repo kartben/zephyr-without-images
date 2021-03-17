kartben/zephyr-without-images

commit 23974065f8fd90aa4145a141ea2def1dd6cf2035

Bluetooth: host: Refactor out advertiser roles from hci_core to adv
Refactor out the advertiser roles handling from hci_core.c to its own
source file in adv.c.
Advertising roles consists of legacy and extended advertiser, and
periodic advertiser.

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

subsys/bluetooth/host/id.c
@@ -14,6 +14,7 @@
 #include "hci_core.h"
 #include "id.h"
 #include "scan.h"
+#include "adv.h"
 #include "smp.h"
 #include "conn_internal.h"
 #include "keys.h"

subsys/bluetooth/host/CMakeLists.txt
@@ -37,6 +37,10 @@ if(CONFIG_BT_HCI_HOST)
     id.c
     )
   zephyr_library_sources_ifdef(
+    CONFIG_BT_BROADCASTER
+    adv.c
+    )
+  zephyr_library_sources_ifdef(
     CONFIG_BT_OBSERVER
     scan.c
     )

subsys/bluetooth/host/adv.h
@@ -1,0 +1,21 @@
+/*
+ * Copyright (c) 2017-2021 Nordic Semiconductor ASA
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+void bt_le_adv_resume(void);
+
+struct bt_le_ext_adv *bt_le_adv_lookup_legacy(void);
+
+void bt_le_adv_delete_legacy(void);
+int bt_le_adv_set_enable(struct bt_le_ext_adv *adv, bool enable);
+
+void bt_le_ext_adv_foreach(void (*func)(struct bt_le_ext_adv *adv, void *data),
+			   void *data);
+
+int bt_le_adv_set_enable_ext(struct bt_le_ext_adv *adv,
+			 bool enable,
+			 const struct bt_le_ext_adv_start_param *param);
+int bt_le_adv_set_enable_legacy(struct bt_le_ext_adv *adv, bool enable);

subsys/bluetooth/host/conn.c
@@ -29,6 +29,7 @@
 
 #include "hci_core.h"
 #include "id.h"
+#include "adv.h"
 #include "conn_internal.h"
 #include "l2cap_internal.h"
 #include "keys.h"

subsys/bluetooth/host/hci_core.h
@@ -364,21 +364,6 @@ void bt_setup_public_id_addr(void);
 
 void bt_finalize_init(void);
 
-int bt_le_adv_start_internal(const struct bt_le_adv_param *param,
-			     const struct bt_data *ad, size_t ad_len,
-			     const struct bt_data *sd, size_t sd_len,
-			     const bt_addr_le_t *peer);
-
-void bt_le_adv_resume(void);
-int bt_le_adv_set_enable_legacy(struct bt_le_ext_adv *adv, bool enable);
-int bt_le_adv_set_enable_ext(struct bt_le_ext_adv *adv,
-			     bool enable,
-			     const struct bt_le_ext_adv_start_param *param);
-struct bt_le_ext_adv *bt_le_adv_lookup_legacy(void);
-int bt_le_adv_set_enable(struct bt_le_ext_adv *adv, bool enable);
-void bt_le_ext_adv_foreach(void (*func)(struct bt_le_ext_adv *adv, void *data),
-			   void *data);
-
 void bt_hci_host_num_completed_packets(struct net_buf *buf);
 
 /* HCI event handlers */
@@ -406,3 +391,7 @@ void bt_hci_le_per_adv_report(struct net_buf *buf);
 void bt_hci_le_per_adv_sync_lost(struct net_buf *buf);
 void bt_hci_le_biginfo_adv_report(struct net_buf *buf);
 void bt_hci_le_past_received(struct net_buf *buf);
+
+/* Adv HCI event handlers */
+void bt_hci_le_adv_set_terminated(struct net_buf *buf);
+void bt_hci_le_scan_req_received(struct net_buf *buf);

commit f7f04cfd8ea0ccab6b138a287f231b98f9c5853d

Bluetooth: host: Fix undefined references to adv in central only builds
Fix regression is central only builds when CONFIG_BT_PRIVACY and/or
CONFIG_BT_EXT_ADV is enabled, or the functions bt_id_reset or
bt_id_delete is called from application.

This resulted in build error for undefined functions
bt_le_ext_adv_foreach and bt_le_adv_lookup_legacy since the definition
for these functions are now only compiled in when CONFIG_BT_BROADCASTER
has been enabled.

Regression from:
23974065f8fd90aa4145a141ea2def1dd6cf2035

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

subsys/bluetooth/host/id.c
@@ -194,7 +194,9 @@ static void le_rpa_invalidate(void)
 		atomic_clear_bit(bt_dev.flags, BT_DEV_RPA_VALID);
 	}
 
-	bt_le_ext_adv_foreach(adv_rpa_invalidate, NULL);
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER)) {
+		bt_le_ext_adv_foreach(adv_rpa_invalidate, NULL);
+	}
 }
 
 #if defined(CONFIG_BT_PRIVACY)
@@ -370,7 +372,8 @@ static void le_update_private_addr(void)
 	uint8_t id = BT_ID_DEFAULT;
 	int err;
 
-	if (IS_ENABLED(CONFIG_BT_EXT_ADV) &&
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER) &&
+	    IS_ENABLED(CONFIG_BT_EXT_ADV) &&
 	    BT_FEAT_LE_EXT_ADV(bt_dev.le.features)) {
 		bt_le_ext_adv_foreach(adv_update_rpa, NULL);
 	}
@@ -395,7 +398,8 @@ static void le_update_private_addr(void)
 		bt_le_create_conn_cancel();
 	}
 
-	if (!(IS_ENABLED(CONFIG_BT_EXT_ADV) &&
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER) &&
+	    !(IS_ENABLED(CONFIG_BT_EXT_ADV) &&
 	      BT_FEAT_LE_EXT_ADV(bt_dev.le.features))) {
 		adv = bt_le_adv_lookup_legacy();
 
@@ -418,7 +422,8 @@ static void le_update_private_addr(void)
 		return;
 	}
 
-	if (adv && adv_enabled) {
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER) &&
+	    adv && adv_enabled) {
 		bt_le_adv_set_enable_legacy(adv, true);
 	}
 
@@ -449,7 +454,9 @@ static void rpa_timeout(struct k_work *work)
 
 	le_rpa_invalidate();
 
-	bt_le_ext_adv_foreach(adv_is_private_enabled, &adv_enabled);
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER)) {
+		bt_le_ext_adv_foreach(adv_is_private_enabled, &adv_enabled);
+	}
 
 	/* IF no roles using the RPA is running we can stop the RPA timer */
 	if (!(adv_enabled ||
@@ -1050,11 +1057,6 @@ int bt_id_create(bt_addr_le_t *addr, uint8_t *irk)
 
 int bt_id_reset(uint8_t id, bt_addr_le_t *addr, uint8_t *irk)
 {
-	struct bt_adv_id_check_data check_data = {
-		.id = id,
-		.adv_enabled = false,
-	};
-
 	if (addr && bt_addr_le_cmp(addr, BT_ADDR_LE_ANY)) {
 		if (addr->type != BT_ADDR_LE_RANDOM ||
 		    !BT_ADDR_IS_STATIC(&addr->a)) {
@@ -1075,9 +1077,16 @@ int bt_id_reset(uint8_t id, bt_addr_le_t *addr, uint8_t *irk)
 		return -EINVAL;
 	}
 
-	bt_le_ext_adv_foreach(adv_id_check_func, &check_data);
-	if (check_data.adv_enabled) {
-		return -EBUSY;
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER)) {
+		struct bt_adv_id_check_data check_data = {
+			.id = id,
+			.adv_enabled = false,
+		};
+
+		bt_le_ext_adv_foreach(adv_id_check_func, &check_data);
+		if (check_data.adv_enabled) {
+			return -EBUSY;
+		}
 	}
 
 	if (IS_ENABLED(CONFIG_BT_CONN) &&
@@ -1097,11 +1106,6 @@ int bt_id_reset(uint8_t id, bt_addr_le_t *addr, uint8_t *irk)
 
 int bt_id_delete(uint8_t id)
 {
-	struct bt_adv_id_check_data check_data = {
-		.id = id,
-		.adv_enabled = false,
-	};
-
 	if (id == BT_ID_DEFAULT || id >= bt_dev.id_count) {
 		return -EINVAL;
 	}
@@ -1110,9 +1114,16 @@ int bt_id_delete(uint8_t id)
 		return -EALREADY;
 	}
 
-	bt_le_ext_adv_foreach(adv_id_check_func, &check_data);
-	if (check_data.adv_enabled) {
-		return -EBUSY;
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER)) {
+		struct bt_adv_id_check_data check_data = {
+			.id = id,
+			.adv_enabled = false,
+		};
+
+		bt_le_ext_adv_foreach(adv_id_check_func, &check_data);
+		if (check_data.adv_enabled) {
+			return -EBUSY;
+		}
 	}
 
 	if (IS_ENABLED(CONFIG_BT_CONN)) {