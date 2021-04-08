PavelVPV/zephyr

Bluetooth: Mesh: Introduce acknowledged message API
The implementation of blocking calls is common for all the client
models.

This change reduces the code duplication by introducing new API that
helps to manage acknowledged messages.

Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

include/bluetooth/mesh/cfg_cli.h
@@ -27,10 +27,7 @@ struct bt_mesh_cfg_cli {
 	struct bt_mesh_model *model;
 
 	/* Internal parameters for tracking message responses. */
-	struct k_sem          op_sync;
-	uint32_t              op_pending;
-	void                 *op_param;
-	uint16_t              op_addr;
+	struct bt_mesh_model_ack_ctx    ack_ctx;
 };
 
 /** @def BT_MESH_MODEL_CFG_CLI

include/bluetooth/mesh/health_cli.h
@@ -46,9 +46,7 @@ struct bt_mesh_health_cli {
 			       size_t fault_count);
 
 	/* Internal parameters for tracking message responses. */
-	struct k_sem          op_sync;
-	uint32_t                 op_pending;
-	void                 *op_param;
+	struct bt_mesh_model_ack_ctx    ack_ctx;
 };
 
 

kartben/zephyr-without-images

include/bluetooth/mesh/cfg_cli.h
@@ -27,10 +27,7 @@ struct bt_mesh_cfg_cli {
 	struct bt_mesh_model *model;
 
 	/* Internal parameters for tracking message responses. */
-	struct k_sem          op_sync;
-	uint32_t              op_pending;
-	void                 *op_param;
-	uint16_t              op_addr;
+	struct bt_mesh_msg_ack_ctx ack_ctx;
 };
 
 /** @def BT_MESH_MODEL_CFG_CLI

include/bluetooth/mesh/health_cli.h
@@ -46,9 +46,7 @@ struct bt_mesh_health_cli {
 			       size_t fault_count);
 
 	/* Internal parameters for tracking message responses. */
-	struct k_sem          op_sync;
-	uint32_t                 op_pending;
-	void                 *op_param;
+	struct bt_mesh_msg_ack_ctx ack_ctx;
 };
 
 