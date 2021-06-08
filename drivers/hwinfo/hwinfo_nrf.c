arvinf/zephyr

drivers: hwinfo: Add reset cause support
Add `hwinfo_get_reset_cause` and `hwinfo_clear_reset_cause` to retrieve
and to clear cause of system reset on supported platforms.

Different platforms can provide different causes of reset, however
there is a great deal of overlap. `enum reset_cause` can be expanded in
the future to support additional reasons, as additional platforms are
supported.

Signed-off-by: Arvin Farahmand <arvinf@ip-logix.com>

drivers/hwinfo/hwinfo_nrf.c
@@ -9,6 +9,21 @@
 #include <string.h>
 #include <hal/nrf_ficr.h>
 #include <sys/byteorder.h>
+#include <hal/nrf_power.h>
+#if NRF_POWER_HAS_RESETREAS == 0
+#include <hal/nrf_reset.h>
+#endif
+
+#if (NRF_POWER_HAS_RESETREAS == 0)
+#if CONFIG_SOC_NRF5340_CPUAPP || CONFIG_SOC_NRF5340_CPUNET
+#define NET_RESET_REASON_MASK \
+	(NRF_RESET_RESETREAS_LSREQ_MASK | NRF_RESET_RESETREAS_LLOCKUP_MASK | \
+	NRF_RESET_RESETREAS_LDOG_MASK | NRF_RESET_RESETREAS_MFORCEOFF_MASK | \
+	NRF_RESET_RESETREAS_LCTRLAP_MASK)
+#else
+#error "Unsupported SoC"
+#endif
+#endif
 
 struct nrf_uid {
 	uint32_t id[2];
@@ -28,4 +43,98 @@ ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 	memcpy(buffer, dev_id.id, length);
 
 	return length;
+}
+
+
+int z_impl_hwinfo_get_reset_cause(u32_t *cause)
+{
+	u32_t flags = 0;
+
+#if NRF_POWER_HAS_RESETREAS
+	u32_t reason = nrf_power_resetreas_get(NRF_POWER);
+#else
+	u32_t reason = nrf_reset_resetreas_get(NRF_RESET);
+	u32_t mask = IS_ENABLED(CONFIG_SOC_NRF5340_CPUNET) ?
+			NET_RESET_REASON_MASK : ~NET_RESET_REASON_MASK;
+	reason &= mask;
+#endif
+
+#if NRF_POWER_HAS_RESETREAS
+	if (reason & NRF_POWER_RESETREAS_RESETPIN_MASK) {
+		flags |= RESET_PIN;
+	}
+	if (reason & NRF_POWER_RESETREAS_DOG_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRF_POWER_RESETREAS_SREQ_MASK) {
+		flags |= RESET_SOFTWARE;
+	}
+	if (reason & NRF_POWER_RESETREAS_LOCKUP_MASK) {
+		flags |= RESET_CPU_LOCKUP;
+	}
+	if (reason & NRF_POWER_RESETREAS_OFF_MASK) {
+		flags |= RESET_LOW_POWER_WAKE;
+	}
+	if (reason & NRF_POWER_RESETREAS_DIF_MASK) {
+		flags |= RESET_DEBUG;
+	}
+#else /* NRF_POWER_HAS_RESETREAS */
+#if CONFIG_SOC_NRF5340_CPUAPP
+	if (reason & NRF_RESET_RESETREAS_RESETPIN_MASK) {
+		flags |= RESET_PIN;
+	}
+	if (reason & NRF_RESET_RESETREAS_DOG0_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRF_RESET_RESETREAS_CTRLAP_MASK) {
+		flags |= RESET_DEBUG;
+	}
+	if (reason & NRF_RESET_RESETREAS_SREQ_MASK) {
+		flags |= RESET_SOFTWARE;
+	}
+	if (reason & NRF_RESET_RESETREAS_LOCKUP_MASK) {
+		flags |= RESET_CPU_LOCKUP;
+	}
+	if (reason & NRF_RESET_RESETREAS_OFF_MASK) {
+		flags |= RESET_LOW_POWER_WAKE;
+	}
+	if (reason & NRF_RESET_RESETREAS_DIF_MASK) {
+		flags |= RESET_DEBUG;
+	}
+	if (reason & NRF_RESET_RESETREAS_DOG1_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+#elif CONFIG_SOC_NRF5340_CPUNET
+	if (reason & NRF_RESET_RESETREAS_LSREQ_MASK) {
+		flags |= RESET_SOFTWARE;
+	}
+	if (reason & NRF_RESET_RESETREAS_LLOCKUP_MASK) {
+		flags |= RESET_CPU_LOCKUP;
+	}
+	if (reason & NRF_RESET_RESETREAS_LDOG_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRF_RESET_RESETREAS_LCTRLAP_MASK) {
+		flags |= RESET_DEBUG;
+	}
+#else
+#error "Unsupported SoC"
+#endif
+#endif
+	*cause = flags;
+
+	return 0;
+}
+
+int z_impl_hwinfo_clear_reset_cause(void)
+{
+	u32_t reason = -1;
+
+#if NRF_POWER_HAS_RESETREAS
+	nrf_power_resetreas_clear(NRF_POWER, reason);
+#else
+	nrf_reset_resetreas_clear(NRF_RESET, reason);
+#endif
+
+	return 0;
 }

kartben/zephyr-without-images

drivers/hwinfo/hwinfo_nrf.c
@@ -9,6 +9,9 @@
 #include <string.h>
 #include <hal/nrf_ficr.h>
 #include <sys/byteorder.h>
+#ifndef CONFIG_BOARD_QEMU_CORTEX_M0
+#include <helpers/nrfx_reset_reason.h>
+#endif
 
 struct nrf_uid {
 	uint32_t id[2];
@@ -29,3 +32,88 @@ ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 
 	return length;
 }
+
+#ifndef CONFIG_BOARD_QEMU_CORTEX_M0
+int z_impl_hwinfo_get_reset_cause(uint32_t *cause)
+{
+	uint32_t flags = 0;
+
+	uint32_t reason = nrfx_reset_reason_get();
+
+	if (reason & NRFX_RESET_REASON_RESETPIN_MASK) {
+		flags |= RESET_PIN;
+	}
+	if (reason & NRFX_RESET_REASON_DOG_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRFX_RESET_REASON_LOCKUP_MASK) {
+		flags |= RESET_CPU_LOCKUP;
+	}
+	if (reason & NRFX_RESET_REASON_OFF_MASK) {
+		flags |= RESET_LOW_POWER_WAKE;
+	}
+	if (reason & NRFX_RESET_REASON_DIF_MASK) {
+		flags |= RESET_DEBUG;
+	}
+
+#if !NRF_POWER_HAS_RESETREAS
+	if (reason & NRFX_RESET_REASON_CTRLAP_MASK) {
+		flags |= RESET_DEBUG;
+	}
+	if (reason & NRFX_RESET_REASON_DOG0_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRFX_RESET_REASON_DOG1_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRFX_RESETREAS_SREQ_MASK) {
+		flags |= RESET_SOFTWARE;
+	}
+
+#if NRF_RESET_HAS_NETWORK
+	if (reason & NRFX_RESET_REASON_LSREQ_MASK) {
+		flags |= RESET_SOFTWARE;
+	}
+	if (reason & NRFX_RESET_REASON_LLOCKUP_MASK) {
+		flags |= RESET_CPU_LOCKUP;
+	}
+	if (reason & NRFX_RESET_REASON_LDOG_MASK) {
+		flags |= RESET_WATCHDOG;
+	}
+	if (reason & NRFX_RESET_REASON_LCTRLAP_MASK) {
+		flags |= RESET_DEBUG;
+	}
+#endif
+
+#else
+	if (reason & NRFX_RESET_REASON_SREQ_MASK) {
+		flags |= RESET_SOFTWARE;
+	}
+#endif
+
+	*cause = flags;
+
+	return 0;
+}
+
+int z_impl_hwinfo_clear_reset_cause(void)
+{
+	uint32_t reason = -1;
+
+	nrfx_reset_reason_clear(reason);
+
+	return 0;
+}
+
+int z_impl_hwinfo_get_supported_reset_cause(uint32_t *supported)
+{
+	*supported = (RESET_PIN
+		      | RESET_WATCHDOG
+		      | RESET_SOFTWARE
+		      | RESET_CPU_LOCKUP
+		      | RESET_LOW_POWER_WAKE
+		      | RESET_DEBUG);
+
+	return 0;
+}
+#endif