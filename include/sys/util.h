kartben/zephyr-without-images

commit df78f1f6307793a11b8c508c3c36a0cdfe4685fb

nuvoton: battery-backed ram
Add bindings and entry for the battery-backed ram in the nuvoton
npcx. This commit is an upstream port of
https://source.chromium.org/chromiumos/chromiumos/codesearch/+/main:src/platform/ec/zephyr/dts/bindings/cros_bbram/nuvoton,npcx-cros-bbram.yaml;l=1;drc=e9af813c36b7b411bf2a01cbc1b09d5fdec49b8a

Along with a bit of cleaning up and an emulator.

Signed-off-by: Yuval Peress <peress@chromium.org>

include/sys/util.h
@@ -224,6 +224,22 @@ static inline int64_t arithmetic_shift_right(int64_t value, uint8_t shift)
 }
 
 /**
+ * @brief byte by byte memcpy.
+ *
+ * Copy `size` bytes of `src` into `dest`. This is guaranteed to be done byte by byte.
+ *
+ * @param dst Pointer to the destination memory.
+ * @param src Pointer to the source of the data.
+ * @param size The number of bytes to copy.
+ */
+static inline void bytecpy(void *dst, const void *src, size_t size)
+{
+	for (size_t i = 0; i < size; ++i) {
+		((uint8_t *)dst)[i] = ((uint8_t *)src)[i];
+	}
+}
+
+/**
  * @brief      Convert a single character into a hexadecimal nibble.
  *
  * @param c     The character to convert

CODEOWNERS
@@ -185,6 +185,7 @@
 /drivers/*/*npcx*                         @MulinChao @WealianLiao @ChiHuaL
 /drivers/adc/                             @anangl
 /drivers/adc/adc_stm32.c                  @cybertale
+/drivers/bbram/*                          @yperess @sjg20 @jackrosenthal
 /drivers/bluetooth/                       @joerchan @jhedberg @Vudentz
 /drivers/cache/                           @carlocaione
 /drivers/syscon/                          @carlocaione

drivers/CMakeLists.txt
@@ -58,3 +58,4 @@ add_subdirectory_ifdef(CONFIG_PTP_CLOCK        ptp_clock)
 add_subdirectory_ifdef(CONFIG_EDAC             edac)
 add_subdirectory_ifdef(CONFIG_CACHE_MANAGEMENT cache)
 add_subdirectory_ifdef(CONFIG_SYSCON           syscon)
+add_subdirectory_ifdef(CONFIG_BBRAM            bbram)

drivers/Kconfig
@@ -117,4 +117,6 @@ source "drivers/cache/Kconfig"
 
 source "drivers/syscon/Kconfig"
 
+source "drivers/bbram/Kconfig"
+
 endmenu

drivers/bbram/CMakeLists.txt
@@ -1,0 +1,7 @@
+# Copyright (c) 2021 Google Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+zephyr_library()
+
+zephyr_library_sources_ifdef(CONFIG_BBRAM_NPCX bbram_npcx.c)
+zephyr_library_sources_ifdef(CONFIG_BBRAM_EMUL bbram_emul.c)

drivers/bbram/Kconfig
@@ -1,0 +1,25 @@
+# Copyright (c) 2021 Google Inc
+# SPDX-License-Identifier: Apache-2.0
+
+menuconfig BBRAM
+	bool "Battery-backed RAM drivers"
+	help
+	  Enable BBRAM (battery-backed RAM) driver configuration.
+
+if BBRAM
+
+module = BBRAM
+module-str = bbram
+source "subsys/logging/Kconfig.template.log_config"
+
+config BBRAM_INIT_PRIORITY
+	int "Init priority"
+	default 10
+	help
+	  BBRAM driver initialization priority
+
+source "drivers/bbram/Kconfig.npcx"
+
+source "drivers/bbram/Kconfig.bbram_emul"
+
+endif # BBRAM

drivers/bbram/Kconfig.bbram_emul
@@ -1,0 +1,7 @@
+# Copyright (c) 2021 Google Inc
+# SPDX-License-Identifier: Apache-2.0
+
+config BBRAM_EMUL
+	bool "Battery-backed RAM emulated drivers"
+	help
+	  This option enables the BBRAM emulation driver.

drivers/bbram/Kconfig.npcx
@@ -1,0 +1,8 @@
+# Copyright (c) 2021 Google Inc
+# SPDX-License-Identifier: Apache-2.0
+
+config BBRAM_NPCX
+	bool "Nuvoton NPCX embedded controller (EC) Battery-backed RAM drivers"
+	depends on SOC_FAMILY_NPCX
+	help
+	  This option enables the BBRAM driver for NPCX family of processors.

commit 8703c1fb76d9212aae4fbf5f338cf87fe2b58871

sys: util: fix compilation with XCC
Xtensa XCC does not like C99 style declarations in for
loops.

Fixes: df78f1f63077 ("nuvoton: battery-backed ram")
Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

include/sys/util.h
@@ -234,7 +234,9 @@ static inline int64_t arithmetic_shift_right(int64_t value, uint8_t shift)
  */
 static inline void bytecpy(void *dst, const void *src, size_t size)
 {
-	for (size_t i = 0; i < size; ++i) {
+	size_t i;
+
+	for (i = 0; i < size; ++i) {
 		((uint8_t *)dst)[i] = ((uint8_t *)src)[i];
 	}
 }