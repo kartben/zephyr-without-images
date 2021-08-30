tejlmand/zephyr

armclang: support for armclang compiler and armlink linker
This is the initial support for the armclang compiler together with the
armlink linker.

Introduced in this commit:
- armclang compiler support
- armlink linker support
- armlink scatter file generator for scatter loading
- dual pass linker script generation

Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

cmake/toolchain/armclang/generic.cmake
@@ -1,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set_ifndef(ARMCLANG_TOOLCHAIN_PATH "$ENV{ARMCLANG_TOOLCHAIN_PATH}")
+set(ARMCLANG_TOOLCHAIN_PATH ${ARMCLANG_TOOLCHAIN_PATH} CACHE PATH "armclang tools install directory")
+assert(ARMCLANG_TOOLCHAIN_PATH "ARMCLANG_TOOLCHAIN_PATH is not set")
+
+if(NOT EXISTS ${ARMCLANG_TOOLCHAIN_PATH})
+  message(FATAL_ERROR "Nothing found at ARMCLANG_TOOLCHAIN_PATH: '${ARMCLANG_TOOLCHAIN_PATH}'")
+endif()
+
+set(TOOLCHAIN_HOME ${ARMCLANG_TOOLCHAIN_PATH})
+
+set(COMPILER armclang)
+set(LINKER armclang)
+set(BINTOOLS armclang)
+
+set(SYSROOT_TARGET arm)
+
+set(CROSS_COMPILE ${TOOLCHAIN_HOME}/bin/)
+#set(SYSROOT_DIR ${TOOLCHAIN_HOME}/${SYSROOT_TARGET})
+
+set(TOOLCHAIN_HAS_NEWLIB OFF CACHE BOOL "True if toolchain supports newlib")

kartben/zephyr-without-images

cmake/toolchain/armclang/generic.cmake
@@ -1,0 +1,21 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set_ifndef(ARMCLANG_TOOLCHAIN_PATH "$ENV{ARMCLANG_TOOLCHAIN_PATH}")
+set(ARMCLANG_TOOLCHAIN_PATH ${ARMCLANG_TOOLCHAIN_PATH} CACHE PATH "armclang tools install directory")
+assert(ARMCLANG_TOOLCHAIN_PATH "ARMCLANG_TOOLCHAIN_PATH is not set")
+
+if(NOT EXISTS ${ARMCLANG_TOOLCHAIN_PATH})
+  message(FATAL_ERROR "Nothing found at ARMCLANG_TOOLCHAIN_PATH: '${ARMCLANG_TOOLCHAIN_PATH}'")
+endif()
+
+set(TOOLCHAIN_HOME ${ARMCLANG_TOOLCHAIN_PATH})
+
+set(COMPILER armclang)
+set(LINKER armlink)
+set(BINTOOLS armclang)
+
+set(SYSROOT_TARGET arm)
+
+set(CROSS_COMPILE ${TOOLCHAIN_HOME}/bin/)
+
+set(TOOLCHAIN_HAS_NEWLIB OFF CACHE BOOL "True if toolchain supports newlib")