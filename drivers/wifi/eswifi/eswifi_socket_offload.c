kartben/zephyr-without-images

commit 64be81fc1935afb937779269eb60d8c712c7a6aa

drivers: wifi: eswifi: fix socket poll
- Fix return value, must be number structures with events
- set revents flag

Signed-off-by: Loic Poulain <loic.poulain@linaro.org>

drivers/wifi/eswifi/eswifi_socket_offload.c
@@ -424,7 +424,7 @@ static int eswifi_socket_poll(struct zsock_pollfd *fds, int nfds, int msecs)
 	int sock, ret;
 	void *obj;
 
-	if (nfds > 1) {
+	if (nfds != 1) {
 		errno = EINVAL;
 		return -1;
 	}
@@ -445,6 +445,11 @@ static int eswifi_socket_poll(struct zsock_pollfd *fds, int nfds, int msecs)
 		return -1;
 	}
 
+	if (!(fds[0].events & POLLIN)) {
+		errno = ENOTSUP;
+		return -1;
+	}
+
 	eswifi_lock(eswifi);
 	socket = &eswifi->socket[sock];
 	eswifi_unlock(eswifi);
@@ -453,8 +458,16 @@ static int eswifi_socket_poll(struct zsock_pollfd *fds, int nfds, int msecs)
 		return -1;
 	}
 
-	ret = k_sem_take(&socket->read_sem, K_MSEC(msecs));
-	return ret;
+	ret = k_sem_take(&socket->read_sem, K_MSEC(msecs*10));
+	if (ret) {
+		errno = ETIMEDOUT;
+		return -1;
+	}
+
+	fds[0].revents = POLLIN;
+
+	/* Report one event */
+	return 1;
 }
 
 static int eswifi_socket_bind(void *obj, const struct sockaddr *addr,

commit 6f05995905619d9d4fbdbae94153b89e0f8e9f7c

drivers: wifi: eswifi: fix socket poll timeout
Fix a problem introduced in 64be81fc19 that caused the poll operation to
take 10x more time than intended.

Signed-off-by: Mateusz Kwiatkowski <kfyatek+publicgit@gmail.com>

drivers/wifi/eswifi/eswifi_socket_offload.c
@@ -458,7 +458,7 @@ static int eswifi_socket_poll(struct zsock_pollfd *fds, int nfds, int msecs)
 		return -1;
 	}
 
-	ret = k_sem_take(&socket->read_sem, K_MSEC(msecs*10));
+	ret = k_sem_take(&socket->read_sem, K_MSEC(msecs));
 	if (ret) {
 		errno = ETIMEDOUT;
 		return -1;