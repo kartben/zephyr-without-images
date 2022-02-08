kartben/zephyr-without-images

commit 58eb9144b55464b6ecd30bf6c1d719af9dc268ae

spi: nrfx_spi*: only run uninit if configured
Only run the `uninit` function if the SPI instance has previously been
configured. This stops an assertion in the HAL drivers from triggering
due to running `uninit` without a previous `init`.

Fixes #42299.

Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

drivers/spi/spi_nrfx_spi.c
@@ -285,8 +285,10 @@ static int spi_nrfx_pm_action(const struct device *dev,
 		break;
 
 	case PM_DEVICE_ACTION_SUSPEND:
-		nrfx_spi_uninit(&config->spi);
-		data->initialized = false;
+		if (data->initialized) {
+			nrfx_spi_uninit(&config->spim);
+			data->initialized = false;
+		}
 		break;
 
 	default:

drivers/spi/spi_nrfx_spim.c
@@ -443,8 +443,10 @@ static int spim_nrfx_pm_action(const struct device *dev,
 		break;
 
 	case PM_DEVICE_ACTION_SUSPEND:
-		nrfx_spim_uninit(&config->spim);
-		data->initialized = false;
+		if (data->initialized) {
+			nrfx_spim_uninit(&config->spim);
+			data->initialized = false;
+		}
 		break;
 
 	default:

commit dbebda44265d15ba422d813669fcb0cab6479488

drivers: spi_nrfx_spi: Fix compilation error
Fix a copy/paste mistake introduced by commit
58eb9144b55464b6ecd30bf6c1d719af9dc268ae.

Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

drivers/spi/spi_nrfx_spi.c
@@ -286,7 +286,7 @@ static int spi_nrfx_pm_action(const struct device *dev,
 
 	case PM_DEVICE_ACTION_SUSPEND:
 		if (data->initialized) {
-			nrfx_spi_uninit(&config->spim);
+			nrfx_spi_uninit(&config->spi);
 			data->initialized = false;
 		}
 		break;