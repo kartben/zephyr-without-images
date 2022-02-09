kartben/zephyr-without-images

commit a3fbea2933aec87486eb82d5bb371be71d29b351

Bluetooth: controller: Fix auxiliary scan context release
Fix the auxiliary scan context release to be performed in
the disabled_cb callback after the ULL reference count is
decremented.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ull_scan_aux.c
@@ -42,6 +42,8 @@ static inline struct ll_scan_aux_set *aux_acquire(void);
 static inline void aux_release(struct ll_scan_aux_set *aux);
 static inline uint8_t aux_handle_get(struct ll_scan_aux_set *aux);
 static inline struct ll_sync_set *sync_create_get(struct ll_scan_set *scan);
+static void last_disabled_cb(void *param);
+static void done_disabled_cb(void *param);
 static void flush(struct ll_scan_aux_set *aux, struct node_rx_hdr *rx);
 static void ticker_cb(uint32_t ticks_at_expire, uint32_t remainder,
 		      uint16_t lazy, uint8_t force, void *param);
@@ -338,7 +340,14 @@ ull_scan_aux_rx_flush:
 #endif /* CONFIG_BT_CTLR_SYNC_PERIODIC */
 
 	if (aux) {
-		flush(aux, rx);
+		struct ull_hdr *hdr;
+
+		/* Setup the disabled callback to flush the auxiliary PDUs */
+		hdr = &aux->ull;
+		LL_ASSERT(!hdr->disabled_cb);
+
+		hdr->disabled_param = rx;
+		hdr->disabled_cb = last_disabled_cb;
 
 		return;
 	}
@@ -351,8 +360,14 @@ void ull_scan_aux_done(struct node_rx_event_done *done)
 {
 	struct lll_scan_aux *lll = (void *)HDR_ULL2LLL(done->param);
 	struct ll_scan_aux_set *aux = (void *)HDR_LLL2EVT(lll);
+	struct ull_hdr *hdr;
 
-	flush(aux, NULL);
+	/* Setup the disabled callback to flush the auxiliary PDUs */
+	hdr = &aux->ull;
+	LL_ASSERT(!hdr->disabled_cb);
+
+	hdr->disabled_param = aux;
+	hdr->disabled_cb = done_disabled_cb;
 }
 
 uint8_t ull_scan_aux_lll_handle_get(struct lll_scan_aux *lll)
@@ -393,6 +408,22 @@ static inline struct ll_sync_set *sync_create_get(struct ll_scan_set *scan)
 #else /* !CONFIG_BT_CTLR_SYNC_PERIODIC */
 	return NULL;
 #endif /* !CONFIG_BT_CTLR_SYNC_PERIODIC */
+}
+
+static void last_disabled_cb(void *param)
+{
+	struct ll_scan_aux_set *aux;
+	struct node_rx_hdr *rx;
+
+	rx = param;
+	aux = (void *)HDR_LLL2EVT(rx->rx_ftr.param);
+
+	flush(aux, rx);
+}
+
+static void done_disabled_cb(void *param)
+{
+	flush(param, NULL);
 }
 
 static void flush(struct ll_scan_aux_set *aux, struct node_rx_hdr *rx)

subsys/bluetooth/controller/ll_sw/ull_scan.c
@@ -855,6 +855,7 @@ static void ticker_op_ext_stop_cb(uint32_t status, void *param)
 		uint32_t ret;
 
 		LL_ASSERT(!hdr->disabled_cb);
+
 		hdr->disabled_param = mfy.param;
 		hdr->disabled_cb = ext_disabled_cb;
 

commit 19f4a5f9bb90c9fc13d3914f0d7ea89b04b2b135

Bluetooth: Controller: Fix auxiliary context release on scan done
Fix auxiliary context release on scan done, do not wait for
reference count to reduce when Periodic Sync events overlap.

Regression introduced in commit a3fbea2933ae ("Bluetooth:
controller: Fix auxiliary scan context release").

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ull_scan_aux.c
@@ -746,7 +746,6 @@ ull_scan_aux_rx_flush:
 void ull_scan_aux_done(struct node_rx_event_done *done)
 {
 	struct ll_scan_aux_set *aux;
-	struct ull_hdr *hdr;
 
 	/* Get reference to ULL context */
 	aux = CONTAINER_OF(done->param, struct ll_scan_aux_set, ull);
@@ -757,8 +756,8 @@ void ull_scan_aux_done(struct node_rx_event_done *done)
 
 		sync = CONTAINER_OF(done->param, struct ll_sync_set, ull);
 		LL_ASSERT(ull_sync_is_valid_get(sync));
-		hdr = &sync->ull;
 
+		/* Auxiliary context will be flushed by ull_scan_aux_stop() */
 		if (unlikely(sync->is_stop) || !sync->lll.lll_aux) {
 			return;
 		}
@@ -778,18 +777,9 @@ void ull_scan_aux_done(struct node_rx_event_done *done)
 		if (unlikely(scan->is_stop)) {
 			return;
 		}
-
-		/* Setup the disabled callback to flush the auxiliary PDUs */
-		hdr = &aux->ull;
 	}
 
-	if (ull_ref_get(hdr) == 0U) {
-		flush(aux);
-	} else {
-		LL_ASSERT(!hdr->disabled_cb);
-		hdr->disabled_param = aux;
-		hdr->disabled_cb = done_disabled_cb;
-	}
+	flush(aux);
 }
 
 struct ll_scan_aux_set *ull_scan_aux_set_get(uint8_t handle)