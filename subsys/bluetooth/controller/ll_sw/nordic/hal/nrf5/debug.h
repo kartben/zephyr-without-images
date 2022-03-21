kartben/zephyr-without-images

commit 724e3cb1d58d29255261f79f903dbb6b78d03ec0

Bluetooth: controller: Enable debug pins with TF-M enabled
Enable debug pins when TF-M has been enabled for the image.
This would otherwise produce an error message as the DEBUG_SETUP would
not have been defined for the non-secure image.

Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/debug.h
@@ -8,6 +8,7 @@
 #if defined(CONFIG_BT_CTLR_DEBUG_PINS) || \
 	defined(CONFIG_BT_CTLR_DEBUG_PINS_CPUAPP)
 #if defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP) || \
+	defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP_NS) || \
 	defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUNET)
 #define DEBUG_PORT       NRF_P1
 #define DEBUG_PIN_IDX0   0
@@ -30,39 +31,20 @@
 #define DEBUG_PIN7       BIT(DEBUG_PIN_IDX7)
 #define DEBUG_PIN8       BIT(DEBUG_PIN_IDX8)
 #define DEBUG_PIN9       BIT(DEBUG_PIN_IDX9)
-#if defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP)
+#if defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP) || \
+	(defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP_NS) && defined(CONFIG_BUILD_WITH_TFM))
 #define DEBUG_SETUP() \
 	do { \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX0] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX1] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX2] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX3] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX4] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX5] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX6] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX7] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX8] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
-		DEBUG_PORT->PIN_CNF[DEBUG_PIN_IDX9] = \
-			(GPIO_PIN_CNF_MCUSEL_NetworkMCU << \
-			 GPIO_PIN_CNF_MCUSEL_Pos); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX0, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX1, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX2, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX3, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX4, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX5, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX6, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX7, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX8, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX9, NRF_GPIO_PIN_MCUSEL_NETWORK);
 	} while (0)
 #endif /* CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP */
 #elif defined(CONFIG_BOARD_NRF52840DK_NRF52840) || \

commit 137969a0b533df3b0a34e3ed731f2e44521eb19b

Bluetooth: Controller: nRF53: Fix compile error when debug pins enabled
Fix compile error when debug pins are enabled for nRF53
Series SoCs.

Regression introduced in commit 724e3cb1d58d ("Bluetooth:
controller: Enable debug pins with TF-M enabled").

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/debug.h
@@ -35,16 +35,16 @@
 	(defined(CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP_NS) && defined(CONFIG_BUILD_WITH_TFM))
 #define DEBUG_SETUP() \
 	do { \
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX0, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX1, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX2, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX3, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX4, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX5, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX6, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX7, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX8, NRF_GPIO_PIN_MCUSEL_NETWORK);
-		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX9, NRF_GPIO_PIN_MCUSEL_NETWORK);
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX0, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX1, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX2, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX3, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX4, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX5, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX6, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX7, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX8, NRF_GPIO_PIN_MCUSEL_NETWORK); \
+		soc_secure_gpio_pin_mcu_select(32 + DEBUG_PIN_IDX9, NRF_GPIO_PIN_MCUSEL_NETWORK); \
 	} while (0)
 #endif /* CONFIG_BOARD_NRF5340DK_NRF5340_CPUAPP */
 #elif defined(CONFIG_BOARD_NRF52840DK_NRF52840) || \