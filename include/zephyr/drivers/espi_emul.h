zephyrproject-rtos/zephyr

emul: Simplify emulator bus initialization and API
Allow emulator creators to write an init function that can be used
across multiple busses so as to reduce the boilerplate and cognitive
load in creating an emulator.

Part of this change includes allowing access to the emul struct from a
field in a {bus}_struct api (e.g. i2c_struct), which removes the need for
sporadic usages of CONTAINER_OF to access the emul struct.

Overall, this change simplifies and reduces the amount of boilerplate
code to get a device emulator up and running, thus reducing excise work
to writing tests.

TEST=twister on accel,espi, and eeprom drivers tests

Signed-off-by: Aaron Massey <aaronmassey@google.com>

include/zephyr/drivers/espi_emul.h
@@ -7,16 +7,16 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_ESPI_SPI_EMUL_H_
 #define ZEPHYR_INCLUDE_DRIVERS_ESPI_SPI_EMUL_H_
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/espi.h>
+#include <zephyr/types.h>
+
 /**
  * @file
  *
  * @brief Public APIs for the eSPI emulation drivers.
  */
-
-#include <zephyr/types.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/espi.h>
-#include <zephyr/drivers/emul.h>
 
 /**
  * @brief eSPI Emulation Interface
@@ -37,39 +37,39 @@ struct espi_emul;
  * Passes eSPI virtual wires set request (virtual wire packet) to the emulator.
  * The emulator updates the state (level) of its virtual wire.
  *
- * @param emul Emulator instance
+ * @param emulator The device Emulator instance
  * @param vw The signal to be set.
  * @param level The level of signal requested LOW(0) or HIGH(1).
  *
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*emul_espi_api_set_vw)(struct espi_emul *emul, enum espi_vwire_signal vw,
+typedef int (*emul_espi_api_set_vw)(const struct emul *emulator, enum espi_vwire_signal vw,
 				    uint8_t level);
 
 /**
  * Passes eSPI virtual wires get request (virtual wire packet) to the emulator.
  * The emulator returns the state (level) of its virtual wire.
  *
- * @param emul Emulator instance
+ * @param emulator The device Emulator instance
  * @param vw The signal to be get.
  * @param level The level of the signal to be get.
  *
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*emul_espi_api_get_vw)(struct espi_emul *emul, enum espi_vwire_signal vw,
+typedef int (*emul_espi_api_get_vw)(const struct emul *emulator, enum espi_vwire_signal vw,
 				    uint8_t *level);
 
 #ifdef CONFIG_ESPI_PERIPHERAL_ACPI_SHM_REGION
 /**
  * Get the ACPI shared memory address owned by the emulator.
  *
- * @param emul Emulator instance.
+ * @param emulator The device Emulator instance
  *
  * @retval The address of the memory.
  */
-typedef uintptr_t (*emul_espi_api_get_acpi_shm)(struct espi_emul *emul);
+typedef uintptr_t (*emul_espi_api_get_acpi_shm)(const struct emul *emulator);
 #endif
 
 /**
@@ -109,8 +109,8 @@ struct emul_espi_device_api {
 /** Node in a linked list of emulators for eSPI devices */
 struct espi_emul {
 	sys_snode_t node;
-	/** Parent emulator */
-	const struct emul *parent;
+	/** Target emulator - REQUIRED for all emulated bus nodes of any type */
+	const struct emul *target;
 	/** API provided for this device */
 	const struct emul_espi_device_api *api;
 	/** eSPI chip-select of the emulated device */

include/zephyr/drivers/i2c_emul.h
@@ -13,16 +13,16 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_I2C_I2C_EMUL_H_
 #define ZEPHYR_INCLUDE_DRIVERS_I2C_I2C_EMUL_H_
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/types.h>
+
 /**
  * @brief I2C Emulation Interface
  * @defgroup i2c_emul_interface I2C Emulation Interface
  * @ingroup io_emulators
  * @{
  */
-
-#include <zephyr/types.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/emul.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -35,8 +35,8 @@ struct i2c_emul_api;
 struct i2c_emul {
 	sys_snode_t node;
 
-	/** Parent emulator */
-	const struct emul *parent;
+	/** Target emulator - REQUIRED for all emulated bus nodes of any type */
+	const struct emul *target;
 
 	/* API provided for this device */
 	const struct i2c_emul_api *api;
@@ -49,16 +49,16 @@ struct i2c_emul {
  * Passes I2C messages to the emulator. The emulator updates the data with what
  * was read back.
  *
- * @param emul Emulator instance
+ * @param emulator The device Emulator instance.
  * @param msgs Array of messages to transfer. For 'read' messages, this function
- *	updates the 'buf' member with the data that was read
+ *	updates the 'buf' member with the data that was read.
  * @param num_msgs Number of messages to transfer.
  * @param addr Address of the I2C target device.
  *
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*i2c_emul_transfer_t)(struct i2c_emul *emul, struct i2c_msg *msgs, int num_msgs,
+typedef int (*i2c_emul_transfer_t)(const struct emul *emulator, struct i2c_msg *msgs, int num_msgs,
 				   int addr);
 
 /**

include/zephyr/drivers/spi_emul.h
@@ -7,15 +7,16 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SPI_SPI_EMUL_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SPI_SPI_EMUL_H_
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/types.h>
+
 /**
  * @file
  *
  * @brief Public APIs for the SPI emulation drivers.
  */
-
-#include <zephyr/types.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/emul.h>
 
 /**
  * @brief SPI Emulation Interface
@@ -35,8 +36,8 @@ struct spi_emul_api;
 struct spi_emul {
 	sys_snode_t node;
 
-	/** Parent emulator */
-	const struct emul *parent;
+	/** Target emulator - REQUIRED for all bus emulators */
+	const struct emul *target;
 
 	/* API provided for this device */
 	const struct spi_emul_api *api;
@@ -49,7 +50,7 @@ struct spi_emul {
  * Passes SPI messages to the emulator. The emulator updates the data with what
  * was read back.
  *
- * @param emul Emulator instance
+ * @param emulator The device Emulator instance
  * @param config Pointer to a valid spi_config structure instance.
  *        Pointer-comparison may be used to detect changes from
  *        previous operations.
@@ -61,7 +62,7 @@ struct spi_emul {
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*spi_emul_io_t)(struct spi_emul *emul, const struct spi_config *config,
+typedef int (*spi_emul_io_t)(const struct emul *emulator, const struct spi_config *config,
 			     const struct spi_buf_set *tx_bufs, const struct spi_buf_set *rx_bufs);
 
 /**

kartben/zephyr-without-images

include/zephyr/drivers/espi_emul.h
@@ -7,16 +7,16 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_ESPI_SPI_EMUL_H_
 #define ZEPHYR_INCLUDE_DRIVERS_ESPI_SPI_EMUL_H_
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/espi.h>
+#include <zephyr/types.h>
+
 /**
  * @file
  *
  * @brief Public APIs for the eSPI emulation drivers.
  */
-
-#include <zephyr/types.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/espi.h>
-#include <zephyr/drivers/emul.h>
 
 /**
  * @brief eSPI Emulation Interface
@@ -37,39 +37,39 @@ struct espi_emul;
  * Passes eSPI virtual wires set request (virtual wire packet) to the emulator.
  * The emulator updates the state (level) of its virtual wire.
  *
- * @param emul Emulator instance
+ * @param target The device Emulator instance
  * @param vw The signal to be set.
  * @param level The level of signal requested LOW(0) or HIGH(1).
  *
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*emul_espi_api_set_vw)(struct espi_emul *emul, enum espi_vwire_signal vw,
+typedef int (*emul_espi_api_set_vw)(const struct emul *target, enum espi_vwire_signal vw,
 				    uint8_t level);
 
 /**
  * Passes eSPI virtual wires get request (virtual wire packet) to the emulator.
  * The emulator returns the state (level) of its virtual wire.
  *
- * @param emul Emulator instance
+ * @param target The device Emulator instance
  * @param vw The signal to be get.
  * @param level The level of the signal to be get.
  *
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*emul_espi_api_get_vw)(struct espi_emul *emul, enum espi_vwire_signal vw,
+typedef int (*emul_espi_api_get_vw)(const struct emul *target, enum espi_vwire_signal vw,
 				    uint8_t *level);
 
 #ifdef CONFIG_ESPI_PERIPHERAL_ACPI_SHM_REGION
 /**
  * Get the ACPI shared memory address owned by the emulator.
  *
- * @param emul Emulator instance.
+ * @param target The device Emulator instance
  *
  * @retval The address of the memory.
  */
-typedef uintptr_t (*emul_espi_api_get_acpi_shm)(struct espi_emul *emul);
+typedef uintptr_t (*emul_espi_api_get_acpi_shm)(const struct emul *target);
 #endif
 
 /**
@@ -80,7 +80,7 @@ typedef uintptr_t (*emul_espi_api_get_acpi_shm)(struct espi_emul *emul);
  *
  * @param dev eSPI emulation controller device
  * @param chipsel Chip-select value
- * @return emulator to use
+ * @return espi_emul to use
  * @return NULL if not found
  */
 typedef struct espi_emul *(*emul_find_emul)(const struct device *dev, unsigned int chipsel);
@@ -109,8 +109,8 @@ struct emul_espi_device_api {
 /** Node in a linked list of emulators for eSPI devices */
 struct espi_emul {
 	sys_snode_t node;
-	/** Parent emulator */
-	const struct emul *parent;
+	/** Target emulator - REQUIRED for all emulated bus nodes of any type */
+	const struct emul *target;
 	/** API provided for this device */
 	const struct emul_espi_device_api *api;
 	/** eSPI chip-select of the emulated device */

include/zephyr/drivers/i2c_emul.h
@@ -13,16 +13,16 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_I2C_I2C_EMUL_H_
 #define ZEPHYR_INCLUDE_DRIVERS_I2C_I2C_EMUL_H_
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/types.h>
+
 /**
  * @brief I2C Emulation Interface
  * @defgroup i2c_emul_interface I2C Emulation Interface
  * @ingroup io_emulators
  * @{
  */
-
-#include <zephyr/types.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/emul.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -35,8 +35,8 @@ struct i2c_emul_api;
 struct i2c_emul {
 	sys_snode_t node;
 
-	/** Parent emulator */
-	const struct emul *parent;
+	/** Target emulator - REQUIRED for all emulated bus nodes of any type */
+	const struct emul *target;
 
 	/* API provided for this device */
 	const struct i2c_emul_api *api;
@@ -49,16 +49,16 @@ struct i2c_emul {
  * Passes I2C messages to the emulator. The emulator updates the data with what
  * was read back.
  *
- * @param emul Emulator instance
+ * @param target The device Emulator instance.
  * @param msgs Array of messages to transfer. For 'read' messages, this function
- *	updates the 'buf' member with the data that was read
+ *	updates the 'buf' member with the data that was read.
  * @param num_msgs Number of messages to transfer.
  * @param addr Address of the I2C target device.
  *
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*i2c_emul_transfer_t)(struct i2c_emul *emul, struct i2c_msg *msgs, int num_msgs,
+typedef int (*i2c_emul_transfer_t)(const struct emul *target, struct i2c_msg *msgs, int num_msgs,
 				   int addr);
 
 /**

include/zephyr/drivers/spi_emul.h
@@ -7,15 +7,16 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_SPI_SPI_EMUL_H_
 #define ZEPHYR_INCLUDE_DRIVERS_SPI_SPI_EMUL_H_
 
+#include <zephyr/device.h>
+#include <zephyr/drivers/emul.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/types.h>
+
 /**
  * @file
  *
  * @brief Public APIs for the SPI emulation drivers.
  */
-
-#include <zephyr/types.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/emul.h>
 
 /**
  * @brief SPI Emulation Interface
@@ -35,8 +36,8 @@ struct spi_emul_api;
 struct spi_emul {
 	sys_snode_t node;
 
-	/** Parent emulator */
-	const struct emul *parent;
+	/** Target emulator - REQUIRED for all bus emulators */
+	const struct emul *target;
 
 	/* API provided for this device */
 	const struct spi_emul_api *api;
@@ -49,7 +50,7 @@ struct spi_emul {
  * Passes SPI messages to the emulator. The emulator updates the data with what
  * was read back.
  *
- * @param emul Emulator instance
+ * @param target The device Emulator instance
  * @param config Pointer to a valid spi_config structure instance.
  *        Pointer-comparison may be used to detect changes from
  *        previous operations.
@@ -61,7 +62,7 @@ struct spi_emul {
  * @retval 0 If successful.
  * @retval -EIO General input / output error.
  */
-typedef int (*spi_emul_io_t)(struct spi_emul *emul, const struct spi_config *config,
+typedef int (*spi_emul_io_t)(const struct emul *target, const struct spi_config *config,
 			     const struct spi_buf_set *tx_bufs, const struct spi_buf_set *rx_bufs);
 
 /**