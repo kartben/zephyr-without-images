kartben/zephyr-without-images

commit cfd968a455de47a4597a624e52ae19d5176c3475

drivers: memc: don't check pin control return code
Some SOCs, such as the RT1064 and RT1024, use internal flash and don't
define pinmux settings for the flexspi. Don't check the return code of
pinctrl_apply_state, because the flexspi driver will fail to initialize
when the pin mux settings are simply not required.

Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

drivers/memc/memc_mcux_flexspi.c
@@ -143,8 +143,11 @@ static int memc_flexspi_init(const struct device *dev)
 		return 0;
 	}
 
-	/* Apply pinctrl state */
-#ifdef CONFIG_PINCTRL
+	/*
+	 * SOCs such as the RT1064 and RT1024 have internal flash, and no pinmux
+	 * settings, so skip pin control state.
+	 */
+#if defined(CONFIG_PINCTRL) && !(defined(CONFIG_SOC_MIMXRT1064) || defined(CONFIG_SOC_MIMXRT1024))
 	int ret;
 
 	ret = pinctrl_apply_state(data->pincfg, PINCTRL_STATE_DEFAULT);

commit 7ead27235c0bdea365b5986d5eefc9316330b6e3

drivers: memc: mcux_flexspi: Always try to apply pinctrl state
This commit fixes an issue introduced in cfd968a where FlexSPI
pinctrl states where applied based on the SoC type.
However multiple FlexSPI instances can be active, some of which
are not pre-configured from ROM.

Signed-off-by: Pieter De Gendt <pieter.degendt@basalte.be>

drivers/memc/memc_mcux_flexspi.c
@@ -145,13 +145,13 @@ static int memc_flexspi_init(const struct device *dev)
 
 	/*
 	 * SOCs such as the RT1064 and RT1024 have internal flash, and no pinmux
-	 * settings, so skip pin control state.
+	 * settings, continue if no pinctrl state found.
 	 */
-#if defined(CONFIG_PINCTRL) && !(defined(CONFIG_SOC_MIMXRT1064) || defined(CONFIG_SOC_MIMXRT1024))
+#ifdef CONFIG_PINCTRL
 	int ret;
 
 	ret = pinctrl_apply_state(data->pincfg, PINCTRL_STATE_DEFAULT);
-	if (ret < 0) {
+	if (ret < 0 && ret != -ENOENT) {
 		return ret;
 	}
 #endif