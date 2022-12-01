erbr-ot/zephyr

Bluetooth: controller: store STO value instead of pre-calculated reload
Instead of storing the calculated reload value (in conn intervals) in
the conn object, now store the timeout value, and perform calculation
when used. This allows using the timeout value across all CIS's without
having to re-calc and store on change of TO

Signed-off-by: Erik Brockhoff <erbr@oticon.com>

subsys/bluetooth/controller/ll_sw/ull_central.c
@@ -287,8 +287,7 @@ uint8_t ll_create_connection(uint16_t scan_interval, uint16_t scan_window,
 	conn->connect_expire = CONN_ESTAB_COUNTDOWN;
 	conn->supervision_expire = 0U;
 	conn_interval_us = (uint32_t)interval * CONN_INT_UNIT_US;
-	conn->supervision_reload = RADIO_CONN_EVENTS(timeout * 10000U,
-							 conn_interval_us);
+	conn->timeout = timeout;
 
 #if defined(CONFIG_BT_LL_SW_LLCP_LEGACY)
 	conn->procedure_expire = 0U;

subsys/bluetooth/controller/ll_sw/ull_conn_types.h
@@ -38,7 +38,7 @@ struct ll_conn {
 	struct lll_conn lll;
 
 	uint16_t connect_expire;
-	uint16_t supervision_reload;
+	uint16_t timeout;
 	uint16_t supervision_expire;
 	uint16_t procedure_reload;
 	uint16_t procedure_expire;
@@ -541,7 +541,7 @@ struct ll_conn {
 #endif /* CONFIG_BT_CTLR_LE_PING */
 
 	uint16_t connect_expire;
-	uint16_t supervision_reload;
+	uint16_t timeout;
 	uint16_t supervision_expire;
 	uint16_t procedure_reload;
 	uint16_t procedure_expire;

subsys/bluetooth/controller/ll_sw/ull_peripheral.c
@@ -93,7 +93,6 @@ void ull_periph_setup(struct node_rx_hdr *rx, struct node_rx_ftr *ftr,
 	uint16_t max_rx_time;
 	uint16_t win_offset;
 	memq_link_t *link;
-	uint16_t timeout;
 	uint8_t chan_sel;
 	void *node;
 
@@ -206,9 +205,7 @@ void ull_periph_setup(struct node_rx_hdr *rx, struct node_rx_ftr *ftr,
 		CONN_INT_UNIT_US;
 
 	/* procedure timeouts */
-	timeout = sys_le16_to_cpu(pdu_adv->connect_ind.timeout);
-	conn->supervision_reload =
-		RADIO_CONN_EVENTS((timeout * 10U * 1000U), conn_interval_us);
+	conn->timeout = sys_le16_to_cpu(pdu_adv->connect_ind.timeout);
 
 #if defined(CONFIG_BT_LL_SW_LLCP_LEGACY)
 	conn->procedure_reload =
@@ -287,7 +284,7 @@ void ull_periph_setup(struct node_rx_hdr *rx, struct node_rx_ftr *ftr,
 
 	cc->interval = lll->interval;
 	cc->latency = lll->latency;
-	cc->timeout = timeout;
+	cc->timeout = conn->timeout;
 	cc->sca = conn->periph.sca;
 
 	lll->handle = ll_conn_handle_get(conn);

tests/bluetooth/controller/common/src/helper_util.c
@@ -266,6 +266,8 @@ void test_setup(struct ll_conn *conn)
 
 	ll_reset();
 	conn->lll.event_counter = 0;
+	conn->lll.interval = 6;
+	conn->timeout = 600;
 	event_active[0] = 0;
 
 	memset(emul_conn_pool, 0x00, sizeof(emul_conn_pool));

kartben/zephyr-without-images

subsys/bluetooth/controller/ll_sw/ull_central.c
@@ -284,8 +284,7 @@ uint8_t ll_create_connection(uint16_t scan_interval, uint16_t scan_window,
 	conn->connect_expire = CONN_ESTAB_COUNTDOWN;
 	conn->supervision_expire = 0U;
 	conn_interval_us = (uint32_t)interval * CONN_INT_UNIT_US;
-	conn->supervision_reload = RADIO_CONN_EVENTS(timeout * 10000U,
-							 conn_interval_us);
+	conn->supervision_timeout = timeout;
 
 #if defined(CONFIG_BT_LL_SW_LLCP_LEGACY)
 	conn->procedure_expire = 0U;

subsys/bluetooth/controller/ll_sw/ull_conn_types.h
@@ -38,7 +38,7 @@ struct ll_conn {
 	struct lll_conn lll;
 
 	uint16_t connect_expire;
-	uint16_t supervision_reload;
+	uint16_t supervision_timeout;
 	uint16_t supervision_expire;
 	uint16_t procedure_reload;
 	uint16_t procedure_expire;
@@ -537,7 +537,7 @@ struct ll_conn {
 #endif /* CONFIG_BT_CTLR_LE_PING */
 
 	uint16_t connect_expire;
-	uint16_t supervision_reload;
+	uint16_t supervision_timeout;
 	uint16_t supervision_expire;
 	uint32_t connect_accept_to;
 

subsys/bluetooth/controller/ll_sw/ull_peripheral.c
@@ -91,7 +91,6 @@ void ull_periph_setup(struct node_rx_hdr *rx, struct node_rx_ftr *ftr,
 	uint16_t max_rx_time;
 	uint16_t win_offset;
 	memq_link_t *link;
-	uint16_t timeout;
 	uint8_t chan_sel;
 	void *node;
 
@@ -204,9 +203,7 @@ void ull_periph_setup(struct node_rx_hdr *rx, struct node_rx_ftr *ftr,
 		CONN_INT_UNIT_US;
 
 	/* procedure timeouts */
-	timeout = sys_le16_to_cpu(pdu_adv->connect_ind.timeout);
-	conn->supervision_reload =
-		RADIO_CONN_EVENTS((timeout * 10U * 1000U), conn_interval_us);
+	conn->supervision_timeout = sys_le16_to_cpu(pdu_adv->connect_ind.timeout);
 
 #if defined(CONFIG_BT_LL_SW_LLCP_LEGACY)
 	conn->procedure_reload =
@@ -286,7 +283,7 @@ void ull_periph_setup(struct node_rx_hdr *rx, struct node_rx_ftr *ftr,
 
 	cc->interval = lll->interval;
 	cc->latency = lll->latency;
-	cc->timeout = timeout;
+	cc->timeout = conn->supervision_timeout;
 	cc->sca = conn->periph.sca;
 
 	lll->handle = ll_conn_handle_get(conn);

tests/bluetooth/controller/common/src/helper_util.c
@@ -269,6 +269,8 @@ void test_setup(struct ll_conn *conn)
 
 	ll_reset();
 	conn->lll.event_counter = 0;
+	conn->lll.interval = 6;
+	conn->supervision_timeout = 600;
 	event_active[0] = 0;
 
 	memset(emul_conn_pool, 0x00, sizeof(emul_conn_pool));