kartben/zephyr-without-images

commit bc2b57a88e0bdeb4a501dad4fe80f83876f3d169

samples: blinky_pwm: Fix sample for rpi_pico
The RP2040 needs a DTS overlay for the PWM to work with the
blinky_pwm examples. This commit adds the required overlay.

Signed-off-by: Yonatan Schachter <yonatan.schachter@gmail.com>

samples/basic/blinky_pwm/boards/rpi_pico.overlay
@@ -1,0 +1,9 @@
+&pwm_led0 {
+	status = "okay";
+};
+
+&pwm {
+	status = "okay";
+	divider-frac-4 = <255>;
+	divider-int-4 = <255>;
+};

commit 5ac611fe76f112586bb250a01c13f631888c637e

samples: basic: blinky_pwm: fix divider setting for rpi_pico
The fractional part of the divider value is a 4 bit value. Setting it to
255 leads to an overflow in pwm_rpi_get_clkdiv(). This has resulted in a
slight deviation from the reported timing, which is btw. printed in nsec.

Fixes: bc2b57a88e0b ("samples: blinky_pwm: Fix sample for rpi_pico")
Signed-off-by: Oliver Barta <o.barta89@gmail.com>

samples/basic/blinky_pwm/boards/rpi_pico.overlay
@@ -4,6 +4,6 @@
 
 &pwm {
 	status = "okay";
-	divider-frac-4 = <255>;
+	divider-frac-4 = <15>;
 	divider-int-4 = <255>;
 };

samples/basic/blinky_pwm/src/main.c
@@ -53,7 +53,7 @@ void main(void)
 		}
 	}
 
-	printk("Done calibrating; maximum/minimum periods %u/%lu usec\n",
+	printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
 	       max_period, MIN_PERIOD);
 
 	period = max_period;