kartben/zephyr-without-images

commit aa5fedc7ab7fd9c982571c219ce9b6e40c8231f3

lib: os: disable caches before reboot
This ensures that all pending writes are committed. This is relevant for
flags in .noinit SRAM that are read back after reset.

Signed-off-by: Armin Brauns <armin.brauns@embedded-solutions.at>

lib/os/reboot.c
@@ -14,6 +14,18 @@ extern void sys_arch_reboot(int type);
 FUNC_NORETURN void sys_reboot(int type)
 {
 	(void)irq_lock();
+
+	/* Disable caches to ensure all data is flushed */
+#if defined(CONFIG_ARCH_CACHE)
+#if defined(CONFIG_DCACHE)
+	sys_cache_data_disable();
+#endif /* CONFIG_DCACHE */
+
+#if defined(CONFIG_ICACHE)
+	sys_cache_instr_disable();
+#endif /* CONFIG_ICACHE */
+#endif /* CONFIG_ARCH_CACHE */
+
 	sys_clock_disable();
 
 	sys_arch_reboot(type);

commit 7e7cc74048c92f4c2c568be4b3e6e46a70fc2cb0

lib: os: reboot: include zephyr/cache.h
Include <zephyr/cache.h> in order to use the Zephyr cache APIs.

Fixes: aa5fedc7ab7fd9c982571c219ce9b6e40c8231f3

Signed-off-by: Henrik Brix Andersen <hebad@vestas.com>

lib/os/reboot.c
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/cache.h>
 #include <zephyr/drivers/timer/system_timer.h>
 #include <zephyr/sys/reboot.h>
 #include <zephyr/kernel.h>