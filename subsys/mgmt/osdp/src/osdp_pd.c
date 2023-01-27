kartben/zephyr-without-images

commit 09e69f391df21af075054025a9bdda14e6cf7522

mgmt/osdp: Add support for Secure Channel
This patch adds Secure Channel capabilities to osdp Control Panel and
Peripheral Device modes.

Signed-off-by: Siddharth Chandrasekaran <siddharth@embedjournal.com>

subsys/mgmt/osdp/src/osdp_pd.c
@@ -26,6 +26,9 @@ LOG_MODULE_DECLARE(osdp, CONFIG_OSDP_LOG_LEVEL);
 #define CMD_BUZ_DATA_LEN               5
 #define CMD_TEXT_DATA_LEN              6   /* variable length command */
 #define CMD_COMSET_DATA_LEN            5
+#define CMD_KEYSET_DATA_LEN            18
+#define CMD_CHLNG_DATA_LEN             8
+#define CMD_SCRYPT_DATA_LEN            16
 
 #define REPLY_ACK_LEN                  1
 #define REPLY_PDID_LEN                 13
@@ -35,6 +38,8 @@ LOG_MODULE_DECLARE(osdp, CONFIG_OSDP_LOG_LEVEL);
 #define REPLY_RSTATR_LEN               2
 #define REPLY_COM_LEN                  6
 #define REPLY_NAK_LEN                  2
+#define REPLY_CCRYPT_LEN               33
+#define REPLY_RMAC_I_LEN               17
 
 static struct osdp_pd_id osdp_pd_id = {
 	.version = CONFIG_OSDP_PD_ID_VERSION,
@@ -51,7 +56,16 @@ static struct osdp_pd_cap osdp_pd_cap[] = {
 		1, /* The PD supports the 16-bit CRC-16 mode */
 		0, /* N/A */
 	},
-
+	{
+		OSDP_PD_CAP_COMMUNICATION_SECURITY,
+#ifdef CONFIG_OSDP_SC_ENABLED
+		1, /* (Bit-0) AES128 support */
+		1, /* (Bit-0) default AES128 key */
+#else
+		0, /* SC not supported */
+		0, /* SC not supported */
+#endif
+	},
 	/* Configured from Kconfig */
 	{
 		OSDP_PD_CAP_CONTACT_STATUS_MONITORING,
@@ -93,7 +107,7 @@ static struct osdp_pd_cap osdp_pd_cap[] = {
 
 static void pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 {
-	int i, ret = -1, pos = 0;
+	int i, ret = -1, pos = 0, tmp;
 	struct osdp_cmd *cmd;
 
 	pd->reply_id = 0;
@@ -275,6 +289,75 @@ static void pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 		pd->reply_id = REPLY_COM;
 		ret = 0;
 		break;
+#ifdef CONFIG_OSDP_SC_ENABLED
+	case CMD_KEYSET:
+		if (len != CMD_KEYSET_DATA_LEN) {
+			LOG_ERR(TAG "CMD_KEYSET length mismatch! %d/18", len);
+			break;
+		}
+		/**
+		 * For CMD_KEYSET to be accepted, PD must be
+		 * ONLINE and SC_ACTIVE.
+		 */
+		if (ISSET_FLAG(pd, PD_FLAG_SC_ACTIVE) == 0) {
+			pd->reply_id = REPLY_NAK;
+			pd->cmd_data[0] = OSDP_PD_NAK_SC_COND;
+			LOG_ERR(TAG "Keyset with SC inactive");
+			break;
+		}
+		/* only key_type == 1 (SCBK) and key_len == 16 is supported */
+		if (buf[pos] != 1 || buf[pos + 1] != 16) {
+			LOG_ERR(TAG "Keyset invalid len/type: %d/%d",
+			      buf[pos], buf[pos + 1]);
+			break;
+		}
+		cmd = osdp_cmd_alloc(pd);
+		if (cmd == NULL) {
+			LOG_ERR(TAG "cmd alloc error");
+			break;
+		}
+		cmd->id = OSDP_CMD_KEYSET;
+		cmd->keyset.type   = buf[pos++];
+		cmd->keyset.length = buf[pos++];
+		memcpy(cmd->keyset.data, buf + pos, 16);
+		memcpy(pd->sc.scbk, buf + pos, 16);
+		osdp_cmd_enqueue(pd, cmd);
+		CLEAR_FLAG(pd, PD_FLAG_SC_USE_SCBKD);
+		CLEAR_FLAG(pd, PD_FLAG_INSTALL_MODE);
+		pd->reply_id = REPLY_ACK;
+		ret = 0;
+		break;
+	case CMD_CHLNG:
+		tmp = OSDP_PD_CAP_COMMUNICATION_SECURITY;
+		if (pd->cap[tmp].compliance_level == 0) {
+			pd->reply_id = REPLY_NAK;
+			pd->cmd_data[0] = OSDP_PD_NAK_SC_UNSUP;
+			break;
+		}
+		if (len != CMD_CHLNG_DATA_LEN) {
+			LOG_ERR(TAG "CMD_CHLNG length mismatch! %d/8", len);
+			break;
+		}
+		osdp_sc_init(pd);
+		CLEAR_FLAG(pd, PD_FLAG_SC_ACTIVE);
+		for (i = 0; i < 8; i++) {
+			pd->sc.cp_random[i] = buf[pos++];
+		}
+		pd->reply_id = REPLY_CCRYPT;
+		ret = 0;
+		break;
+	case CMD_SCRYPT:
+		if (len != CMD_SCRYPT_DATA_LEN) {
+			LOG_ERR(TAG "CMD_SCRYPT length mismatch! %d/16", len);
+			break;
+		}
+		for (i = 0; i < 16; i++) {
+			pd->sc.cp_cryptogram[i] = buf[pos++];
+		}
+		pd->reply_id = REPLY_RMAC_I;
+		ret = 0;
+		break;
+#endif /* CONFIG_OSDP_SC_ENABLED */
 	default:
 		pd->reply_id = REPLY_NAK;
 		pd->cmd_data[0] = OSDP_PD_NAK_CMD_UNKNOWN;
@@ -305,6 +388,9 @@ static int pd_build_reply(struct osdp_pd *pd, uint8_t *buf, int max_len)
 	struct osdp_cmd *cmd;
 
 	data_off = osdp_phy_packet_get_data_offset(pd, buf);
+#ifdef CONFIG_OSDP_SC_ENABLED
+	uint8_t *smb = osdp_phy_packet_get_smb(pd, buf);
+#endif
 	buf += data_off;
 	max_len -= data_off;
 	if (max_len <= 0) {
@@ -428,7 +514,71 @@ static int pd_build_reply(struct osdp_pd *pd, uint8_t *buf, int max_len)
 		buf[len++] = pd->cmd_data[0];
 		ret = 0;
 		break;
+#ifdef CONFIG_OSDP_SC_ENABLED
+	case REPLY_CCRYPT:
+		if (smb == NULL) {
+			break;
+		}
+		if (max_len < REPLY_CCRYPT_LEN) {
+			LOG_ERR(TAG "Out of buffer space!");
+			return -1;
+		}
+		osdp_fill_random(pd->sc.pd_random, 8);
+		osdp_compute_session_keys(TO_CTX(pd));
+		osdp_compute_pd_cryptogram(pd);
+		buf[len++] = pd->reply_id;
+		for (i = 0; i < 8; i++) {
+			buf[len++] = pd->sc.pd_client_uid[i];
+		}
+		for (i = 0; i < 8; i++) {
+			buf[len++] = pd->sc.pd_random[i];
+		}
+		for (i = 0; i < 16; i++) {
+			buf[len++] = pd->sc.pd_cryptogram[i];
+		}
+		smb[0] = 3;      /* length */
+		smb[1] = SCS_12; /* type */
+		smb[2] = ISSET_FLAG(pd, PD_FLAG_SC_USE_SCBKD) ? 0 : 1;
+		ret = 0;
+		break;
+	case REPLY_RMAC_I:
+		if (smb == NULL) {
+			break;
+		}
+		if (max_len < REPLY_RMAC_I_LEN) {
+			LOG_ERR(TAG "Out of buffer space!");
+			return -1;
+		}
+		osdp_compute_rmac_i(pd);
+		buf[len++] = pd->reply_id;
+		for (i = 0; i < 16; i++) {
+			buf[len++] = pd->sc.r_mac[i];
+		}
+		smb[0] = 3;       /* length */
+		smb[1] = SCS_14;  /* type */
+		if (osdp_verify_cp_cryptogram(pd) == 0) {
+			smb[2] = 1;  /* CP auth succeeded */
+			SET_FLAG(pd, PD_FLAG_SC_ACTIVE);
+			if (ISSET_FLAG(pd, PD_FLAG_SC_USE_SCBKD)) {
+				LOG_WRN(TAG "SC Active with SCBK-D");
+			} else {
+				LOG_INF(TAG "SC Active");
+			}
+		} else {
+			smb[2] = 0;  /* CP auth failed */
+			LOG_WRN(TAG "failed to verify CP_crypt");
+		}
+		ret = 0;
+		break;
+#endif /* CONFIG_OSDP_SC_ENABLED */
 	}
+
+#ifdef CONFIG_OSDP_SC_ENABLED
+	if (smb && (smb[1] > SCS_14) && ISSET_FLAG(pd, PD_FLAG_SC_ACTIVE)) {
+		smb[0] = 2; /* length */
+		smb[1] = (len > 1) ? SCS_18 : SCS_16;
+	}
+#endif /* CONFIG_OSDP_SC_ENABLED */
 
 	if (ret != 0) {
 		/* catch all errors and report it as a RECORD error to CP */
@@ -559,7 +709,8 @@ void osdp_update(struct osdp *ctx)
 		if (ret == 1) {
 			break;
 		}
-		if (ret == -1 || (pd->rx_buf_len > 0 &&
+		if (ret == -1 || ((pd->rx_buf_len > 0 ||
+		    ISSET_FLAG(pd, PD_FLAG_SC_ACTIVE)) &&
 		    osdp_millis_since(pd->tstamp) > OSDP_RESP_TOUT_MS)) {
 			/**
 			 * When we receive a command from PD after a timeout,
@@ -617,8 +768,9 @@ static void osdp_pd_set_attributes(struct osdp_pd *pd, struct osdp_pd_cap *cap,
 	}
 }
 
-int osdp_setup(struct osdp *ctx)
+int osdp_setup(struct osdp *ctx, uint8_t *key)
 {
+	ARG_UNUSED(key);
 	struct osdp_pd *pd;
 
 	if (ctx->cp->num_pd != 1) {
@@ -627,6 +779,15 @@ int osdp_setup(struct osdp *ctx)
 	pd = TO_PD(ctx, 0);
 	osdp_pd_set_attributes(pd, osdp_pd_cap, &osdp_pd_id);
 	SET_FLAG(pd, PD_FLAG_PD_MODE);
+#ifdef CONFIG_OSDP_SC_ENABLED
+	if (key == NULL) {
+		LOG_WRN(TAG "SCBK not provided. PD is in INSTALL_MODE");
+		SET_FLAG(pd, PD_FLAG_INSTALL_MODE);
+	} else {
+		memcpy(pd->sc.scbk, key, 16);
+	}
+	SET_FLAG(pd, PD_FLAG_SC_CAPABLE);
+#endif
 	return 0;
 }
 

include/mgmt/osdp.h
@@ -235,6 +235,12 @@ int osdp_cp_send_cmd_comset(int pd, struct osdp_cmd_comset *p);
 
 #endif /* CONFIG_OSDP_MODE_PD */
 
+#ifdef CONFIG_OSDP_SC_ENABLED
+
+uint32_t osdp_get_sc_status_mask(void);
+
+#endif
+
 #ifdef __cplusplus
 }
 #endif

subsys/mgmt/osdp/CMakeLists.txt
@@ -12,6 +12,9 @@ zephyr_library_sources_ifdef(CONFIG_OSDP_MODE_PD src/osdp_pd.c)
 # CP mode specific sources
 zephyr_library_sources_ifdef(CONFIG_OSDP_MODE_CP src/osdp_cp.c)
 
+# Secure Channel sources
+zephyr_library_sources_ifdef(CONFIG_OSDP_SC_ENABLED src/osdp_sc.c)
+
 # Common sources
 zephyr_library_sources_ifdef(CONFIG_OSDP
 	src/osdp_phy.c

subsys/mgmt/osdp/Kconfig
@@ -58,7 +58,7 @@ config OSDP_UART_BUFFER_LENGTH
 
 config OSDP_THREAD_STACK_SIZE
 	int "OSDP Thread stack size"
-	default 512
+	default 1024
 	help
 	  Thread stack size for osdp refresh thread
 
@@ -67,6 +67,36 @@ config OSDP_PACKET_TRACE
 	help
 	  Prints bytes sent/received over OSDP to console for debugging.
 	  LOG_HEXDUMP_DBG() is used to achieve this and can be very verbose.
+
+config OSDP_SC_ENABLED
+	bool "Enable OSDP Secure Channel"
+	default y
+	select CRYPTO
+	select CRYPTO_MBEDTLS_SHIM
+	select MBEDTLS
+	select MBEDTLS_CIPHER_CCM_ENABLED
+	select ENTROPY_GENERATOR
+	help
+	  Secure the OSDP communication channel with encryption and mutual
+	  authentication.
+
+if OSDP_SC_ENABLED
+
+config OSDP_SC_RETRY_WAIT_SEC
+	int "Retry wait time in seconds after a Secure Channel error"
+	default 600
+	help
+	  Time in seconds to wait after a secure channel failure, and before
+	  retrying to establish it.
+
+config OSDP_CRYPTO_DRV_NAME
+	string "Crypto driver to use with OSDP"
+	default "CRYPTO_MTLS"
+	help
+	  OSDP Secure Channel uses AES-128 to secure communication between
+	  CP and PD. Provide an available crypto driver name here.
+
+endif # OSDP_SC_ENABLED
 
 if OSDP_MODE_PD
 source "subsys/mgmt/osdp/Kconfig.pd"

subsys/mgmt/osdp/Kconfig.cp
@@ -41,3 +41,14 @@ config OSDP_PD_POLL_RATE
 	  The Control Panel must query the Peripheral Device periodically to
 	  maintain connection sequence and to get status and events. This option
 	  defined the number of times such a POLL command is sent per second.
+
+if OSDP_SC_ENABLED
+
+config OSDP_MASTER_KEY
+	string "Secure Channel Master Key"
+	default "NONE"
+	help
+	  Hexadecimal string representation of the the 16 byte OSDP Secure Channel
+	  master Key. This is a mandatory key when secure channel is enabled.
+
+endif # OSDP_SC_ENABLED

subsys/mgmt/osdp/Kconfig.pd
@@ -31,6 +31,22 @@ config OSDP_PD_ADDRESS
 	  Address 0x7F is reserved as a broadcast address to which all PDs would
 	  respond.
 
+if OSDP_SC_ENABLED
+
+config OSDP_PD_SCBK
+	string "Secure Channel Base Key (SCBK)"
+	default "NONE"
+	help
+	  Hexadecimal string representation of the the 16 byte OSDP PD Secure
+	  Channel Base Key. When this field is sent to "NONE", the PD is set to
+	  "Install Mode". In this mode, the PD would allow a CP to setup a secure
+	  channel with default SCBK. Once as secure channel is active with the
+	  default key, the CP can send a KEYSET command to set new keys to the PD.
+	  It is up to the user to make sure that the PD enters the "Install Mode"
+	  only during provisioning time (controlled environment).
+
+endif # OSDP_SC_ENABLED
+
 menu "Peripheral Device ID Information"
 
 config OSDP_PD_ID_VENDOR_CODE

subsys/mgmt/osdp/src/osdp.c
@@ -16,6 +16,12 @@
 
 LOG_MODULE_REGISTER(osdp, CONFIG_OSDP_LOG_LEVEL);
 
+#ifdef CONFIG_OSDP_MODE_PD
+#define OSDP_KEY_STRING CONFIG_OSDP_PD_SCBK
+#else
+#define OSDP_KEY_STRING CONFIG_OSDP_MASTER_KEY
+#endif
+
 struct osdp_device {
 	struct ring_buf rx_buf;
 	struct ring_buf tx_buf;
@@ -152,7 +158,8 @@ void osdp_refresh(void *arg1, void *arg2, void *arg3)
 static int osdp_init(const struct device *arg)
 {
 	ARG_UNUSED(arg);
-	uint8_t c;
+	int len;
+	uint8_t c, *key = NULL, key_buf[16];
 	struct osdp *ctx;
 	struct osdp_device *p = &osdp_device;
 	struct osdp_channel channel = {
@@ -202,7 +209,24 @@ static int osdp_init(const struct device *arg)
 		LOG_ERR("OSDP build ctx failed!");
 		k_panic();
 	}
-	if (osdp_setup(ctx)) {
+
+	if (IS_ENABLED(CONFIG_OSDP_SC_ENABLED)) {
+		if (strcmp(OSDP_KEY_STRING, "NONE") != 0) {
+			len = strlen(OSDP_KEY_STRING);
+			if (len != 32) {
+				LOG_ERR("Key string length must be 32");
+				k_panic();
+			}
+			len = hex2bin(OSDP_KEY_STRING, 32, key_buf, 16);
+			if (len != 16) {
+				LOG_ERR("Failed to parse key buffer");
+				k_panic();
+			}
+			key = key_buf;
+		}
+	}
+
+	if (osdp_setup(ctx, key)) {
 		LOG_ERR("Failed to setup OSDP device!");
 		k_panic();
 	}

subsys/mgmt/osdp/src/osdp_common.c
@@ -10,6 +10,11 @@
 #include <sys/crc.h>
 #include <logging/log.h>
 
+#ifdef CONFIG_OSDP_SC_ENABLED
+#include <crypto/cipher.h>
+#include <random/rand32.h>
+#endif
+
 #include "osdp_common.h"
 
 LOG_MODULE_DECLARE(osdp, CONFIG_OSDP_LOG_LEVEL);
@@ -74,3 +79,122 @@ struct osdp_cmd *osdp_cmd_get_last(struct osdp_pd *pd)
 {
 	return (struct osdp_cmd *)sys_slist_peek_tail(&pd->cmd.queue);
 }
+
+#ifdef CONFIG_OSDP_SC_ENABLED
+
+void osdp_encrypt(uint8_t *key, uint8_t *iv, uint8_t *data, int len)
+{
+	const struct device *dev;
+	struct cipher_ctx ctx = {
+		.keylen = 16,
+		.key.bit_stream = key,
+		.flags = CAP_NO_IV_PREFIX
+	};
+	struct cipher_pkt encrypt = {
+		.in_buf = data,
+		.in_len = len,
+		.out_buf = data,
+		.out_len = len
+	};
+
+	dev = device_get_binding(CONFIG_OSDP_CRYPTO_DRV_NAME);
+	if (dev == NULL) {
+		LOG_ERR("Failed to get crypto dev binding!");
+		return;
+	}
+
+	if (iv != NULL) {
+		if (cipher_begin_session(dev, &ctx,
+					 CRYPTO_CIPHER_ALGO_AES,
+					 CRYPTO_CIPHER_MODE_CBC,
+					 CRYPTO_CIPHER_OP_ENCRYPT)) {
+			LOG_ERR("Failed at cipher_begin_session");
+			return;
+		}
+		if (cipher_cbc_op(&ctx, &encrypt, iv)) {
+			LOG_ERR("CBC ENCRYPT - Failed");
+		}
+	} else {
+		if (cipher_begin_session(dev, &ctx,
+					 CRYPTO_CIPHER_ALGO_AES,
+					 CRYPTO_CIPHER_MODE_ECB,
+					 CRYPTO_CIPHER_OP_ENCRYPT)) {
+			LOG_ERR("Failed at cipher_begin_session");
+			return;
+		}
+		if (cipher_block_op(&ctx, &encrypt)) {
+			LOG_ERR("ECB ENCRYPT - Failed");
+		}
+	}
+	cipher_free_session(dev, &ctx);
+}
+
+void osdp_decrypt(uint8_t *key, uint8_t *iv, uint8_t *data, int len)
+{
+	const struct device *dev;
+	struct cipher_ctx ctx = {
+		.keylen = 16,
+		.key.bit_stream = key,
+		.flags = CAP_NO_IV_PREFIX
+	};
+	struct cipher_pkt decrypt = {
+		.in_buf = data,
+		.in_len = len,
+		.out_buf = data,
+		.out_len = len
+	};
+
+	dev = device_get_binding(CONFIG_OSDP_CRYPTO_DRV_NAME);
+	if (dev == NULL) {
+		LOG_ERR("Failed to get crypto dev binding!");
+		return;
+	}
+
+	if (iv != NULL) {
+		if (cipher_begin_session(dev, &ctx,
+					 CRYPTO_CIPHER_ALGO_AES,
+					 CRYPTO_CIPHER_MODE_CBC,
+					 CRYPTO_CIPHER_OP_DECRYPT)) {
+			LOG_ERR("Failed at cipher_begin_session");
+			return;
+		}
+		if (cipher_cbc_op(&ctx, &decrypt, iv)) {
+			LOG_ERR("CBC DECRYPT - Failed");
+		}
+	} else {
+		if (cipher_begin_session(dev, &ctx,
+					 CRYPTO_CIPHER_ALGO_AES,
+					 CRYPTO_CIPHER_MODE_ECB,
+					 CRYPTO_CIPHER_OP_DECRYPT)) {
+			LOG_ERR("Failed at cipher_begin_session");
+			return;
+		}
+		if (cipher_block_op(&ctx, &decrypt)) {
+			LOG_ERR("ECB DECRYPT - Failed");
+		}
+	}
+	cipher_free_session(dev, &ctx);
+}
+
+void osdp_fill_random(uint8_t *buf, int len)
+{
+	sys_csrand_get(buf, len);
+}
+
+uint32_t osdp_get_sc_status_mask(void)
+{
+	int i;
+	uint32_t mask = 0;
+	struct osdp_pd *pd;
+	struct osdp *ctx = osdp_get_ctx();
+
+	for (i = 0; i < NUM_PD(ctx); i++) {
+		pd = TO_PD(ctx, i);
+		if (ISSET_FLAG(pd, PD_FLAG_SC_ACTIVE)) {
+			mask |= 1 << i;
+		}
+	}
+	return mask;
+}
+
+#endif /* CONFIG_OSDP_SC_ENABLED */

commit ce2904f00d3acd2a2c43089c80f02373338fc977

mgmt/osdp: pd: Fix error reply code for CMD_KEYSET
By default, on errors, pd_decode_command replies with osdp_NAK with
sub-error code set to OSDP_PD_NAK_CMD_LEN (achieved using the ret ==
OSDP_PD_ERR_GENERIC check before return). This is works for all packet
framing errors; but when a more specific error code needs to be sent, ret
has to be set to something other than OSDP_PD_ERR_GENERIC (a suitable error
code happens to be OSDP_PD_ERR_REPLY) to prevent the tail check from
overwriting the error info.

In CMD_KEYSET, we fill a more specific error code but do not set the ret to
OSDP_PD_ERR_REPLY. It causes this error to be reported as a framing
error hence loosing some extended info about the error. Fix this issue by
reordering the checks a bit.

Fixes: 09e69f391df2 "mgmt/osdp: Add support for Secure Channel"
Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

subsys/mgmt/osdp/src/osdp_pd.c
@@ -466,8 +466,14 @@ static int pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 		if (len != CMD_KEYSET_DATA_LEN) {
 			break;
 		}
+		/* only key_type == 1 (SCBK) and key_len == 16 is supported */
+		if (buf[pos] != 1 || buf[pos + 1] != 16) {
+			LOG_ERR("Keyset invalid len/type: %d/%d",
+				buf[pos], buf[pos + 1]);
+			break;
+		}
+		ret = OSDP_PD_ERR_REPLY;
 		if (!pd_cmd_cap_ok(pd, NULL)) {
-			ret = OSDP_PD_ERR_REPLY;
 			break;
 		}
 		/**
@@ -480,12 +486,6 @@ static int pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 			LOG_ERR("Keyset with SC inactive");
 			break;
 		}
-		/* only key_type == 1 (SCBK) and key_len == 16 is supported */
-		if (buf[pos] != 1 || buf[pos + 1] != 16) {
-			LOG_ERR("Keyset invalid len/type: %d/%d",
-				buf[pos], buf[pos + 1]);
-			break;
-		}
 		cmd.id = OSDP_CMD_KEYSET;
 		cmd.keyset.type = buf[pos++];
 		cmd.keyset.length = buf[pos++];
@@ -494,7 +494,6 @@ static int pd_decode_command(struct osdp_pd *pd, uint8_t *buf, int len)
 			LOG_ERR("Keyset without a command callback! The SC new "
 				"SCBK will be lost when the PD reboots.");
 		} else if (!do_command_callback(pd, &cmd)) {
-			ret = OSDP_PD_ERR_REPLY;
 			break;
 		}
 		memcpy(pd->sc.scbk, cmd.keyset.data, 16);