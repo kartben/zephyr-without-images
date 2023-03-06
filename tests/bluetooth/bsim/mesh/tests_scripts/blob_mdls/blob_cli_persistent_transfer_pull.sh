Balaklaka/zephyr

tests: Bluetooth: Mesh: Add tests for BLOB Transfer in Pull mode
This commit adds several BabbleSim tests for BLOB transfer:
- A sanity test checking that BLOB Transfer completes successfully in
both, Push and Pull modes that runs transfer for 4 targets
- blob_cli_trans_resume test for Pull mode
- Test checking that BLOB client continues BLOB transfer if one of
targets times out while sending chunks.

Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_persistent_transfer_pull.sh
@@ -1,0 +1,11 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test that BLOB Client continues BLOB Transfer after one target timed out while sending chunks.
+RunTest mesh_blob \
+	blob_cli_trans_persistency_pull \
+	blob_srv_trans_persistency_pull \
+	blob_srv_trans_persistency_pull

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_trans_complete_pull.sh
@@ -1,0 +1,11 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test that BLOB Transfer completes successfully in Pull mode
+RunTest mesh_blob blob_cli_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete \
+	-- -argstest use-pull-mode=1

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_trans_complete_push.sh
@@ -1,0 +1,10 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test that BLOB Transfer completes successfully in Push mode
+RunTest mesh_blob blob_cli_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete

kartben/zephyr-without-images

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_persistent_transfer_pull.sh
@@ -1,0 +1,12 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test that BLOB Client continues BLOB Transfer after one target timed out while sending chunks.
+conf=prj_mesh1d1_conf
+RunTest blob_pst_pull \
+	blob_cli_trans_persistency_pull \
+	blob_srv_trans_persistency_pull \
+	blob_srv_trans_persistency_pull

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_trans_complete_pull.sh
@@ -1,0 +1,12 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test that BLOB Transfer completes successfully in Pull mode
+conf=prj_mesh1d1_conf
+RunTest blob_success_pull blob_cli_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete \
+	-- -argstest use-pull-mode=1

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_trans_complete_push.sh
@@ -1,0 +1,11 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# Test that BLOB Transfer completes successfully in Push mode
+conf=prj_mesh1d1_conf
+RunTest blob_success_push blob_cli_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete \
+	blob_srv_trans_complete blob_srv_trans_complete