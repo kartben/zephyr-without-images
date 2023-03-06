Balaklaka/zephyr

Bluetooth: Mesh: DFU models
Adds DFU models from the Mesh Model specification v1.1.

Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>

include/zephyr/bluetooth/mesh.h
@@ -25,6 +25,9 @@
 #include <zephyr/bluetooth/mesh/health_cli.h>
 #include <zephyr/bluetooth/mesh/blob_cli.h>
 #include <zephyr/bluetooth/mesh/blob_io_flash.h>
+#include <zephyr/bluetooth/mesh/dfu_srv.h>
+#include <zephyr/bluetooth/mesh/dfd_srv.h>
+#include <zephyr/bluetooth/mesh/dfu_cli.h>
 #include <zephyr/bluetooth/mesh/proxy.h>
 #include <zephyr/bluetooth/mesh/heartbeat.h>
 #include <zephyr/bluetooth/mesh/cdb.h>

kartben/zephyr-without-images

Co-authored-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
Co-authored-by: Krzysztof Kopyściński <krzysztof.kopyscinski@codecoup.pl>
Co-authored-by: Anders Storrø <anders.storro@nordicsemi.no>
Co-authored-by: Mia Koen <mia.koen@nordicsemi.no>
Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

include/zephyr/bluetooth/mesh.h
@@ -25,6 +25,9 @@
 #include <zephyr/bluetooth/mesh/health_cli.h>
 #include <zephyr/bluetooth/mesh/blob_cli.h>
 #include <zephyr/bluetooth/mesh/blob_io_flash.h>
+#include <zephyr/bluetooth/mesh/dfu_srv.h>
+#include <zephyr/bluetooth/mesh/dfu_cli.h>
+#include <zephyr/bluetooth/mesh/dfu_metadata.h>
 #include <zephyr/bluetooth/mesh/proxy.h>
 #include <zephyr/bluetooth/mesh/heartbeat.h>
 #include <zephyr/bluetooth/mesh/cdb.h>