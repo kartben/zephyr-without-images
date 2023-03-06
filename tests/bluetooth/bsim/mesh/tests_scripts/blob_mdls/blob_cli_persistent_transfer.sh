Balaklaka/zephyr

Tests: Bluetooth: Mesh: BLOB cli transfer persist
Tests that BLOB Client continues the Push BLOB Transfer procedure
as long as there is at least one active target.

Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_persistent_transfer.sh
@@ -1,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# The test instance sequence must stay as it is due to addressing scheme
+RunTest mesh_blob \
+	blob_cli_fail_on_persistency \
+	blob_srv_fail_on_block_start\
+	blob_srv_fail_on_block_get \
+	blob_srv_fail_on_xfer_get \
+	blob_srv_fail_on_nothing

kartben/zephyr-without-images

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_persistent_transfer.sh
@@ -1,0 +1,14 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+# The test instance sequence must stay as it is due to addressing scheme
+conf=prj_mesh1d1_conf
+RunTest blob_fail \
+	blob_cli_fail_on_persistency \
+	blob_srv_fail_on_block_start\
+	blob_srv_fail_on_block_get \
+	blob_srv_fail_on_xfer_get \
+	blob_srv_fail_on_nothing