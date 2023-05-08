zephyrproject-rtos/zephyr

Bluetooth: Audio: Make broadcast assistant params const
Make the add and mod source parameters `const`, as well
as the broadcast code.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

include/zephyr/bluetooth/audio/bap.h
@@ -550,8 +550,8 @@ struct bt_bap_broadcast_assistant_add_src_param {
  *
  * @return Error value. 0 on success, GATT error or ERRNO on fail.
  */
-int bt_bap_broadcast_assistant_add_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_add_src_param *param);
+int bt_bap_broadcast_assistant_add_src(
+	struct bt_conn *conn, const struct bt_bap_broadcast_assistant_add_src_param *param);
 
 /** Parameters for modifying a source */
 struct bt_bap_broadcast_assistant_mod_src_param {
@@ -582,8 +582,8 @@ struct bt_bap_broadcast_assistant_mod_src_param {
  *
  *  @return Error value. 0 on success, GATT error or ERRNO on fail.
  */
-int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_mod_src_param *param);
+int bt_bap_broadcast_assistant_mod_src(
+	struct bt_conn *conn, const struct bt_bap_broadcast_assistant_mod_src_param *param);
 
 /** @brief Set a broadcast code to the specified receive state.
  *
@@ -595,7 +595,7 @@ int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
  */
 int bt_bap_broadcast_assistant_set_broadcast_code(
 	struct bt_conn *conn, uint8_t src_id,
-	uint8_t broadcast_code[BT_BAP_BROADCAST_CODE_SIZE]);
+	const uint8_t broadcast_code[BT_BAP_BROADCAST_CODE_SIZE]);
 
 /** @brief Remove a source from the server.
  *

subsys/bluetooth/audio/bap_broadcast_assistant.c
@@ -691,7 +691,7 @@ int bt_bap_broadcast_assistant_scan_stop(struct bt_conn *conn)
 }
 
 int bt_bap_broadcast_assistant_add_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_add_src_param *param)
+				       const struct bt_bap_broadcast_assistant_add_src_param *param)
 {
 	struct bt_bap_bass_cp_add_src *cp;
 
@@ -761,7 +761,7 @@ int bt_bap_broadcast_assistant_add_src(struct bt_conn *conn,
 }
 
 int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_mod_src_param *param)
+				       const struct bt_bap_broadcast_assistant_mod_src_param *param)
 {
 	struct bt_bap_bass_cp_mod_src *cp;
 	bool known_recv_state;
@@ -846,7 +846,7 @@ int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
 
 int bt_bap_broadcast_assistant_set_broadcast_code(
 	struct bt_conn *conn, uint8_t src_id,
-	uint8_t broadcast_code[BT_BAP_BROADCAST_CODE_SIZE])
+	const uint8_t broadcast_code[BT_BAP_BROADCAST_CODE_SIZE])
 {
 	struct bt_bap_bass_cp_broadcase_code *cp;
 

kartben/zephyr-without-images

include/zephyr/bluetooth/audio/bap.h
@@ -2035,8 +2035,8 @@ struct bt_bap_broadcast_assistant_add_src_param {
  *
  * @return Error value. 0 on success, GATT error or ERRNO on fail.
  */
-int bt_bap_broadcast_assistant_add_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_add_src_param *param);
+int bt_bap_broadcast_assistant_add_src(
+	struct bt_conn *conn, const struct bt_bap_broadcast_assistant_add_src_param *param);
 
 /** Parameters for modifying a source */
 struct bt_bap_broadcast_assistant_mod_src_param {
@@ -2067,8 +2067,8 @@ struct bt_bap_broadcast_assistant_mod_src_param {
  *
  *  @return Error value. 0 on success, GATT error or ERRNO on fail.
  */
-int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_mod_src_param *param);
+int bt_bap_broadcast_assistant_mod_src(
+	struct bt_conn *conn, const struct bt_bap_broadcast_assistant_mod_src_param *param);
 
 /** @brief Set a broadcast code to the specified receive state.
  *
@@ -2079,7 +2079,8 @@ int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
  *  @return Error value. 0 on success, GATT error or ERRNO on fail.
  */
 int bt_bap_broadcast_assistant_set_broadcast_code(
-	struct bt_conn *conn, uint8_t src_id, uint8_t broadcast_code[BT_AUDIO_BROADCAST_CODE_SIZE]);
+	struct bt_conn *conn, uint8_t src_id,
+	const uint8_t broadcast_code[BT_AUDIO_BROADCAST_CODE_SIZE]);
 
 /** @brief Remove a source from the server.
  *

subsys/bluetooth/audio/bap_broadcast_assistant.c
@@ -693,7 +693,7 @@ int bt_bap_broadcast_assistant_scan_stop(struct bt_conn *conn)
 }
 
 int bt_bap_broadcast_assistant_add_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_add_src_param *param)
+				       const struct bt_bap_broadcast_assistant_add_src_param *param)
 {
 	struct bt_bap_bass_cp_add_src *cp;
 
@@ -763,7 +763,7 @@ int bt_bap_broadcast_assistant_add_src(struct bt_conn *conn,
 }
 
 int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
-				       struct bt_bap_broadcast_assistant_mod_src_param *param)
+				       const struct bt_bap_broadcast_assistant_mod_src_param *param)
 {
 	struct bt_bap_bass_cp_mod_src *cp;
 	bool known_recv_state;
@@ -849,7 +849,7 @@ int bt_bap_broadcast_assistant_mod_src(struct bt_conn *conn,
 
 int bt_bap_broadcast_assistant_set_broadcast_code(
 	struct bt_conn *conn, uint8_t src_id,
-	uint8_t broadcast_code[BT_AUDIO_BROADCAST_CODE_SIZE])
+	const uint8_t broadcast_code[BT_AUDIO_BROADCAST_CODE_SIZE])
 {
 	struct bt_bap_bass_cp_broadcase_code *cp;
 