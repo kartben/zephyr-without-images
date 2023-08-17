PavelVPV/zephyr

tests: Bluetooth: Mesh: add cfg mesh with mbedtls PSA to bsim CI
Commit adds configuration for mesh with mbedtls PSA
to CI BabbleSim tests as well as emulation of
the Internal Trustable Storage(ITS) based on Zephyr's
settings to run in parallel environment.

Signed-off-by: Aleksandr Khromykh <aleksandr.khromykh@nordicsemi.no>

tests/bsim/bluetooth/mesh/tests_scripts/access/access_ext_sub_cap.sh
@@ -8,3 +8,6 @@ RunTest mesh_access_extended_model_subs_cap access_sub_capacity_ext_model
 
 conf=prj_mesh1d1_conf
 RunTest mesh_access_extended_model_subs_cap_1d1 access_sub_capacity_ext_model
+
+conf=prj_psa_conf
+RunTest mesh_access_extended_model_subs_cap_psa access_sub_capacity_ext_model

tests/bsim/bluetooth/mesh/tests_scripts/advertiser/proxy_mixin.sh
@@ -24,3 +24,7 @@ RunTest mesh_adv_proxy_mixin adv_tx_proxy_mixin adv_rx_proxy_mixin
 conf=prj_mesh1d1_conf
 overlay=overlay_gatt_conf
 RunTest mesh_adv_proxy_mixin_1d1 adv_tx_proxy_mixin adv_rx_proxy_mixin
+
+conf=prj_psa_conf
+overlay=overlay_gatt_conf
+RunTest mesh_adv_proxy_mixin_psa adv_tx_proxy_mixin adv_rx_proxy_mixin

tests/bsim/bluetooth/mesh/tests_scripts/beacon/invalid.sh
@@ -12,3 +12,8 @@ conf=prj_mesh1d1_conf
 RunTest mesh_beacon_invalid_1d1 \
 	beacon_tx_invalid \
 	beacon_rx_invalid
+
+conf=prj_psa_conf
+RunTest mesh_beacon_invalid_psa \
+	beacon_tx_invalid \
+	beacon_rx_invalid

tests/bsim/bluetooth/mesh/tests_scripts/sar/stress_test.sh
@@ -16,3 +16,7 @@ source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 conf=prj_mesh1d1_conf
 RunTest sar_test \
 	sar_cli_max_len_sdu_send sar_srv_max_len_sdu_receive
+
+conf=prj_psa_conf
+RunTest sar_test_psa \
+	sar_cli_max_len_sdu_send sar_srv_max_len_sdu_receive

tests/bsim/bluetooth/mesh/tests_scripts/transport/va_collision.sh
@@ -9,3 +9,6 @@ RunTest mesh_transport_va_collision transport_tx_va_collision transport_rx_va_co
 
 conf=prj_mesh1d1_conf
 RunTest mesh_transport_va_collision_1d1 transport_tx_va_collision transport_rx_va_collision
+
+conf=prj_psa_conf
+RunTest mesh_transport_va_collision_psa transport_tx_va_collision transport_rx_va_collision

kartben/zephyr-without-images

tests/bsim/bluetooth/mesh/tests_scripts/access/access_ext_sub_cap.sh
@@ -8,3 +8,7 @@ RunTest mesh_access_extended_model_subs_cap access_sub_capacity_ext_model
 
 conf=prj_mesh1d1_conf
 RunTest mesh_access_extended_model_subs_cap_1d1 access_sub_capacity_ext_model
+
+conf=prj_mesh1d1_conf
+overlay=overlay_psa_conf
+RunTest mesh_access_extended_model_subs_cap_psa access_sub_capacity_ext_model

tests/bsim/bluetooth/mesh/tests_scripts/advertiser/proxy_mixin.sh
@@ -24,3 +24,7 @@ RunTest mesh_adv_proxy_mixin adv_tx_proxy_mixin adv_rx_proxy_mixin
 conf=prj_mesh1d1_conf
 overlay=overlay_gatt_conf
 RunTest mesh_adv_proxy_mixin_1d1 adv_tx_proxy_mixin adv_rx_proxy_mixin
+
+conf=prj_mesh1d1_conf
+overlay="overlay_gatt_conf_overlay_psa_conf"
+RunTest mesh_adv_proxy_mixin_psa adv_tx_proxy_mixin adv_rx_proxy_mixin

tests/bsim/bluetooth/mesh/tests_scripts/beacon/invalid.sh
@@ -12,3 +12,9 @@ conf=prj_mesh1d1_conf
 RunTest mesh_beacon_invalid_1d1 \
 	beacon_tx_invalid \
 	beacon_rx_invalid
+
+conf=prj_mesh1d1_conf
+overlay=overlay_psa_conf
+RunTest mesh_beacon_invalid_psa \
+	beacon_tx_invalid \
+	beacon_rx_invalid

tests/bsim/bluetooth/mesh/tests_scripts/sar/stress_test.sh
@@ -16,3 +16,8 @@ source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
 conf=prj_mesh1d1_conf
 RunTest sar_test \
 	sar_cli_max_len_sdu_send sar_srv_max_len_sdu_receive
+
+conf=prj_mesh1d1_conf
+overlay=overlay_psa_conf
+RunTest sar_test_psa \
+	sar_cli_max_len_sdu_send sar_srv_max_len_sdu_receive

tests/bsim/bluetooth/mesh/tests_scripts/transport/va_collision.sh
@@ -9,3 +9,7 @@ RunTest mesh_transport_va_collision transport_tx_va_collision transport_rx_va_co
 
 conf=prj_mesh1d1_conf
 RunTest mesh_transport_va_collision_1d1 transport_tx_va_collision transport_rx_va_collision
+
+conf=prj_mesh1d1_conf
+overlay=overlay_psa_conf
+RunTest mesh_transport_va_collision_psa transport_tx_va_collision transport_rx_va_collision