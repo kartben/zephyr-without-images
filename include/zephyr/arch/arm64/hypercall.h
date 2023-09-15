firscity/zephyr

xen: implement Xen domain control interface
Add Xen domctl API implementation for Zephyr as control domain.

Previously Zephyr OS was used as unprivileged Xen domain (Domain-U),
but it also can be used as lightweight Xen control domain (Domain-0).
To implement such fuctionality additional Xen interfaces are needed.
One of them is Xen domain controls (domctls) - it allows to create,
configure and manage Xen domains.

Also, used it as a possibility to update files copyright and licenses
identifiers in touched files.

Signed-off-by: Dmytro Firsov <dmytro_firsov@epam.com>

include/zephyr/arch/arm64/hypercall.h
@@ -1,7 +1,6 @@
+/* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (c) 2021-2022 EPAM Systems
- *
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021-2023 EPAM Systems
  */
 
 #ifndef ZEPHYR_INCLUDE_ARCH_ARM64_HYPERCALL_H_
@@ -14,5 +13,9 @@ int HYPERVISOR_event_channel_op(int op, void *param);
 int HYPERVISOR_hvm_op(int op, void *param);
 int HYPERVISOR_memory_op(int op, void *param);
 int HYPERVISOR_grant_table_op(int op, void *uop, unsigned int count);
+
+#ifdef CONFIG_XEN_INITIAL_DOMAIN
+int HYPERVISOR_domctl(void *param);
+#endif
 
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM64_HYPERCALL_H_ */

kartben/zephyr-without-images

include/zephyr/arch/arm64/hypercall.h
@@ -1,7 +1,6 @@
+/* SPDX-License-Identifier: Apache-2.0 */
 /*
- * Copyright (c) 2021-2022 EPAM Systems
- *
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021-2023 EPAM Systems
  */
 
 #ifndef ZEPHYR_INCLUDE_ARCH_ARM64_HYPERCALL_H_
@@ -14,5 +13,9 @@ int HYPERVISOR_event_channel_op(int op, void *param);
 int HYPERVISOR_hvm_op(int op, void *param);
 int HYPERVISOR_memory_op(int op, void *param);
 int HYPERVISOR_grant_table_op(int op, void *uop, unsigned int count);
+
+#ifdef CONFIG_XEN_DOM0
+int HYPERVISOR_domctl(void *param);
+#endif
 
 #endif /* ZEPHYR_INCLUDE_ARCH_ARM64_HYPERCALL_H_ */