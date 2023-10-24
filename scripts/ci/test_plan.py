PerMac/zephyr

scripts: Allow using alternative ignore-patters in test_plan.py
Expand test_plan.py args with --ignore-path. This allows to provide an
alternative lists of patterns for the script.

Signed-off-by: Maciej Perkowski <Maciej.Perkowski@nordicsemi.no>

scripts/ci/test_plan.py
@@ -85,7 +85,7 @@ class Tag:
         return "<Tag {}>".format(self.name)
 
 class Filters:
-    def __init__(self, modified_files, pull_request=False, platforms=[], no_path_name = False):
+    def __init__(self, modified_files, pull_request=False, platforms=[], no_path_name = False, ignore_path=None):
         self.modified_files = modified_files
         self.twister_options = []
         self.full_twister = False
@@ -95,6 +95,9 @@ class Filters:
         self.platforms = platforms
         self.default_run = False
         self.no_path_name = no_path_name
+        self.ignore_path = f"{zephyr_base}/scripts/ci/twister_ignore.txt"
+        if ignore_path:
+            self.ignore_path = ignore_path
 
     def process(self):
         self.find_tags()
@@ -263,7 +266,7 @@ class Filters:
             logging.info(f'Potential tag based filters: {exclude_tags}')
 
     def find_excludes(self, skip=[]):
-        with open("scripts/ci/twister_ignore.txt", "r") as twister_ignore:
+        with open(self.ignore_path, "r") as twister_ignore:
             ignores = twister_ignore.read().splitlines()
             ignores = filter(lambda x: not x.startswith("#"), ignores)
 
@@ -315,6 +318,8 @@ def parse_args():
             help="Number of tests per builder")
     parser.add_argument('--no-path-name', action="store_true",
             help="Don't put paths into test suites' names ")
+    parser.add_argument('--ignore-path', default=None,
+            help="Path to a text file with patterns of files to be matched against changed files")
 
     return parser.parse_args()
 
@@ -337,7 +342,7 @@ if __name__ == "__main__":
         print("\n".join(files))
         print("=========")
 
-    f = Filters(files, args.pull_request, args.platform, args.no_path_name)
+    f = Filters(files, args.pull_request, args.platform, args.no_path_name, args.ignore_path)
     f.process()
 
     # remove dupes and filtered cases

kartben/zephyr-without-images

scripts/ci/test_plan.py
@@ -93,7 +93,7 @@ class Tag:
         return "<Tag {}>".format(self.name)
 
 class Filters:
-    def __init__(self, modified_files, pull_request=False, platforms=[], detailed_test_id=True):
+    def __init__(self, modified_files, ignore_path, pull_request=False, platforms=[], detailed_test_id=True):
         self.modified_files = modified_files
         self.twister_options = []
         self.full_twister = False
@@ -103,6 +103,7 @@ class Filters:
         self.platforms = platforms
         self.default_run = False
         self.detailed_test_id = detailed_test_id
+        self.ignore_path = ignore_path
 
     def process(self):
         self.find_modules()
@@ -317,7 +318,7 @@ class Filters:
             logging.info(f'Potential tag based filters: {exclude_tags}')
 
     def find_excludes(self, skip=[]):
-        with open("scripts/ci/twister_ignore.txt", "r") as twister_ignore:
+        with open(self.ignore_path, "r") as twister_ignore:
             ignores = twister_ignore.read().splitlines()
             ignores = filter(lambda x: not x.startswith("#"), ignores)
 
@@ -373,6 +374,9 @@ def parse_args():
             help="Don't put paths into tests' names.")
     parser.add_argument('-r', '--repo-to-scan', default=None,
             help="Repo to scan")
+    parser.add_argument('--ignore-path',
+            default=os.path.join(zephyr_base, 'scripts', 'ci', 'twister_ignore.txt'),
+            help="Path to a text file with patterns of files to be matched against changed files")
 
     # Include paths in names by default.
     parser.set_defaults(detailed_test_id=True)
@@ -400,7 +404,7 @@ if __name__ == "__main__":
         print("\n".join(files))
         print("=========")
 
-    f = Filters(files, args.pull_request, args.platform, args.detailed_test_id)
+    f = Filters(files, args.ignore_path, args.pull_request, args.platform, args.detailed_test_id)
     f.process()
 
     # remove dupes and filtered cases