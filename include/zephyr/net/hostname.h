metratec/zephyr

net: add set hostname function
This commit adds a function to set the hostname on runtime.

Signed-off-by: Gerhard Jörges <joerges@metratec.com>

include/zephyr/net/hostname.h
@@ -22,10 +22,16 @@ extern "C" {
  * @{
  */
 
-#define NET_HOSTNAME_MAX_LEN				\
-	(sizeof(CONFIG_NET_HOSTNAME) - 1 +		\
-	 (IS_ENABLED(CONFIG_NET_HOSTNAME_UNIQUE) ?	\
-	  sizeof("0011223344556677") - 1 : 0))
+#if defined(CONFIG_NET_HOSTNAME_MAX_LEN)
+#define NET_HOSTNAME_MAX_LEN                                                                       \
+	MAX(CONFIG_NET_HOSTNAME_MAX_LEN,                                                           \
+	    (sizeof(CONFIG_NET_HOSTNAME) - 1 +                                                     \
+	     (IS_ENABLED(CONFIG_NET_HOSTNAME_UNIQUE) ? sizeof("0011223344556677") - 1 : 0)))
+#else
+#define NET_HOSTNAME_MAX_LEN                                                                       \
+	(sizeof(CONFIG_NET_HOSTNAME) - 1 +                                                         \
+	 (IS_ENABLED(CONFIG_NET_HOSTNAME_UNIQUE) ? sizeof("0011223344556677") - 1 : 0))
+#endif
 
 #if defined(CONFIG_NET_HOSTNAME_ENABLE)
 #define NET_HOSTNAME_SIZE NET_HOSTNAME_MAX_LEN + 1
@@ -48,6 +54,23 @@ static inline const char *net_hostname_get(void)
 	return "zephyr";
 }
 #endif /* CONFIG_NET_HOSTNAME_ENABLE */
+
+/**
+ * @brief Set the device hostname
+ *
+ * @param hostname new hostname as char array.
+ * @param len Length of the hostname array.
+ *
+ * @return 0 if ok, <0 on error
+ */
+#if defined(CONFIG_NET_HOSTNAME_DYNAMIC)
+int net_hostname_set(char *hostname, size_t len);
+#else
+static inline int net_hostname_set(char *hostname, size_t len)
+{
+	return -ENOTSUP;
+}
+#endif
 
 /**
  * @brief Initialize and set the device hostname.

subsys/net/hostname.c
@@ -17,14 +17,14 @@ LOG_MODULE_REGISTER(net_hostname, CONFIG_NET_HOSTNAME_LOG_LEVEL);
 #include <zephyr/net/net_core.h>
 #include <zephyr/net/net_mgmt.h>
 
-static char hostname[NET_HOSTNAME_SIZE];
+static char _hostname[NET_HOSTNAME_SIZE];
 
 static void trigger_net_event(void)
 {
 	if (IS_ENABLED(CONFIG_NET_MGMT_EVENT_INFO)) {
 		struct net_event_l4_hostname info;
 
-		memcpy(info.hostname, hostname, sizeof(hostname));
+		memcpy(info.hostname, _hostname, sizeof(_hostname));
 		net_mgmt_event_notify_with_info(NET_EVENT_HOSTNAME_CHANGED, NULL,
 						&info, sizeof(info));
 	} else {
@@ -34,8 +34,25 @@ static void trigger_net_event(void)
 
 const char *net_hostname_get(void)
 {
-	return hostname;
+	return _hostname;
 }
+
+#if defined(CONFIG_NET_HOSTNAME_DYNAMIC)
+int net_hostname_set(char *hostname, size_t len)
+{
+	if (len > NET_HOSTNAME_MAX_LEN) {
+		return -ENOMEM;
+	}
+
+	memcpy(_hostname, hostname, len);
+	_hostname[len] = 0;
+
+	NET_DBG("New hostname %s", _hostname);
+	trigger_net_event();
+
+	return 0;
+}
+#endif
 
 #if defined(CONFIG_NET_HOSTNAME_UNIQUE)
 int net_hostname_set_postfix(const uint8_t *hostname_postfix,
@@ -62,11 +79,11 @@ int net_hostname_set_postfix(const uint8_t *hostname_postfix,
 	}
 
 	for (i = 0; i < postfix_len; i++, pos += 2) {
-		snprintk(&hostname[sizeof(CONFIG_NET_HOSTNAME) - 1 + pos],
-			 2 + 1, "%02x", hostname_postfix[i]);
+		snprintk(&_hostname[sizeof(CONFIG_NET_HOSTNAME) - 1 + pos], 2 + 1, "%02x",
+			 hostname_postfix[i]);
 	}
 
-	NET_DBG("New hostname %s", hostname);
+	NET_DBG("New hostname %s", _hostname);
 
 #if !defined(CONFIG_NET_HOSTNAME_UNIQUE_UPDATE)
 	postfix_set = true;
@@ -79,7 +96,7 @@ int net_hostname_set_postfix(const uint8_t *hostname_postfix,
 
 void net_hostname_init(void)
 {
-	memcpy(hostname, CONFIG_NET_HOSTNAME, sizeof(CONFIG_NET_HOSTNAME) - 1);
+	memcpy(_hostname, CONFIG_NET_HOSTNAME, sizeof(CONFIG_NET_HOSTNAME) - 1);
 
 	NET_DBG("Hostname set to %s", CONFIG_NET_HOSTNAME);
 	trigger_net_event();

kartben/zephyr-without-images

include/zephyr/net/hostname.h
@@ -22,10 +22,16 @@ extern "C" {
  * @{
  */
 
-#define NET_HOSTNAME_MAX_LEN				\
-	(sizeof(CONFIG_NET_HOSTNAME) - 1 +		\
-	 (IS_ENABLED(CONFIG_NET_HOSTNAME_UNIQUE) ?	\
-	  sizeof("0011223344556677") - 1 : 0))
+#if defined(CONFIG_NET_HOSTNAME_MAX_LEN)
+#define NET_HOSTNAME_MAX_LEN                                                                       \
+	MAX(CONFIG_NET_HOSTNAME_MAX_LEN,                                                           \
+	    (sizeof(CONFIG_NET_HOSTNAME) - 1 +                                                     \
+	     (IS_ENABLED(CONFIG_NET_HOSTNAME_UNIQUE) ? sizeof("0011223344556677") - 1 : 0)))
+#else
+#define NET_HOSTNAME_MAX_LEN                                                                       \
+	(sizeof(CONFIG_NET_HOSTNAME) - 1 +                                                         \
+	 (IS_ENABLED(CONFIG_NET_HOSTNAME_UNIQUE) ? sizeof("0011223344556677") - 1 : 0))
+#endif
 
 #if defined(CONFIG_NET_HOSTNAME_ENABLE)
 #define NET_HOSTNAME_SIZE NET_HOSTNAME_MAX_LEN + 1
@@ -48,6 +54,23 @@ static inline const char *net_hostname_get(void)
 	return "zephyr";
 }
 #endif /* CONFIG_NET_HOSTNAME_ENABLE */
+
+/**
+ * @brief Set the device hostname
+ *
+ * @param host new hostname as char array.
+ * @param len Length of the hostname array.
+ *
+ * @return 0 if ok, <0 on error
+ */
+#if defined(CONFIG_NET_HOSTNAME_DYNAMIC)
+int net_hostname_set(char *host, size_t len);
+#else
+static inline int net_hostname_set(char *host, size_t len)
+{
+	return -ENOTSUP;
+}
+#endif
 
 /**
  * @brief Initialize and set the device hostname.

subsys/net/hostname.c
@@ -37,6 +37,23 @@ const char *net_hostname_get(void)
 	return hostname;
 }
 
+#if defined(CONFIG_NET_HOSTNAME_DYNAMIC)
+int net_hostname_set(char *host, size_t len)
+{
+	if (len > NET_HOSTNAME_MAX_LEN) {
+		return -ENOMEM;
+	}
+
+	memcpy(hostname, host, len);
+	hostname[len] = 0;
+
+	NET_DBG("New hostname %s", hostname);
+	trigger_net_event();
+
+	return 0;
+}
+#endif
+
 #if defined(CONFIG_NET_HOSTNAME_UNIQUE)
 int net_hostname_set_postfix(const uint8_t *hostname_postfix,
 			     int postfix_len)
@@ -62,8 +79,8 @@ int net_hostname_set_postfix(const uint8_t *hostname_postfix,
 	}
 
 	for (i = 0; i < postfix_len; i++, pos += 2) {
-		snprintk(&hostname[sizeof(CONFIG_NET_HOSTNAME) - 1 + pos],
-			 2 + 1, "%02x", hostname_postfix[i]);
+		snprintk(&hostname[sizeof(CONFIG_NET_HOSTNAME) - 1 + pos], 2 + 1, "%02x",
+			 hostname_postfix[i]);
 	}
 
 	NET_DBG("New hostname %s", hostname);