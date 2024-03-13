kartben/zephyr-without-images

commit 8e12a901a01bf13917af10dc4b51968d09ffe567

modem: modem_cellular: CID 338164
strncpy() passed incorrect size

Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

drivers/modem/modem_cellular.c
@@ -337,7 +337,7 @@ static void modem_cellular_chat_on_cgmr(struct modem_chat *chat, char **argv, ui
 		return;
 	}
 
-	strncpy(data->fw_version, argv[1], sizeof(data->fw_version));
+	strncpy(data->fw_version, argv[1], sizeof(data->fw_version) - 1);
 }
 
 static void modem_cellular_chat_on_csq(struct modem_chat *chat, char **argv, uint16_t argc,

commit 79121b7b65887e4c4a55cd44eaf3409afa2b0536

modem: modem_cellular: increase fw_version size
Fixes an issue introduced with `8e12a901`

Signed-off-by: Lucas Denefle <lucas.denefle@converge.io>

drivers/modem/modem_cellular.c
@@ -31,7 +31,7 @@ LOG_MODULE_REGISTER(modem_cellular, CONFIG_MODEM_LOG_LEVEL);
 #define MODEM_CELLULAR_DATA_IMSI_LEN         (22)
 #define MODEM_CELLULAR_DATA_ICCID_LEN        (22)
 #define MODEM_CELLULAR_DATA_MANUFACTURER_LEN (64)
-#define MODEM_CELLULAR_DATA_FW_VERSION_LEN   (64)
+#define MODEM_CELLULAR_DATA_FW_VERSION_LEN   (65)
 
 /* Magic constants */
 #define CSQ_RSSI_UNKNOWN		     (99)