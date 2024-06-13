kz-space/zephyr

net: ptp: Add event generation and event handler for PTP Port
Introduction of PTP stack's core functions responsible for
event generation and handling of these events. Events are generated
base on timeouts and/or PTP messages received via BSD sockets
assigned to a specific PTP Port. These function will be used
in PTP thread.

Signed-off-by: Adam Wojasinski <awojasinski@baylibre.com>

subsys/net/lib/ptp/port.h
@@ -89,6 +89,25 @@ enum ptp_port_state ptp_port_state(struct ptp_port *port);
  * @return PTP Port event.
  */
 enum ptp_port_event ptp_port_timer_event_gen(struct ptp_port *port, struct k_timer *timer);
+/**
+ * @brief Function generating PTP Port events based on messages
+ * that has been recived on a PTP Port's socket.
+ *
+ * @param[in] port  Pointer to the PTP Port structure.
+ * @param[in] idx   Index of the PTP Port's socket to be checked.
+ *
+ * @return PTP Port event.
+ */
+enum ptp_port_event ptp_port_event_gen(struct ptp_port *port, int idx);
+
+/**
+ * @brief Function handling PTP Port event.
+ *
+ * @param[in] port	  Pointer to the PTP Port structure.
+ * @param[in] event	  PTP Port Event to be processed.
+ * @param[in] master_diff Flag indicating whether PTP Master has changed.
+ */
+void ptp_port_event_handle(struct ptp_port *port, enum ptp_port_event event, bool master_diff);
 
 /**
  * @brief Function checking if two port identities are equal.

kartben/zephyr-without-images

subsys/net/lib/ptp/port.h
@@ -103,6 +103,25 @@ enum ptp_port_state ptp_port_state(struct ptp_port *port);
  * @return PTP Port event.
  */
 enum ptp_port_event ptp_port_timer_event_gen(struct ptp_port *port, struct k_timer *timer);
+/**
+ * @brief Function generating PTP Port events based on messages
+ * that has been recived on a PTP Port's socket.
+ *
+ * @param[in] port  Pointer to the PTP Port structure.
+ * @param[in] idx   Index of the PTP Port's socket to be checked.
+ *
+ * @return PTP Port event.
+ */
+enum ptp_port_event ptp_port_event_gen(struct ptp_port *port, int idx);
+
+/**
+ * @brief Function handling PTP Port event.
+ *
+ * @param[in] port    Pointer to the PTP Port structure.
+ * @param[in] event   PTP Port Event to be processed.
+ * @param[in] tt_diff Flag indicating whether PTP TimeTransmitter has changed.
+ */
+void ptp_port_event_handle(struct ptp_port *port, enum ptp_port_event event, bool tt_diff);
 
 /**
  * @brief Function checking if two port identities are equal.