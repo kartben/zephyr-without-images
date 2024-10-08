kartben/zephyr-without-images

commit dec3908ee8666693dc7805d5f7a244579d224805

drivers: dma: stm32: fix dma_stop stream busy handling for HAL override
For DMA channels overridden by HAL DMA, there was no way of
resetting stream busy variable.

Co-authored-by: Maciej Sobkowski <msobkowski@antmicro.com>
Signed-off-by: Dominik Lau <dlau@internships.antmicro.com>

drivers/dma/dma_stm32_bdma.c
@@ -758,6 +758,11 @@ BDMA_STM32_EXPORT_API int bdma_stm32_stop(const struct device *dev, uint32_t id)
 		return -EINVAL;
 	}
 
+	if (stream->hal_override) {
+		stream->busy = false;
+		return 0;
+	}
+
 	/* Repeated stop : return now if channel is already stopped */
 	if (!stm32_bdma_is_enabled_channel(bdma, id)) {
 		return 0;

drivers/dma/dma_stm32.c
@@ -604,6 +604,11 @@ DMA_STM32_EXPORT_API int dma_stm32_stop(const struct device *dev, uint32_t id)
 		return -EINVAL;
 	}
 
+	if (stream->hal_override) {
+		stream->busy = false;
+		return 0;
+	}
+
 	/* Repeated stop : return now if channel is already stopped */
 	if (!stm32_dma_is_enabled_stream(dma, id)) {
 		return 0;

drivers/dma/dma_stm32u5.c
@@ -638,6 +638,11 @@ static int dma_stm32_stop(const struct device *dev, uint32_t id)
 		return -EINVAL;
 	}
 
+	if (stream->hal_override) {
+		stream->busy = false;
+		return 0;
+	}
+
 	/* Repeated stop : return now if channel is already stopped */
 	if (!stm32_dma_is_enabled_stream(dma, id)) {
 		return 0;

commit 96e498822209ba364cd93efa1367e1f8824e65b2

drivers: dma: stm32_bdma: fix regression
Fix regression introduced by dec3908ee8666693dc7805d5f7a244579d224805
undefined variable name is used.

Signed-off-by: Abderrahmane Jarmouni <git@jarmouni.me>

drivers/dma/dma_stm32_bdma.c
@@ -758,8 +758,8 @@ BDMA_STM32_EXPORT_API int bdma_stm32_stop(const struct device *dev, uint32_t id)
 		return -EINVAL;
 	}
 
-	if (stream->hal_override) {
-		stream->busy = false;
+	if (channel->hal_override) {
+		channel->busy = false;
 		return 0;
 	}
 